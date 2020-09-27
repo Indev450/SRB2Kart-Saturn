KartKrewDev/RingRacers

Don't reset special music in P_RestoreMusic
Because that resets Grow music when you stack Grow over Invincibility, and
Invincibility ends. (Also Megamix Themes.)

Thanks Snu, for calling P_RestoreMusic every tic!

src/k_kart.c
@@ -7101,9 +7101,11 @@ void K_MoveKartPlayer(player_t *player, boolean onground)
 									P_SetScale(overlay, player->mo->scale);
 								}
 								player->kartstuff[k_invincibilitytimer] = itemtime+(2*TICRATE); // 10 seconds
+								if (P_IsDisplayPlayer(player))
+									S_ChangeMusicSpecial("kinvnc");
+								else
+									S_StartSound(player->mo, (cv_kartinvinsfx.value ? sfx_alarmg : sfx_kinvnc));
 								P_RestoreMusic(player);
-								if (!P_IsDisplayPlayer(player))
-									S_StartSound(player->mo, (cv_kartinvinsfx.value ? sfx_alarmi : sfx_kinvnc));
 								K_PlayPowerGloatSound(player->mo);
 								player->kartstuff[k_itemamount]--;
 							}
@@ -7304,9 +7306,11 @@ void K_MoveKartPlayer(player_t *player, boolean onground)
 									if (cv_kartdebugshrink.value && !modeattacking && !player->bot)
 										player->mo->destscale = (6*player->mo->destscale)/8;
 									player->kartstuff[k_growshrinktimer] = itemtime+(4*TICRATE); // 12 seconds
-									P_RestoreMusic(player);
-									if (!P_IsDisplayPlayer(player))
+									if (P_IsDisplayPlayer(player))
+										S_ChangeMusicSpecial("kgrow");
+									else
 										S_StartSound(player->mo, (cv_kartinvinsfx.value ? sfx_alarmg : sfx_kgrow));
+									P_RestoreMusic(player);
 									S_StartSound(player->mo, sfx_kc5a);
 								}
 								player->kartstuff[k_itemamount]--;

Indev450/SRB2Kart-Saturn

src/k_kart.c
@@ -5561,9 +5561,11 @@ void K_MoveKartPlayer(player_t *player, boolean onground)
 							P_SetScale(overlay, player->mo->scale);
 						}
 						player->kartstuff[k_invincibilitytimer] = itemtime+(2*TICRATE); // 10 seconds
-						P_RestoreMusic(player);
-						if (!P_IsLocalPlayer(player))
+						if (P_IsLocalPlayer(player))
+							S_ChangeMusicSpecial("kinvnc");
+						else
 							S_StartSound(player->mo, (cv_kartinvinsfx.value ? sfx_alarmi : sfx_kinvnc));
+						P_RestoreMusic(player);
 						K_PlayPowerGloatSound(player->mo);
 						player->kartstuff[k_itemamount]--;
 					}
@@ -5764,9 +5766,11 @@ void K_MoveKartPlayer(player_t *player, boolean onground)
 							if (cv_kartdebugshrink.value && !modeattacking && !player->bot)
 								player->mo->destscale = (6*player->mo->destscale)/8;
 							player->kartstuff[k_growshrinktimer] = itemtime+(4*TICRATE); // 12 seconds
-							P_RestoreMusic(player);
-							if (!P_IsLocalPlayer(player))
+							if (P_IsLocalPlayer(player))
+								S_ChangeMusicSpecial("kgrow");
+							else
 								S_StartSound(player->mo, (cv_kartinvinsfx.value ? sfx_alarmg : sfx_kgrow));
+							P_RestoreMusic(player);
 							S_StartSound(player->mo, sfx_kc5a);
 						}
 						player->kartstuff[k_itemamount]--;