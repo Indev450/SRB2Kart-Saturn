f53d37052af1fd0fcb3c250bbbfa19985da34386
James R <justsomejames2@gmail.com>
2021-05-02 21:59:23 -0700

    Replace LUA_PushLightUserdata with LUA_PushUserdata
    See c92378fa and a7e20f70. I didn't realize that light
    userdata's metatable is shared--like numbers or strings.
    So it cannot be paired with a metatable.
    
    I also made a few minor tweaks to Lua cvars, other than
    accounting for the double pointer in the userdata.

--- src/lua_script.h
+++ src/lua_script.h
@@ -77,7 +77,7 @@ void LUA_Archive(savebuffer_t *save, boolean network);
 void LUA_UnArchive(savebuffer_t *save, boolean network);
 
 void Got_Luacmd(UINT8 **cp, INT32 playernum); // lua_consolelib.c
-void LUA_CVarChanged(const char *name); // lua_consolelib.c
+void LUA_CVarChanged(void *cvar); // lua_consolelib.c
 int Lua_optoption(lua_State *L, int narg, int def, int list_ref);
 int Lua_CreateFieldTable(lua_State *L, const char *const lst[]);
 void LUA_HookNetArchive(lua_CFunction archFunc, savebuffer_t *save);
