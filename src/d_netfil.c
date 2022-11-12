major-gnuisance/srb2k

I hate everything

src/d_netfil.c
@@ -445,7 +445,7 @@ void CL_LoadServerFiles(void)
 			continue; // Already loaded
 		else if (fileneeded[i].status == FS_FOUND)
 		{
-			P_AddWadFile(fileneeded[i].filename);
+			P_AddWadFile(fileneeded[i].filename, false);
 			G_SetGameModified(true, false);
 			fileneeded[i].status = FS_OPEN;
 		}

Indev450/SRB2Kart-Saturn

src/d_netfil.c
@@ -581,7 +581,7 @@ boolean CL_LoadServerFiles(void)
 			continue; // Already loaded
 		else if (fileneeded[i].status == FS_FOUND)
 		{
-			P_PartialAddWadFile(fileneeded[i].filename);
+			P_PartialAddWadFile(fileneeded[i].filename, false);
 			G_SetGameModified(true, false);
 			fileneeded[i].status = FS_OPEN;
 			return false;