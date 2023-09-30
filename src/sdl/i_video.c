0e2ffdeb1dedfd9954ddd4897d8d180ead70a014
James R. <justsomejames2@gmail.com>
2023-09-30 16:07:53 -0700

    SDLSetMode: render immediately after resolution change to avoid 1-frame of texture presented at wrong size

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -1313,6 +1313,11 @@ static void SDLSetMode(INT32 width, INT32 height, SDL_bool fullscreen)
 
 		texture = SDL_CreateTexture(renderer, sw_texture_format, SDL_TEXTUREACCESS_STREAMING, width, height);
 	}
+
+	if (graphics_started)
+	{
+		I_UpdateNoVsync();
+	}
 }
 
 //
