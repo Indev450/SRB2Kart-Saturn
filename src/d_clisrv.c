f545acec826e1d60de5e946f556f1dc90dbb4d85
toaster <rollerorbital@gmail.com>
2023-11-05 18:50:33 +0000

    SV_StopServer: Use G_SetGamestate instead of a direct write
    An apparent oversight identified while working on the previous commit.

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -4727,7 +4727,9 @@ void SV_StopServer(void)
 		Y_EndIntermission();
 	if (gamestate == GS_VOTING)
 		Y_EndVote();
-	gamestate = wipegamestate = GS_NULL;
+
+	G_SetGamestate(GS_NULL);
+	wipegamestate = GS_NULL;
 
 	for (i = 0; i < MAXSPLITSCREENPLAYERS; i++)
 		localtextcmd[i][0] = 0;
