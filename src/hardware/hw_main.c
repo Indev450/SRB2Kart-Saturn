84140f3faa64f95f6574e732b517188663506835
Alug <alufoliehorst@gmail.com>
2023-11-15 10:11:25 +0100

    Revert "why did i remove this back then??"
    i saw what this did again
    fix for avoiding precip crash, cause precip doesent have that stuff
    
    This reverts commit 2e4e0a8554538c841fb4c7be63449b32365d6bc0.

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4796,15 +4796,9 @@ static inline void HWR_DrawPrecipitationSprite(gr_vissprite_t *spr)
 		HWR_Lighting(&Surf, lightlevel, colormap);
 	}
 
-	if (spr->mobj->flags2 & MF2_SHADOW)
-	{
-		Surf.PolyColor.s.alpha = 0x40;
-		blend = PF_Translucent;
-	}
-	else if (spr->mobj->frame & FF_TRANSMASK)
+	if (spr->mobj->frame & FF_TRANSMASK)
 		blend = HWR_TranstableToAlpha((spr->mobj->frame & FF_TRANSMASK)>>FF_TRANSSHIFT, &Surf);
 	else
-
 	{
 		// BP: i agree that is little better in environement but it don't
 		//     work properly under glide nor with fogcolor to ffffff :(
