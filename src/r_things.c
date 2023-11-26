139d2e6be89667ec4fd65d4fa7aa50bd18a8c405
hayaunderscore <stophayapls123@proton.me>
2023-11-26 10:16:16 +0800

    Localskins on replays

--- src/r_things.c
+++ src/r_things.c
@@ -3272,6 +3272,8 @@ void SetLocalPlayerSkin(INT32 playernum, const char *skinname, consvar_t *cvar)
 			CV_StealthSet(cvar, "none");
 		}
 	}
+
+	demo_extradata[playernum] |= DXD_LOCALSKIN;
 }
 
 // Same as SetPlayerSkin, but uses the skin #.
