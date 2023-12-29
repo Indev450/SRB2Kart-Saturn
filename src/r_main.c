f274ed3fd80e9c0b79b2552690c5253e0d43ce1b
Alug <alufoliehorst@gmail.com>
2023-12-29 01:35:50 +0100

    dont need that

--- src/r_main.c
+++ src/r_main.c
@@ -489,32 +489,6 @@ angle_t R_PointToAngleEx(INT32 x2, INT32 y2, INT32 x1, INT32 y1)
 		0;
 }
 
-angle_t R_PointToAngleEx64(INT64 x2, INT64 y2, INT64 x1, INT64 y1)
-{
-    INT64 dx = x1 - x2;
-    INT64 dy = y1 - y2;
-
-    // Check for potential overflow or underflow
-    if (dx < INT64_MIN || dx > INT64_MAX || dy < INT64_MIN || dy > INT64_MAX)
-    {
-        x1 = (INT64)(dx / 2 + x2);
-        y1 = (INT64)(dy / 2 + y2);
-    }
-
-    return (y1 -= y2, (x1 -= x2) || y1) ?
-    x1 >= 0 ?
-    y1 >= 0 ?
-         (x1 > y1) ? tantoangle[SlopeDivEx(y1, x1)] :                            // octant 0
-         ANGLE_90 - tantoangle[SlopeDivEx(x1, y1)] :                               // octant 1
-         x1 > (y1 = -y1) ? 0 - tantoangle[SlopeDivEx(y1, x1)] :                    // octant 8
-         ANGLE_270 + tantoangle[SlopeDivEx(x1, y1)] :                              // octant 7
-         y1 >= 0 ? (x1 = -x1) > y1 ? ANGLE_180 - tantoangle[SlopeDivEx(y1, x1)] :  // octant 3
-         ANGLE_90 + tantoangle[SlopeDivEx(x1, y1)] :                             // octant 2
-         (x1 = -x1) > (y1 = -y1) ? ANGLE_180 + tantoangle[SlopeDivEx(y1, x1)] :    // octant 4
-         ANGLE_270 - tantoangle[SlopeDivEx(x1, y1)] :                              // octant 5
-        0;
-}
-
 //
 // R_ScaleFromGlobalAngle
 // Returns the texture mapping scale for the current line (horizontal span)
