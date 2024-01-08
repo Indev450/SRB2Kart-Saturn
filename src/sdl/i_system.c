f1e146906a2138c614d4204b4f376530e0cb00c7
Indev <bdmitry450@gmail.com>
2024-01-08 23:14:22 +0300

    Mention crash log in crash popup

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -420,7 +420,7 @@ static void I_ReportSignal(int num, int coredumped)
 {
 	//static char msg[] = "oh no! back to reality!\r\n";
 	const char *      sigmsg;
-	char msg[128];
+	char msg[256];
 
 	switch (num)
 	{
@@ -459,15 +459,25 @@ static void I_ReportSignal(int num, int coredumped)
 			sprintf(msg, "%s (core dumped)", sigmsg);
 		else
 			strcat(msg, " (core dumped)");
-
-		sigmsg = msg;
 	}
+	else
+	{
+		sprintf(msg, "%s", sigmsg);
+	}
+
+#ifdef HAVE_LIBBACKTRACE
+	strncat(msg, "\n\nCrash report have been saved into srb2kart-crash-log.txt", 255);
+#elif defined(_WIN32)
+	// Uuh assume we are on 32bit windows where we still have rpt files?
+	// Really unsure about this one
+	strncat(msg, "\n\nCrash report have been saved into srb2kart.rpt", 255);
+#endif
 
 	I_OutputMsg("\nProcess killed by signal: %s\n\n", sigmsg);
 
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
 		"Process killed by signal",
-		sigmsg, NULL);
+		msg, NULL);
 }
 
 #ifndef NEWSIGNALHANDLER
