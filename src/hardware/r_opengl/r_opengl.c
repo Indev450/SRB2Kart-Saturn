8f6cc895a016504f82f5e7062c15c3fecd924093
Alug <alufoliehorst@gmail.com>
2024-01-16 16:22:30 +0100

    fix ogl shearing with flipcam

--- src/hardware/r_opengl/r_opengl.c
+++ src/hardware/r_opengl/r_opengl.c
@@ -3951,6 +3951,8 @@ EXPORT void HWRAPI(SetTransform) (FTransform *stransform)
 	if (shearing)
 	{
 		float dy = FIXED_TO_FLOAT(AIMINGTODY(stransform->viewaiming)) * 2; //screen_width/BASEVIDWIDTH;
+		if (stransform->flip)
+			dy *= -1.0f;
 		pglTranslatef(0.0f, -dy/BASEVIDHEIGHT, 0.0f);
 	}
 
