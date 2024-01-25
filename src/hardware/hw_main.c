b233514b58e7791dc600faa8c6ef19be70c52a88
Alug <alufoliehorst@gmail.com>
2024-01-25 16:48:16 +0100

    more offset

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -3655,7 +3655,7 @@ void HWR_Subsector(size_t num)
 						HWR_AddTransparentFloor(0,
 					                       &extrasubsectors[num],
 										   true,
-					                       *rover->topheight-5000, //dumbass z-fight fix
+					                       *rover->topheight-20000,
 					                       *gr_frontsector->lightlist[light].lightlevel,
 					                       alpha, rover->master->frontsector, PF_Fog|PF_NoTexture,
 										   true, rover->master->frontsector->extra_colormap);
