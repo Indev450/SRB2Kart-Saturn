072bb755044eb6852d3b08db4a6b71f9a7e2e27d
Alug <alufoliehorst@gmail.com>
2024-02-20 13:18:23 +0100

    oop

--- src/hardware/hw_cache.c
+++ src/hardware/hw_cache.c
@@ -568,8 +568,7 @@ GLMapTexture_t *HWR_GetTexture(INT32 tex)
 #ifdef PARANOIA
 		I_Error("HWR_GetTexture: Invalid texture ID %d", tex);
 #else
-		HWR_SetCurrentTexture(NULL);
-		return NULL;
+		tex = 0;
 #endif
 	}
 
