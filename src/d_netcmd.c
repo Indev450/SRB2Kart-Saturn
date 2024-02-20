994ee060a74d359af0f488ec8ad500d6fb61c13a
Alug <alufoliehorst@gmail.com>
2024-02-20 17:36:05 +0100

    a

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -513,10 +513,6 @@ consvar_t cv_mute = {"mute", "Off", CV_NETVAR|CV_CALL, CV_OnOff, Mute_OnChange, 
 
 consvar_t cv_sleep = {"cpusleep", "1", CV_SAVE, sleeping_cons_t, NULL, -1, NULL, NULL, 0, 0, NULL};
 
-static CV_PossibleValue_t skinselectspin_cons_t[] = {
-	{0, "Off"}, {1, "Slow"}, {2, "2"}, {3, "3"}, {4, "4"}, {5, "5"}, {6, "6"}, {7, "7"}, {8, "8"}, {9, "9"}, {10, "Fast"}, {SKINSELECTSPIN_PAIN, "Pain"}, {0, NULL}};
-consvar_t cv_skinselectspin = {"skinselectspin", "5", CV_SAVE, skinselectspin_cons_t, NULL, 0, NULL, NULL, 0, 0, NULL};
-
 static CV_PossibleValue_t nametagtrans_cons_t[] = {
 	{0, "Never"}, {1, "Far only"}, {2, "Dynamic"}, {3, "Always"}, {4, "HUD Trans."}, {0, NULL}};
 /*static CV_PossibleValue_t nametagscaling_cons_t[] = {
