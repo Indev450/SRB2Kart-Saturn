50023b0021043ae0c9c28abe060f69dae3ea2ba9
Alug <alufoliehorst@gmail.com>
2024-02-26 20:58:18 +0100

    replace SCR_FPSflags with V_LocalTransFlag
    so we can actually display hud transparency stuff before the hud appears, since that doesent work with V_HUDTRANS

--- src/v_video.c
+++ src/v_video.c
@@ -477,6 +477,12 @@ static UINT8 hudplusalpha[11]  = { 10,  8,  6,  4,  2,  0,  0,  0,  0,  0,  0};
 static UINT8 hudminusalpha[11] = { 10,  9,  9,  8,  8,  7,  7,  6,  6,  5,  5};
 UINT8 hudtrans = 0;
 
+// this is pretty dumb, but has to be done like this, otherwise the fps counter just disappears sometimes for no reason lol
+INT32 V_LocalTransFlag(void)
+{
+	return ((10-cv_translucenthud.value)*V_10TRANS);
+}
+
 static const UINT8 *v_colormap = NULL;
 static const UINT8 *v_translevel = NULL;
 
