e86490103504306006d5583db868f76abd7419e2
Alug <alufoliehorst@gmail.com>
2024-03-02 01:09:10 +0100

    Options Menu for custom console variables
    https://git.do.srb2.org/STJr/SRB2/-/merge_requests/2380#9cc73f4b99ff925448389969b0ade6ba3947fc31

--- src/lua_consolelib.c
+++ src/lua_consolelib.c
@@ -17,6 +17,11 @@
 #include "byteptr.h"
 #include "z_zone.h"
 
+#include "d_netfil.h"
+
+#include "m_menu.h"
+#include "w_wad.h"
+
 #include "lua_script.h"
 #include "lua_libs.h"
 #include "lua_hud.h" // hud_running errors
@@ -186,8 +191,8 @@ static int lib_comAddCommand(lua_State *L)
 	const char *luaname = luaL_checkstring(L, 1);
 
 	// must store in all lowercase
-	char *name = Z_StrDup(luaname);
-	strlwr(name);
+	char *comname = Z_StrDup(luaname);
+	strlwr(comname);
 
 	luaL_checktype(L, 2, LUA_TFUNCTION);
 	NOHUD
@@ -220,25 +225,26 @@ static int lib_comAddCommand(lua_State *L)
 
 		lua_pushvalue(L, 3);
 		lua_rawseti(L, -2, 2);
-	lua_setfield(L, -2, name);
+	lua_setfield(L, -2, comname);
 
 	// Try to add the Lua command
-	com_return = COM_AddLuaCommand(name);
+	com_return = COM_AddLuaCommand(comname);
 
 	if (com_return < 0)
 	{ // failed to add -- free the lowercased name and return error
-		Z_Free(name);
+		Z_Free(comname);
 		return luaL_error(L, "Couldn't add a new console command \"%s\"", luaname);
 	}
 	else if (com_return == 1)
 	{ // command existed already -- free our name as the old string will continue to be used
-		CONS_Printf("Replaced command \"%s\"\n", name);
-		Z_Free(name);
+		CONS_Printf("Replaced command \"%s\"\n", comname);
+		Z_Free(comname);
 	}
 	else
 	{ // new command was added -- do NOT free the string as it will forever be used by the console
-		CONS_Printf("Added command \"%s\"\n", name);
+		CONS_Printf("Added command \"%s\"\n", comname);
 	}
+
 	return 0;
 }
 
@@ -314,12 +320,17 @@ static int lib_cvRegisterVar(lua_State *L)
 	const char *k;
 	lua_Integer i;
 	consvar_t *cvar;
+
+	const char* category = NULL;
+	const char* menu_name = NULL;
+
 	luaL_checktype(L, 1, LUA_TTABLE);
 	lua_settop(L, 1); // Clear out all other possible arguments, leaving only the first one.
 	NOHUD
 	cvar = lua_newuserdata(L, sizeof(consvar_t));
 	luaL_getmetatable(L, META_CVAR);
 	lua_setmetatable(L, -2);
+	boolean hasminmax = false;
 
 #define FIELDERROR(f, e) luaL_error(L, "bad value for " LUA_QL(f) " in table passed to " LUA_QL("CV_RegisterVar") " (%s)", e);
 #define TYPEERROR(f, t) FIELDERROR(f, va("%s expected, got %s", lua_typename(L, t), luaL_typename(L, -1)))
@@ -385,13 +396,17 @@ static int lib_cvRegisterVar(lua_State *L)
 					if (lua_type(L, 5) != LUA_TSTRING
 					|| lua_type(L, 6) != LUA_TNUMBER)
 						FIELDERROR("PossibleValue", "custom PossibleValue table requires a format of string=integer, i.e. {MIN=0, MAX=9999}");
-					cvpv[i].strvalue = Z_StrDup(lua_tostring(L, 5));
+					char *strvalue = Z_StrDup(lua_tostring(L, 5));
+					if (fasticmp(strvalue, "MIN"))
+						hasminmax = true;
+					cvpv[i].strvalue = strvalue;
 					cvpv[i].value = (INT32)lua_tonumber(L, 6);
 					i++;
 					lua_pop(L, 1);
 				}
 				cvpv[i].value = 0;
 				cvpv[i].strvalue = NULL;
+				hasminmax = false;
 				cvar->PossibleValue = cvpv;
 			} else
 				FIELDERROR("PossibleValue", va("%s or CV_PossibleValue_t expected, got %s", lua_typename(L, LUA_TTABLE), luaL_typename(L, -1)))
@@ -404,6 +419,18 @@ static int lib_cvRegisterVar(lua_State *L)
 			lua_setfield(L, 5, cvar->name);
 			lua_pop(L, 1);
 			cvar->func = Lua_OnChange;
+		}
+		else if (((i == 5 && !(cvar->flags & CV_CALL)) 
+				|| (cvar->flags & CV_CALL && i == 7)) 
+				|| (k && fasticmp(k, "category")))
+		{ 
+			category = lua_isnoneornil(L, 4) ? NULL : lua_tostring(L, 4);
+		}
+		else if (((i == 6 && !(cvar->flags & CV_CALL))
+			|| (cvar->flags & CV_CALL && i == 8))
+			|| (k && fasticmp(k, "menuname")))
+		{
+			menu_name = lua_isnoneornil(L, 4) ? NULL : lua_tostring(L, 4);
 		}
 		lua_pop(L, 1);
 	}
@@ -428,6 +455,22 @@ static int lib_cvRegisterVar(lua_State *L)
 	CV_RegisterVar(cvar);
 	if (cvar->flags & CV_MODIFIED)
 		return luaL_error(L, "failed to register cvar (probable conflict with internal variable/command names)");
+
+	if (!(((cvar->flags & CV_HIDEN)) || (cvar->flags & CV_NOSHOWHELP))
+	{
+		if (!category)
+		{
+			char *temp = strdup(wadfiles[numwadfiles - 1]->filename);
+			nameonly(temp);
+
+			category = temp;
+		}
+
+		if (menu_name && menu_name[0] != '\0')
+			M_SlotCvarIntoModMenu(cvar, category, menu_name, hasminmax);
+		else
+			M_SlotCvarIntoModMenu(cvar, category, cvar->name, hasminmax);
+	}
 
 	// return cvar userdata
 	return 1;
