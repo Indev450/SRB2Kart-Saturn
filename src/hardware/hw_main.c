83175d5c09d4bda0c47b56f1c92ff5307a44fd40
Alug <alufoliehorst@gmail.com>
2024-03-03 06:45:37 +0100

    bruh
    im going insane

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -1161,7 +1161,7 @@ static void HWR_SplitWall(sector_t *sector, FOutVector *wallVerts, INT32 texnum,
 
 		if (solid)
 		{
-			height = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v1x, v1y));
+			bheight = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v1x, v1y));
 			endbheight = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v2x, v2y));
 		}
 
