9c6669fd5c4f86ee9e4b84decb4b28ba0b12fc1c
Alug <alufoliehorst@gmail.com>
2024-03-03 05:18:37 +0100

    Do not reload every texture when adding a wad
    https://git.do.srb2.org/STJr/SRB2/-/merge_requests/1725?commit_id=37add34b9c729b21e624e22d64159073c9258e25

--- src/p_setup.c
+++ src/p_setup.c
@@ -3234,6 +3234,8 @@ UINT16 P_PartialAddWadFile(const char *wadfilename, boolean local)
 	if (!devparm && digmreplaces)
 		CONS_Printf(M_GetText("%s digital musics replaced\n"), sizeu1(digmreplaces));
 
+	R_LoadTexturesPwad(wadnum);
+
 	//
 	// search for sprite replacements
 	//
@@ -3328,7 +3330,7 @@ boolean P_MultiSetupWadFiles(boolean fullsetup)
 	if (partadd_stage == 1)
 	{
 		// Reload all textures, unconditionally for better or worse.
-		R_LoadTextures();
+		//R_LoadTextures();
 
 		// Reload ANIMATED / ANIMDEFS
 		P_InitPicAnims();
