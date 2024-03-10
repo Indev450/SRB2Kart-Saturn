5838e60bf6d7d3bd04401716b3ad3f5d13e1f9eb
Alug <alufoliehorst@gmail.com>
2024-03-10 03:43:21 +0100

    fix overflow stuffs

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -1618,7 +1618,7 @@ static void SV_SendPlayerInfo(INT32 node)
 			continue;
 		}
 
-		if (!playeringame[i])
+		if (playeringame[i] == UINT8_MAX || !playeringame[i])
 		{
 			netbuffer->u.playerinfo[i].node = 255; // This slot is empty.
 			continue;
