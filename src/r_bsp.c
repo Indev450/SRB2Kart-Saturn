780162f51606920951e35a3bf83942f6b209af68
Alug <alufoliehorst@gmail.com>
2024-03-11 11:00:14 +0100

    optimize hwr_renderbpsnode
    actually just make it behave like the software version lol

--- src/r_bsp.c
+++ src/r_bsp.c
@@ -1239,9 +1239,9 @@ void R_RenderBSPNode(INT32 bspnum)
 {
 	node_t *bsp;
 	INT32 side;
-	
+
 	ps_numbspcalls.value.i++;
-	
+
 	while (!(bspnum & NF_SUBSECTOR))  // Found a subsector?
 	{
 		bsp = &nodes[bspnum];
@@ -1252,7 +1252,6 @@ void R_RenderBSPNode(INT32 bspnum)
 		R_RenderBSPNode(bsp->children[side]);
 
 		// Possibly divide back space.
-
 		if (!R_CheckBBox(bsp->bbox[side^1]))
 			return;
 
@@ -1260,7 +1259,8 @@ void R_RenderBSPNode(INT32 bspnum)
 	}
 
 	// PORTAL CULLING
-	if (portalcullsector) {
+	if (portalcullsector)
+	{
 		sector_t *sect = subsectors[bspnum & ~NF_SUBSECTOR].sector;
 		if (sect != portalcullsector)
 			return;
