75f28689715e9388781cd6f48a8f205dd2abb3ee
Alug <alufoliehorst@gmail.com>
2024-03-12 02:13:35 +0100

    inline some things

--- src/tables.c
+++ src/tables.c
@@ -27,7 +27,7 @@
 
 #include "tables.h"
 
-unsigned SlopeDiv(unsigned num, unsigned den)
+FUNCINLINE ATTRINLINE unsigned SlopeDiv(unsigned num, unsigned den)
 {
 	unsigned ans;
 	num <<= (FINE_FRACBITS-FRACBITS);
@@ -38,7 +38,7 @@ unsigned SlopeDiv(unsigned num, unsigned den)
 	return ans <= SLOPERANGE ? ans : SLOPERANGE;
 }
 
-UINT64 SlopeDivEx(unsigned int num, unsigned int den)
+FUNCINLINE ATTRINLINE UINT64 SlopeDivEx(unsigned int num, unsigned int den)
 {
 	UINT64 ans;
 	if (den < 512)
