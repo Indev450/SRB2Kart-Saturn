e04a074a79ba42078518f8494477d2a8d6a6764b
Alug <alufoliehorst@gmail.com>
2024-03-12 16:30:00 +0100

    Revert "condensetics"
    This reverts commit 87001f7e782b39cabf3a85db225d5bdb3b14badf.

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -220,20 +220,18 @@ UINT16 software_MAXPACKETLENGTH;
   * \return The full tic value
   *
   */
-inline tic_t ExpandTics(INT32 low, tic_t basetic)
+tic_t ExpandTics(INT32 low, tic_t basetic)
 {
 	INT32 delta;
-	INT32 opt;
 
 	delta = low - (basetic & UINT8_MAX);
-	opt = basetic & ~UINT8_MAX;
 
-	if (delta < 64)
-		return opt + 256 + low;
+	if (delta >= -64 && delta <= 64)
+		return (basetic & ~UINT8_MAX) + low;
 	else if (delta > 64)
-		return opt - 256 + low;
-
-	return opt + low;
+		return (basetic & ~UINT8_MAX) - 256 + low;
+	else //if (delta < -64)
+		return (basetic & ~UINT8_MAX) + 256 + low;
 }
 
 // -----------------------------------------------------------------
