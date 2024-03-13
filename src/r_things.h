9884b294c62c34bdbd206bd427ba03a8e8a5041e
Alug <alufoliehorst@gmail.com>
2024-03-13 21:55:23 +0100

    i want to see more precip thx

--- src/r_things.h
+++ src/r_things.h
@@ -24,7 +24,7 @@
 
 // number of sprite lumps for spritewidth,offset,topoffset lookup tables
 // Fab: this is a hack : should allocate the lookup tables per sprite
-#define MAXVISSPRITES 2048 // added 2-2-98 was 128
+#define MAXVISSPRITES 4096 // added 2-2-98 was 128
 
 #define VISSPRITECHUNKBITS 6	// 2^6 = 64 sprites per chunk
 #define VISSPRITESPERCHUNK (1 << VISSPRITECHUNKBITS)
