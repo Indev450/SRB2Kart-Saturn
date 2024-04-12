c92ef4d757ef22bb1c9f1f497a13b90292da56ba
Alug <alufoliehorst@gmail.com>
2024-04-12 20:41:10 +0200

    fix software crash
    Zzz...

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -219,7 +219,8 @@ static void Impl_SetWindowIcon(void);
 boolean downsample = false;
 void RefreshSDLSurface(void)
 {
-	OglSdlSurface(vid.width, vid.height);
+	if (rendermode == render_opengl)
+		OglSdlSurface(vid.width, vid.height);
 }
 #endif
 
@@ -747,7 +748,7 @@ static INT32 SDLJoyAxis(const Sint16 axis, evtype_t which)
 #ifdef HWRENDER
 void I_DownSample(void)
 {
-	if (!cv_grframebuffer.value)
+	if (!cv_grframebuffer.value || !rendermode == render_opengl)
 	{
 		downsample = false;
 		return;
@@ -843,7 +844,7 @@ static void Impl_HandleWindowEvent(SDL_WindowEvent evt)
 	}
 
 #ifdef HWRENDER
-	if (windowmoved)
+	if (windowmoved && rendermode == render_opengl)
 	{
 		I_DownSample();
 	}
@@ -1927,7 +1928,8 @@ INT32 VID_SetMode(INT32 modeNum)
 	//Impl_SetWindowName("SRB2Kart "VERSIONSTRING);
 
 #ifdef HWRENDER
-	I_DownSample();
+	if (rendermode == render_opengl)
+		I_DownSample();
 #endif
 
 	SDLSetMode(vid.width, vid.height, USE_FULLSCREEN);
