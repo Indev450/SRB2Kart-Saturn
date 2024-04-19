463dccab8b077547ba959d6e3a4b4eb1ac7d4410
Alug <alufoliehorst@gmail.com>
2024-04-19 04:22:29 +0200

    some smol fixes

--- src/dehacked.c
+++ src/dehacked.c
@@ -334,7 +334,7 @@ static void readPlayer(MYFILE *f, INT32 num)
 				// It works down here, though.
 				{
 					INT32 numline = 0;
-					for (i = 0; i < MAXLINELEN-1; i++)
+					for (i = 0; (size_t)i < sizeof(description[num].notes)-1; i++)
 					{
 						if (numline < 20 && description[num].notes[i] == '\n')
 							numline++;
