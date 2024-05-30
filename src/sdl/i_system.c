040ddb1f40b17c84427c95a241a102c0e8f633bb
Indev <bdmitry450@gmail.com>
2024-05-30 19:23:14 +0300

    Revert "Revert "Don't traverse the entire HOME directory to find wads""
    This reverts commit 443905ffb3dd8904c395c9c50570ccc17b42dd27.

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -4039,12 +4039,16 @@ static const char *locateWad(void)
 #endif
 #ifndef NOHOME
 	// find in $HOME
-	I_OutputMsg(",HOME");
+	I_OutputMsg(",HOME/" DEFAULTDIR);
 	if ((envstr = I_GetEnv("HOME")) != NULL)
 	{
+		char *tmp = malloc(strlen(envstr) + sizeof(DEFAULTDIR));
+		strcpy(tmp, envstr);
+		strcat(tmp, DEFAULTDIR);
 		WadPath = searchWad(envstr);
 		if (WadPath)
 			return WadPath;
+		free(tmp);
 	}
 #endif
 #ifdef DEFAULTSEARCHPATH1
