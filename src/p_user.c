6ffb50530dff945c4b42b8bd1307186fb468e5f3
Alug <alufoliehorst@gmail.com>
2024-06-09 18:13:17 +0200

    add mobj check here

--- src/p_user.c
+++ src/p_user.c
@@ -1195,6 +1195,9 @@ mobj_t *P_SpawnGhostMobj(mobj_t *mobj)
 {
 	mobj_t *ghost;
 
+	if (!mobj || P_MobjWasRemoved(mobj))
+		return;
+
 	ghost = P_SpawnMobj(mobj->x, mobj->y, mobj->z, MT_GHOST);
 
 	P_SetScale(ghost, mobj->scale);
