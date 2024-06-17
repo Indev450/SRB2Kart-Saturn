578f5b85dde3c0bbde6afa8f8cc35e5186a1c1b4
Alug <alufoliehorst@gmail.com>
2024-06-17 12:51:29 +0200

    Revert "more interp fixes"
    This reverts commit 3a1a58bcfc7e26147d79454898a77987167887a0.

--- src/p_spec.c
+++ src/p_spec.c
@@ -2371,7 +2371,11 @@ static void P_ProcessLineSpecial(line_t *line, mobj_t *mo, sector_t *callsec)
 					y = sides[line->sidenum[0]].rowoffset;
 					z = line->frontsector->ceilingheight;
 
-					P_SetOrigin(mo, mo->x + x, mo->y + y, mo->z + z);
+					P_UnsetThingPosition(mo);
+					mo->x += x;
+					mo->y += y;
+					mo->z += z;
+					P_SetThingPosition(mo);
 
 					if (mo->player)
 					{
@@ -2390,6 +2394,7 @@ static void P_ProcessLineSpecial(line_t *line, mobj_t *mo, sector_t *callsec)
 								camera[i].reset = true;
 								camera[i].subsector = R_PointInSubsector(camera[i].x, camera[i].y);
 								R_RelativeTeleportViewInterpolation(i, x, y, z, 0);
+								R_ResetViewInterpolation(i + 1); // reset view interp as well
 								break;
 							}
 						}
