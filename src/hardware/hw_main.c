d59f45a26a2ef5bc5f61d14a9931c5c9f976c88a
Alug <alufoliehorst@gmail.com>
2024-07-10 06:55:47 +0200

    smol optimization to HWR_Addline

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -2538,6 +2538,18 @@ doaddline:
 	{
 		gr_backsector = R_FakeFlat(gr_backsector, &tempsec, NULL, NULL, true);
 
+		if ((gr_backsector->ceilingpic == skyflatnum && gr_frontsector->ceilingpic == skyflatnum) && (gr_backsector->floorpic == skyflatnum && gr_frontsector->floorpic == skyflatnum)) // everything's sky? let's save us a bit of time then
+		{
+			if (!line->polyseg &&
+				!line->sidedef->midtexture
+				&& ((!gr_frontsector->ffloors && !gr_backsector->ffloors)
+				|| gr_frontsector->tag == gr_backsector->tag))
+				return; // line is empty, don't even bother
+				// treat like wide open window instead
+				HWR_ProcessSeg(); // Doesn't need arguments because they're defined globally :D
+				return;
+		}
+
 		if (CheckClip(gr_frontsector, gr_backsector))
 		{
 			gld_clipper_SafeAddClipRange(angle2, angle1);
