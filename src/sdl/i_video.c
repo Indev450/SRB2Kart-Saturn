8bb1c7a03263bd77bb58d0fcf40f9d7e992ae0be
Alug <alufoliehorst@gmail.com>
2024-07-14 18:03:21 +0200

    Revert "run this in actual fullscreen"
    This reverts commit 4a2d6b22412643b5c9ace9b2a0c791072c760213.

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -243,7 +243,7 @@ static void SDLSetMode(INT32 width, INT32 height, SDL_bool fullscreen)
 		if (fullscreen)
 		{
 			wasfullscreen = SDL_TRUE;
-			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 		}
 		else // windowed mode
 		{
@@ -268,7 +268,7 @@ static void SDLSetMode(INT32 width, INT32 height, SDL_bool fullscreen)
 		SDL_SetWindowSize(window, width, height);
 		if (fullscreen)
 		{
-			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 		}
 	}
 
@@ -1964,7 +1964,7 @@ static SDL_bool Impl_CreateWindow(SDL_bool fullscreen)
 		return SDL_FALSE;
 
 	if (fullscreen)
-		flags |= SDL_WINDOW_FULLSCREEN;
+		flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
 
 	if (borderlesswindow)
 		flags |= SDL_WINDOW_BORDERLESS;
@@ -1992,7 +1992,7 @@ static SDL_bool Impl_CreateWindow(SDL_bool fullscreen)
 
 	// Create a window
 	window = SDL_CreateWindow("SRB2Kart "VERSIONSTRING, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			vid.width, vid.height, flags);
+			realwidth, realheight, flags);
 
 	if (window == NULL)
 	{
