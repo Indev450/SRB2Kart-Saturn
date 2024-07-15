52a882bda2832b41c2bf5411df3b34a502c969ab
Alug <alufoliehorst@gmail.com>
2024-07-15 21:58:00 +0200

    remove this check

--- src/p_inter.c
+++ src/p_inter.c
@@ -1910,9 +1910,6 @@ void P_KillMobj(mobj_t *target, mobj_t *inflictor, mobj_t *source)
 	mobj_t *mo;
 	int ms;
 
-	if (!target || P_MobjWasRemoved(target))
-		return;
-
 	if (!useNightsSS && G_IsSpecialStage(gamemap) && target->player && sstimer > 6)
 		sstimer = 6; // Just let P_Ticker take care of the rest.
 
