77a2976d05f02dfcc5124c0b27d815deab9f8b1f
Alug <alufoliehorst@gmail.com>
2024-07-30 23:54:49 +0200

    smol interp fixes

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -49,6 +49,7 @@
 #include "s_sound.h" // sfx_syfail
 #include "m_perfstats.h"
 #include "d_main.h"
+#include "r_fps.h"
 
 #ifdef CLIENT_LOADINGSCREEN
 // cl loading screen
@@ -6109,6 +6110,8 @@ boolean TryRunTics(tic_t realtics)
 
 	if (ticking)
 	{
+		boolean tickInterp = true;
+
 		// run the count * tics
 		while (neededtic > gametic)
 		{
@@ -6119,7 +6122,18 @@ boolean TryRunTics(tic_t realtics)
 			if (update_stats)
 				PS_START_TIMING(ps_tictime);
 
-			G_Ticker((gametic % NEWTICRATERATIO) == 0);
+			boolean run = (gametic % NEWTICRATERATIO) == 0;
+
+			if (run && tickInterp)
+			{
+				// Update old view state BEFORE ticking so resetting
+				// the old interpolation state from game logic works.
+				R_UpdateViewInterpolation();
+				tickInterp = false; // do not update again in sped-up tics
+			}
+
+			G_Ticker(run);
+
 			ExtraDataTicker();
 			gametic++;
 			consistancy[gametic%TICQUEUE] = Consistancy();
