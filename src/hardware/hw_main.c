341815c0ed9bd3639c51c01ec1b1c8c92f0f45de
Alug <alufoliehorst@gmail.com>
2024-09-02 12:10:24 +0200

    this can be static

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -307,6 +307,7 @@ ps_metric_t ps_hw_batchsorttime = {0};
 ps_metric_t ps_hw_batchdrawtime = {0};
 
 static void HWR_AddPrecipitationSprites(void);
+static void HWR_ProjectPrecipitationSprite(precipmobj_t *thing);
 
 // ==========================================================================
 // View position
@@ -5089,7 +5090,7 @@ void HWR_ProjectSprite(mobj_t *thing)
 }
 
 // Precipitation projector for hardware mode
-void HWR_ProjectPrecipitationSprite(precipmobj_t *thing)
+static void HWR_ProjectPrecipitationSprite(precipmobj_t *thing)
 {
 	gr_vissprite_t *vis;
 	float tr_x, tr_y;
--- src/hardware/hw_main.h
+++ src/hardware/hw_main.h
@@ -136,7 +136,7 @@ void HWR_AddPolyObjectSegs(void);
 // hw_main.c: Sprites
 void HWR_AddSprites(sector_t *sec);
 void HWR_ProjectSprite(mobj_t *thing);
-void HWR_ProjectPrecipitationSprite(precipmobj_t *thing);
+//void HWR_ProjectPrecipitationSprite(precipmobj_t *thing);
 void HWR_DrawSprites(void);
 
 // hw_bsp.c
