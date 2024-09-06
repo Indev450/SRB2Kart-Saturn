e7a0ac104a2e1b289a3b95bcf535986004b0356b
Alug <alufoliehorst@gmail.com>
2024-09-06 19:59:39 +0200

    oops again

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -156,10 +156,8 @@ static boolean cl_redownloadinggamestate = false;
 boolean is_client_saturn[MAXNETNODES];
 #endif
 
-#ifdef SATURNSYNCH
 #ifdef SATURNJOIN
 #define ISSATURN 69
-#endif
 #endif
 
 static UINT8 localtextcmd[MAXTEXTCMD];
