589c16e1fc2234ddbdf0ef753a9a3f17d19295c0
Alug <alufoliehorst@gmail.com>
2024-09-08 14:51:01 +0200

    smol cleanup

--- src/p_setup.h
+++ src/p_setup.h
@@ -93,7 +93,6 @@ size_t P_PrecacheLevelFlats(void);
 void P_AllocMapHeader(INT16 i);
 
 // Needed for NiGHTS
-void P_ReloadRings(void);
 void P_DeleteGrades(INT16 i);
 void P_AddGradesForMare(INT16 i, UINT8 mare, char *gtext);
 UINT8 P_GetGrade(UINT32 pscore, INT16 map, UINT8 mare);
