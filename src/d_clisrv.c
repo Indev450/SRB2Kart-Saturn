97ee57ff9c1aff47338ad5d978d0362f2a1c3144
Alug <alufoliehorst@gmail.com>
2024-09-13 13:30:23 +0200

    add smol comment

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -1878,6 +1878,7 @@ static void CL_ReloadReceivedSavegame(void)
 		neededtic = gametic;
 	maketic = neededtic;
 
+	// set camera angle to somewhat match where your player is turning
 	for (i = 0; i <= splitscreen; i++)
 	{
 		localangle[i] = players[displayplayers[i]].cmd.angleturn<<16;
