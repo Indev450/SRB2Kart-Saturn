2c0129be4724c3ec8d7faef86b7534ba2f033fec
Alug <alufoliehorst@gmail.com>
2024-09-23 20:38:38 +0200

    dont read map marker files just skip em instead
    thx indev <3

--- src/w_wad.c
+++ src/w_wad.c
@@ -2216,6 +2216,7 @@ virtres_t* vres_GetMap(lumpnum_t lumpnum)
 			}
 		}
 		numlumps++;
+		++lumpnum; // Skip map marker
 
 		vlumps = (virtlump_t*)(Z_Malloc(sizeof(virtlump_t)*numlumps, PU_LEVEL, NULL));
 		for (i = 0; i < numlumps; i++, lumpnum++)
