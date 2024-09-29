661984b62053728f51c63707aeddeaf02840c93e
Alug <alufoliehorst@gmail.com>
2024-09-29 21:51:31 +0200

    no need to even traverse part of the blockmap when PRECIP_STORM_NORAIN and PRECIP_BLANK

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4626,7 +4626,7 @@ static void HWR_AddPrecipitationSprites(void)
 		drawdist = (fixed_t)(cv_drawdist_precip.value) * precipscale;
 
 	// No to infinite precipitation draw distance.
-	if (drawdist == 0)
+	if (drawdist == 0 || curWeather == PRECIP_BLANK || curWeather == PRECIP_STORM_NORAIN)
 	{
 		return;
 	}
