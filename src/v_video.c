2dddc817131d3249420fc47605ed863bf68b26f7
Alug <alufoliehorst@gmail.com>
2024-09-30 15:42:55 +0200

    Put all text input logic into separate file and use it for all text input fields

--- src/v_video.c
+++ src/v_video.c
@@ -2847,11 +2847,14 @@ INT32 V_LevelNameHeight(const char *string)
 //
 // Find string width from hu_font chars
 //
-INT32 V_StringWidth(const char *string, INT32 option)
+INT32 V_SubStringWidth(const char *string, INT32 length, INT32 option)
 {
 	INT32 c, w = 0;
 	INT32 spacewidth = 4, charwidth = 0;
-	size_t i;
+	ssize_t i;
+
+	if (length < 0)
+		length = strlen(string);
 
 	switch (option & V_SPACINGMASK)
 	{
@@ -2867,7 +2870,7 @@ INT32 V_StringWidth(const char *string, INT32 option)
 			break;
 	}
 
-	for (i = 0; i < strlen(string); i++)
+	for (i = 0; string[i] && i < length; i++)
 	{
 		c = string[i];
 		if ((UINT8)c >= 0x80 && (UINT8)c <= 0x8F) //color parsing! -Inuyasha 2.16.09
@@ -2886,11 +2889,14 @@ INT32 V_StringWidth(const char *string, INT32 option)
 //
 // Find string width from hu_font chars, 0.5x scale
 //
-INT32 V_SmallStringWidth(const char *string, INT32 option)
+INT32 V_SmallSubStringWidth(const char *string, INT32 length, INT32 option)
 {
 	INT32 c, w = 0;
 	INT32 spacewidth = 2, charwidth = 0;
-	size_t i;
+	ssize_t i;
+
+	if (length < 0)
+		length = strlen(string);
 
 	switch (option & V_SPACINGMASK)
 	{
@@ -2906,7 +2912,7 @@ INT32 V_SmallStringWidth(const char *string, INT32 option)
 			break;
 	}
 
-	for (i = 0; i < strlen(string); i++)
+	for (i = 0; string[i] && i < length; i++)
 	{
 		c = string[i];
 		if ((UINT8)c >= 0x80 && (UINT8)c <= 0x8F) //color parsing! -Inuyasha 2.16.09
@@ -2925,12 +2931,15 @@ INT32 V_SmallStringWidth(const char *string, INT32 option)
 //
 // Find string width from tny_font chars
 //
-INT32 V_ThinStringWidth(const char *string, INT32 option)
+INT32 V_ThinSubStringWidth(const char *string, INT32 length, INT32 option)
 {
 	INT32 c, w = 0;
 	INT32 spacewidth = 2, charwidth = 0;
 	boolean lowercase = (option & V_ALLOWLOWERCASE);
-	size_t i;
+	ssize_t i;
+
+	if (length < 0)
+		length = strlen(string);
 
 	switch (option & V_SPACINGMASK)
 	{
@@ -2947,7 +2956,7 @@ INT32 V_ThinStringWidth(const char *string, INT32 option)
 			break;
 	}
 
-	for (i = 0; i < strlen(string); i++)
+	for (i = 0; string[i] && i < length; i++)
 	{
 		c = string[i];
 		if ((UINT8)c >= 0x80 && (UINT8)c <= 0x8F) //color parsing! -Inuyasha 2.16.09
@@ -2965,7 +2974,7 @@ INT32 V_ThinStringWidth(const char *string, INT32 option)
 		else
 		{
 			w += (charwidth ? charwidth
-				: ((option & V_6WIDTHSPACE && i < strlen(string)-1) ? max(1, SHORT(tny_font[c]->width)-1) // Reuse this flag for the alternate bunched-up spacing
+				: ((option & V_6WIDTHSPACE && i < length-1) ? max(1, SHORT(tny_font[c]->width)-1) // Reuse this flag for the alternate bunched-up spacing
 				: SHORT(tny_font[c]->width)));
 		}
 	}
