9657dc0f1b774ebf06bf54eba6c1aefdb3ea7b2a
Alug <alufoliehorst@gmail.com>
2024-10-06 18:44:14 +0200

    prevent this from become super large somehow lol
    just like cap it at 19 it works just fine

--- src/d_main.c
+++ src/d_main.c
@@ -655,7 +655,7 @@ static boolean D_Display(void)
 // =========================================================================
 
 tic_t rendergametic;
-static int menuInputDelayTimer = 0;
+static SINT8 menuInputDelayTimer = 0;
 
 void D_SRB2Loop(void)
 {
@@ -793,9 +793,10 @@ void D_SRB2Loop(void)
 				event_t myev;
 				myev.type = ev_keydown;
 
-				menuInputDelayTimer++;
+				if (menuInputDelayTimer < 19)
+					menuInputDelayTimer++;
 
-				if (menuInputDelayTimer >= 19) // TICRATE * ( (k+2) (1 - [wz + h + j - q]^2 - [(gk + 2g + k + 1)(h + j) + h - z]^2 - [16(k + 1)^3(k + 2)(n + 1)^2 + 1 - f^2]^2 calculated by my butt
+				if (menuInputDelayTimer == 19) // TICRATE * ( (k+2) (1 - [wz + h + j - q]^2 - [(gk + 2g + k + 1)(h + j) + h - z]^2 - [16(k + 1)^3(k + 2)(n + 1)^2 + 1 - f^2]^2 calculated by my butt
 				{
 					if (DPADUPSCROLL)
 						DPADSCROLLINPUT(KEY_UPARROW)
