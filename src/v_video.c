6333fc73e9ddad2f068100d77439f6a93647fa0d
Alug <alufoliehorst@gmail.com>
2024-10-25 20:21:04 +0200

    move the return ealier in V_DoPostProcessor

--- src/v_video.c
+++ src/v_video.c
@@ -3212,6 +3212,9 @@ void V_DoPostProcessor(INT32 view, player_t *player, INT32 param)
 	if (view < 0 || view > 3 || view > splitscreen)
 		return;
 
+	if (!player->postimgflags)
+		return;
+
 	if ((view == 1 && splitscreen == 1) || view >= 2)
 		yoffset = viewheight;
 	else
@@ -3224,9 +3227,6 @@ void V_DoPostProcessor(INT32 view, player_t *player, INT32 param)
 
 	UINT8 *tmpscr = screens[4];
 	UINT8 *srcscr = screens[0];
-
-	if (!player->postimgflags)
-		return;
 
 	if (player->postimgflags & POSTIMG_WATER)
 	{
