22164031feb027f7215c06944980aad63f4de7c4
Alug <alufoliehorst@gmail.com>
2024-11-02 01:05:06 +0100

    gu?

--- src/hardware/hw_portal.c
+++ src/hardware/hw_portal.c
@@ -171,7 +171,7 @@ void HWR_RenderPortal(gl_portal_t* portal, gl_portal_t* rootportal, const float 
 		HWR_PortalFrame(rootportal);
 	else // current frame is not a portal frame but the main view!
 	{
-		R_SetupFrame(player, false);
+		R_SetupFrame(viewssnum, false);
 		portalclipline = NULL;
 	}
 
