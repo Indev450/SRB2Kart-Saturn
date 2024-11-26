d6a2fe099383413aa275811bfa60d7688d4e30bf
Alug <alufoliehorst@gmail.com>
2024-11-26 18:21:10 +0100

    init polyobj thinker cachable bool
    otherwise shit will die lol

--- src/p_polyobj.c
+++ src/p_polyobj.c
@@ -153,6 +153,9 @@ FUNCINLINE static ATTRINLINE void PolyObj_AddThinker(thinker_t *th)
 	th->next = thinkercap.next;
 	th->prev = &thinkercap;
 	thinkercap.next = th;
+
+	//th->references = 0;
+	th->cachable = false; // not initialising this made the mobjcache die thx!
 }
 
 static void FreeSideLists(void)
