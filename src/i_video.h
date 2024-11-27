b8483376c42474fff3e3a561aefc4d3d03d7e821
Alug <alufoliehorst@gmail.com>
2024-11-27 22:29:49 +0100

    dont draw final screen texture twice if you use native res
    no need to
    saves some performance

--- src/i_video.h
+++ src/i_video.h
@@ -139,6 +139,8 @@ UINT32 I_GetRefreshRate(void);
 
 boolean I_UseNativeKeyboard(void);
 
+boolean I_CheckNativeRes(void);
+
 #ifdef USE_FBO_OGL
 #if defined (__unix__)
 extern boolean xwaylandcrap;
