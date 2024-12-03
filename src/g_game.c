68090a55e12d5ecbbd31aebc12388c43649f3206
Alug <alufoliehorst@gmail.com>
2024-12-03 21:50:45 +0100

    change this a little

--- src/g_game.c
+++ src/g_game.c
@@ -7826,13 +7826,16 @@ boolean G_CheckDemoStatus(void)
 		return true;
 	}
 
-	if (demo.recording && (modeattacking || demo.savemode != DSM_NOTSAVING))
+	if (!demo.recording)
+		return false;
+
+	if (modeattacking || demo.savemode != DSM_NOTSAVING)
 	{
 		G_SaveDemo();
 		return true;
 	}
-	else if (demo.recording)
-		G_ResetDemoRecording();
+
+	G_ResetDemoRecording();
 
 	return false;
 }
