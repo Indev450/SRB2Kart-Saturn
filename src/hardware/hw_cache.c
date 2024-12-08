0e843c2a96dcb1df8aeebaea8391f90b4ae34b6f
Alug <alufoliehorst@gmail.com>
2024-12-08 01:27:57 +0100

    stuff

--- src/hardware/hw_cache.c
+++ src/hardware/hw_cache.c
@@ -336,8 +336,8 @@ static void HWR_GenerateTexture(INT32 texnum, GLMapTexture_t *gltex, boolean noe
 	else
 		gltex->mipmap.flags = TF_CHROMAKEYED | TF_WRAPXY;
 
-	gltex->mipmap.width = (UINT16)texture->width;
-	gltex->mipmap.height = (UINT16)texture->height;
+	gltex->mipmap.width = SHORT(texture->width);
+	gltex->mipmap.height = SHORT(texture->height);
 
 	if (skyspecial)
 		gltex->mipmap.format = GL_TEXFMT_RGBA; // that skyspecial code below assumes this format ...
@@ -416,14 +416,13 @@ void HWR_MakePatch (patch_t *patch, GLPatch_t *glPatch, GLMipmap_t *glMipmap, bo
 		glPatch->leftoffset = SHORT(patch->leftoffset);
 		glPatch->topoffset = SHORT(patch->topoffset);
 
-		//glMipmap->width = (UINT16)SHORT(patch->width);
-		//glMipmap->height = (UINT16)SHORT(patch->height);
 		glMipmap->width = glMipmap->height = 1;
 		while (glMipmap->width < glPatch->width) glMipmap->width <<= 1;
 		while (glMipmap->height < glPatch->height) glMipmap->height <<= 1;
 
 		// no wrap around, no chroma key
 		glMipmap->flags = 0;
+
 		// setup the texture info
 		glMipmap->format = patchformat;
 
@@ -443,8 +442,6 @@ void HWR_MakePatch (patch_t *patch, GLPatch_t *glPatch, GLMipmap_t *glMipmap, bo
 			glPatch->width, glPatch->height,
 			patch);
 	}
-
-	//glPatch->max_s = glPatch->max_t = 1.0f;
 }
 
 // =================================================
