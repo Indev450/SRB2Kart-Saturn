13a5909c3f31c87d5c088e1fa012ad945bc1be8a
Alug <alufoliehorst@gmail.com>
2024-12-08 04:13:16 +0100

    oop

--- src/r_things.c
+++ src/r_things.c
@@ -1389,14 +1389,6 @@ static void R_ProjectSprite(mobj_t *thing)
 			else
 				pitchnroll += rollangle;
 
-			// this is kinda dumb lkmao, but try to mitigate shadows being weirdly offset on slopes
-			if (thing->type == MT_SHADOW)
-			{
-				sprinfo->available = true; // < lmao
-				sprinfo->pivot[(thing->frame & FF_FRAMEMASK)].x = spr_offset>>FRACBITS;
-				sprinfo->pivot[(thing->frame & FF_FRAMEMASK)].y = -12; // noones gonna replace shadow sprite anyways, right?
-			}
-
 			rollangle = R_GetRollAngle(pitchnroll);
 			rotsprite = Patch_GetRotatedSprite(sprframe, (thing->frame & FF_FRAMEMASK), rot, flip, false, sprinfo, rollangle);
 
