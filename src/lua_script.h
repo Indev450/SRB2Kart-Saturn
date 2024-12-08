0eec77d8b3ce96b9cda81d6c4cd908002f5f270e
Alug <alufoliehorst@gmail.com>
2024-12-08 18:43:17 +0100

    a

--- src/lua_script.h
+++ src/lua_script.h
@@ -13,10 +13,21 @@
 #include "m_fixed.h"
 #include "doomtype.h"
 #include "d_player.h"
+#include "p_saveg.h"
 
+#ifdef NOBLUAJIT
 #include "blua/lua.h"
 #include "blua/lualib.h"
 #include "blua/lauxlib.h"
+#else
+#include "command.h"
+#include "bluajit/lua.h"
+#include "bluajit/lualib.h"
+#include "bluajit/lauxlib.h"
+#define abs_index(L, i)		((i) > 0 || (i) <= LUA_REGISTRYINDEX ? (i) : \
+					lua_gettop(L) + (i) + 1)
+extern consvar_t cv_luajit;
+#endif
 
 #define lua_optboolean(L, i) (!lua_isnoneornil(L, i) && lua_toboolean(L, i))
 #define lua_opttrueboolean(L, i) (lua_isnoneornil(L, i) || lua_toboolean(L, i))
