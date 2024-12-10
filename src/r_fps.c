3b64263dc649043a2ef0853d56fdd44468cd0b52
Alug <alufoliehorst@gmail.com>
2024-12-10 21:16:06 +0100

    Unconditionally interpolate view
    even if it may NOT change
    but i get the feeling it may cause some issues

--- src/r_fps.c
+++ src/r_fps.c
@@ -91,6 +91,15 @@ static levelinterpolator_t **levelinterpolators;
 static size_t levelinterpolators_len;
 static size_t levelinterpolators_size;
 
+static inline fixed_t R_LerpFixedView(fixed_t from, fixed_t to, fixed_t frac)
+{
+	return from + FixedMul(frac, to - from);
+}
+
+static inline angle_t R_LerpAngleView(angle_t from, angle_t to, fixed_t frac)
+{
+	return from + FixedMul(frac, to - from);
+}
 
 static inline fixed_t R_LerpFixed(fixed_t from, fixed_t to, fixed_t frac)
 {
@@ -153,7 +162,7 @@ static void R_SetupFreelook(void)
 
 void R_InterpolateViewRollAngle(fixed_t frac)
 {
-	viewroll = R_LerpAngle(oldview->roll, newview->roll, frac);
+	viewroll = R_LerpAngleView(oldview->roll, newview->roll, frac);
 }
 
 void R_InterpolateView(fixed_t frac, boolean forceinvalid)
@@ -181,13 +190,13 @@ void R_InterpolateView(fixed_t frac, boolean forceinvalid)
 		prevview = newview;
 	}
 
-	viewx = R_LerpFixed(prevview->x, newview->x, frac);
-	viewy = R_LerpFixed(prevview->y, newview->y, frac);
-	viewz = R_LerpFixed(prevview->z, newview->z, frac);
+	viewx = R_LerpFixedView(prevview->x, newview->x, frac);
+	viewy = R_LerpFixedView(prevview->y, newview->y, frac);
+	viewz = R_LerpFixedView(prevview->z, newview->z, frac);
 
-	viewangle = R_LerpAngle(prevview->angle, newview->angle, frac);
-	aimingangle = R_LerpAngle(prevview->aim, newview->aim, frac);
-	viewroll = R_LerpAngle(prevview->roll, newview->roll, frac);
+	viewangle = R_LerpAngleView(prevview->angle, newview->angle, frac);
+	aimingangle = R_LerpAngleView(prevview->aim, newview->aim, frac);
+	viewroll = R_LerpAngleView(prevview->roll, newview->roll, frac);
 
 	viewsin = FINESINE(viewangle>>ANGLETOFINESHIFT);
 	viewcos = FINECOSINE(viewangle>>ANGLETOFINESHIFT);
