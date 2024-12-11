521b53dc09c60120f13751a5a8e906a649a2db03
Alug <alufoliehorst@gmail.com>
2024-12-11 23:23:46 +0100

    maybe fix fade stuff idk

--- src/sdl/ogl_sdl.c
+++ src/sdl/ogl_sdl.c
@@ -44,6 +44,7 @@
 #include "hwsym_sdl.h"
 #include "../m_argv.h"
 #include "../i_video.h"
+#include "../f_finale.h"
 
 #ifdef DEBUG_TO_FILE
 #include <stdarg.h>
@@ -280,6 +281,7 @@ void OglSdlFinishUpdate(boolean waitvbl)
 #ifdef USE_FBO_OGL
 	&& (!UseScreenFBO())
 #endif
+	|| WipeInAction
 	)
 		//HWR_DrawScreenFinalTexture(realwidth, realheight, false);
 		GL_DrawScreenFinalTexture(HWD_SCREENTEXTURE_GENERIC2, realwidth, realheight, false);
