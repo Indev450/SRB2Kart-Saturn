8df8749a9cebbe68ab7820283a17b9ed1378b571
Alug <alufoliehorst@gmail.com>
2024-12-20 00:51:05 +0100

    return false here

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -723,12 +723,13 @@ boolean I_CheckNativeRes(void)
 
 	if (SDL_GetCurrentDisplayMode(currentDisplayIndex, &curmode) != 0)
 	{
-		return resstate;
+		return false;
 	}
 
 	resstate = ((vid.width == curmode.w) && (vid.height == curmode.h));
 	oldwidth = vid.width;
 	oldheight = vid.height;
+
 	return resstate;
 }
 
