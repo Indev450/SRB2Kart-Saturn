773df88224a4d998d679effb83ce5931bb6923b7
Alug <alufoliehorst@gmail.com>
2024-12-23 17:44:48 +0100

    dont remember removing this in s_sound

--- src/s_sound.c
+++ src/s_sound.c
@@ -866,6 +866,7 @@ void S_ClearSfx(void)
 
 static void S_StopChannel(INT32 cnum)
 {
+	INT32 i;
 	channel_t *c = &channels[cnum];
 
 	if (c->sfxinfo)
@@ -873,6 +874,12 @@ static void S_StopChannel(INT32 cnum)
 		// stop the sound playing
 		if (I_SoundIsPlaying(c->handle))
 			I_StopSound(c->handle);
+
+		// check to see
+		//  if other channels are playing the sound
+		for (i = 0; i < numofchannels; i++)
+			if (cnum != i && c->sfxinfo == channels[i].sfxinfo)
+				break;
 
 		// degrade usefulness of sound data
 		c->sfxinfo->usefulness--;
