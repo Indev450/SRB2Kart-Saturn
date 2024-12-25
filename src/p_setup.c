9bde946c5594de7eedad1b96e72be828dd7a0613
Alug <alufoliehorst@gmail.com>
2024-12-25 01:56:58 +0100

    add tweakmap support
    https://git.do.srb2.org/KartKrew/RingRacers/-/commit/7fb44bdb0239a8012be73cb9ee384a9fdca9addb

--- src/p_setup.c
+++ src/p_setup.c
@@ -2834,7 +2834,7 @@ boolean P_SetupLevel(boolean skipprecip, boolean reloadinggamestate)
 	curmapvirt = vres_GetMap(lastloadedmaplumpnum);
 
 	R_ReInitColormaps(mapheaderinfo[gamemap-1]->palette,
-		(encoremode ? W_CheckNumForName(va("%sE", maplumpname)) : LUMPERROR));
+		W_CheckNumForName(va("%s%c", maplumpname, (encoremode ? 'E' : 'T'))));
 	CON_SetupBackColormap();
 
 	// SRB2 determines the sky texture to be used depending on the map header.
