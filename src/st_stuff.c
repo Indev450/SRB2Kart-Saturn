64a5a4920b6dbe1e2695010654906d7f6284f81c
Alug <alufoliehorst@gmail.com>
2024-12-28 18:24:56 +0100

    cleanup K_DirectorIsAvailable

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -665,7 +665,7 @@ static void ST_overlayDrawer(void)
 
 	if (!hu_showscores) // hide the following if TAB is held
 	{
-		if (cv_showdirectorhud.value && !splitscreen && ((demo.playback && !camera[stplyrnum].freecam && (!demo.title || !modeattacking)) || !P_IsLocalPlayer(stplyr)) && !K_DirectorIsPlayerAlone())
+		if (cv_showdirectorhud.value && !splitscreen && !P_IsLocalPlayer(stplyr) && K_DirectorIsAvailable(0) && !K_DirectorIsPlayerAlone())
 		{
 			char directortext[20] = {0};
 
