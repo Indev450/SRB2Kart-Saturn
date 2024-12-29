4110130ca7dc96980df6ad6b437cc393ce741057
Alug <alufoliehorst@gmail.com>
2024-12-29 06:36:36 +0100

    block freecam toggling when player is in spec

--- src/p_user.c
+++ src/p_user.c
@@ -3141,13 +3141,17 @@ void P_ToggleDemoCamera(UINT8 viewnum)
 {
 	camera_t *cam = &camera[viewnum];
 
+	// dont let freecam be toggled when in spec lel
+	if (players[displayplayers[viewnum]].spectator)
+		return;
+
 	if (!cam->freecam)	// toggle on
 	{
 		cam->freecam = true;
 		cam->button_a_held = 2;
 		cam->reset_aiming = true;
 	}
-	else	// toggle off
+	else				// toggle off
 	{
 		cam->freecam = false;
 	}
