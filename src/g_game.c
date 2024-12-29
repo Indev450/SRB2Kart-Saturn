6101ded74ae9ce3d1986b4dd5c79cb595b0ebcb4
Alug <alufoliehorst@gmail.com>
2024-12-29 17:56:35 +0100

    allocate some extra buffer for demobuffer
    incase this might overflow that we dont write to unvalid memory
    thx indev <3

--- src/g_game.c
+++ src/g_game.c
@@ -6044,7 +6044,7 @@ void G_RecordDemo(const char *name)
 
 		maxsize = cv_maxdemosize.value*1024*1024;
 
-		demobuf.buffer = Z_Malloc(maxsize, PU_STATIC, NULL);
+		demobuf.buffer = Z_Malloc(maxsize + 100*1024, PU_STATIC, NULL);
 		demoend = demobuf.buffer + maxsize;
 
 		if (demobuf.buffer)
@@ -6062,7 +6062,7 @@ void G_RecordMetal(void)
 		Z_Free(demobuf.buffer);
 	demobuf.p = NULL;
 	metalrecording = false;
-	demobuf.buffer = Z_Malloc(maxsize, PU_STATIC, NULL);
+	demobuf.buffer = Z_Malloc(maxsize + 100*1024, PU_STATIC, NULL);
 	demoend = demobuf.buffer + maxsize;
 
 	if (demobuf.buffer)
