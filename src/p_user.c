2186fc6ce075b57d189d6d319ce44f8b58c8b31c
Alug <alufoliehorst@gmail.com>
2024-12-29 06:40:13 +0100

    dont need to do this twice

--- src/p_user.c
+++ src/p_user.c
@@ -3387,24 +3387,6 @@ static void P_DemoCameraMovement(camera_t *cam, UINT8 num)
 		}
 	}
 
-	if (rendermode == render_soft
-#ifdef HWRENDER
-		|| (rendermode == render_opengl && (cv_glshearing.value == 1))
-#endif
-		)
-	{
-		// Extra restriction on this so it's not possible to
-		// distort the view too much.
-		if ((INT32)cam->aiming > ANGLE_45)
-			cam->aiming = ANGLE_45;
-		else if ((INT32)cam->aiming < -ANGLE_45)
-			cam->aiming = -ANGLE_45;
-	}
-	else
-	{
-		G_ClipAimingPitch((INT32 *)&cam->aiming);
-	}
-
 	cam->momx = cam->momy = cam->momz = 0;
 	if (cmd->forwardmove != 0)
 	{
