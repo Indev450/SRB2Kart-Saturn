d12320bbb5bc413401e4969af036e7365e4c1564
Alug <alufoliehorst@gmail.com>
2024-12-30 11:24:14 +0100

    dont disable this.... if its disabled kek

--- src/m_menu.c
+++ src/m_menu.c
@@ -3410,12 +3410,10 @@ void PDistort_menu_Onchange(void)
 {
 	if (!cv_sloperoll.value)
 	{
-		OP_PlayerDistortMenu[sloperotate].status = IT_GRAYEDOUT;
 		OP_PlayerDistortMenu[sliptide].status = IT_GRAYEDOUT;
 	}
 	else
 	{
-		OP_PlayerDistortMenu[sloperotate].status = IT_STRING | IT_CVAR;
 		OP_PlayerDistortMenu[sliptide].status = IT_STRING | IT_CVAR;
 	}
 
