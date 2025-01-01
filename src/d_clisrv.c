81c1f2d1a837e02ad99e4afa23e317b870d8e38a
Alug <alufoliehorst@gmail.com>
2025-01-01 17:06:03 +0100

    always close any menus when using "connect" command

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -3120,9 +3120,10 @@ static void Command_connect(void)
 		return;
 	}
 
+	M_ClearMenus(true);
+
 	if (Playing() || demo.title)
 	{
-		M_ClearMenus(true);
 		if (demo.title)
 			G_CheckDemoStatus();
 
