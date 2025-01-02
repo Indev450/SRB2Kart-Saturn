854cb7ee49dc6e1c0cf2b458fc8ec6879a43f7f4
Alug <alufoliehorst@gmail.com>
2025-01-02 05:21:25 +0100

    this doesent work for us

--- src/p_user.c
+++ src/p_user.c
@@ -3362,13 +3362,13 @@ static void P_DemoCameraMovement(camera_t *cam, UINT8 num)
 	}
 
 	// if you hold item, you will lock on to displayplayer. (The last player you were ""f12-ing"")
-	if (cmd->buttons & BT_ATTACK)
+	// well this only really works in replays for us, since we still move our spec player around which causes displayplayer to be the spec player
+	if (demo.playback)
 	{
-		lastp = &players[displayplayers[0]];	// Fun fact, I was trying displayplayers[0]->mo as if it was Lua like an absolute idiot.
-
-		// dont chase after your spectator self
-		if (!lastp->spectator)
+		if (cmd->buttons & BT_ATTACK)
 		{
+			lastp = &players[displayplayers[0]];	// Fun fact, I was trying displayplayers[0]->mo as if it was Lua like an absolute idiot.
+
 			const fixed_t dist = R_PointToDist2(cam->x, cam->y, lastp->mo->x, lastp->mo->y);
 			cam->angle = R_PointToAngle2(cam->x, cam->y, lastp->mo->x, lastp->mo->y);
 			cam->aiming = R_PointToAngle2(0, cam->z, dist, lastp->mo->z + lastp->mo->scale*128*P_MobjFlip(lastp->mo));	// This is still unholy. Aim a bit above their heads.
