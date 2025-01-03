3dd0313d5379517ceaba6cef65d8168ac96ee001
Alug <alufoliehorst@gmail.com>
2025-01-03 01:14:01 +0100

    only ever touch this if player has a mobjs
    angleturn is dumb in kart and rarely really is what we want

--- src/g_game.c
+++ src/g_game.c
@@ -1750,7 +1750,8 @@ void G_FixCamera(UINT8 view)
 
 	// The order of displayplayers can change, which would
 	// invalidate localangle.
-	localangle[view - 1] = (P_MobjWasRemoved(player->mo) ? ((angle_t)player->cmd.angleturn << 16) : player->mo->angle);
+	if (!P_MobjWasRemoved(player->mo))
+		localangle[view - 1] = player->mo->angle;
 
 	P_ResetCamera(player, &camera[view - 1]);
 
