e105d8f51eae21b711202ecbdb42a8d42b134e68
Alug <alufoliehorst@gmail.com>
2025-01-03 15:54:44 +0100

    apparently this is slower for everything but rendering

--- src/r_main.h
+++ src/r_main.h
@@ -140,17 +140,6 @@ FUNCINLINE static ATTRINLINE subsector_t *R_PointInSubsector(fixed_t x, fixed_t 
 	return &subsectors[nodenum & ~NF_SUBSECTOR];
 }
 
-// uses R_PointOnSideFast
-// SHOULD NOT BE USED FOR ANYTHING GAMEPLAY RELATED!!
-FUNCINLINE static ATTRINLINE subsector_t *R_PointInSubsectorFast(fixed_t x, fixed_t y)
-{
-	size_t nodenum = numnodes-1;
-
-	while (!(nodenum & NF_SUBSECTOR))
-		nodenum = nodes[nodenum].children[R_PointOnSideFast(x, y, nodes+nodenum)];
-
-	return &subsectors[nodenum & ~NF_SUBSECTOR];
-}
 
 #define R_PointToDist(x, y) R_PointToDist2(viewx, viewy, x, y)
 #define R_PointToDist2(px2, py2, px1, py1) FixedHypot((px1) - (px2), (py1) - (py2))
