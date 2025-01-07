5106c391bbc69dd15994e314f722ac9439a81178
Alug <alufoliehorst@gmail.com>
2025-01-07 16:29:33 +0100

    readd check for banan
    this time without mobjwasremoved
    just check if banan exists

--- src/k_kart.c
+++ src/k_kart.c
@@ -4327,6 +4327,10 @@ void K_KillBananaChain(mobj_t *banana, mobj_t *inflictor, mobj_t *source)
 	mobj_t *cachenext;
 
 killnext:
+
+	if (!banana)
+		return;
+
 	cachenext = banana->hnext;
 
 	if (banana->health)
