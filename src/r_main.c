864d747e3bbdcb6df62992ec92ed3b2f15e6cded
Alug <alufoliehorst@gmail.com>
2025-01-07 19:49:36 +0100

    check if subsector has sector before assigning

--- src/r_main.c
+++ src/r_main.c
@@ -1062,11 +1062,6 @@ mobj_t *viewmobj;
 
 static void R_SetupCommonFrame(player_t * player, subsector_t * subsector)
 {
-	if (!player)
-	{
-		return;
-	}
-
 	newview->player = player;
 
 	newview->x += quake.x;
@@ -1075,7 +1070,7 @@ static void R_SetupCommonFrame(player_t * player, subsector_t * subsector)
 
 	newview->roll = R_ViewRollAngle(player);
 
-	if (subsector != NULL)
+	if (subsector && subsector->sector)
 		newview->sector = subsector->sector;
 	else
 		newview->sector = R_PointInSubsector(newview->x, newview->y)->sector;
