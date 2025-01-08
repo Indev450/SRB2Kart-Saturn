0e1f843f5078158458ebafba6f2dee20529a9e1c
Alug <alufoliehorst@gmail.com>
2025-01-08 05:52:01 +0100

    ;

--- src/y_inter.c
+++ src/y_inter.c
@@ -345,7 +345,7 @@ static void Y_PlayerStandingsDrawer(y_data_t *standings, INT32 x, INT32 hilicol)
 		return;
 	}
 
-	INT32 i;;
+	INT32 i;
 
 #define NUMFORNEWCOLUMN 8
 	INT32 y = 41, gutter = ((standings->numplayers > NUMFORNEWCOLUMN) ? 0 : (BASEVIDWIDTH/2));
