017646e704817e151c39ada39e3be6c851768642
Alug <alufoliehorst@gmail.com>
2025-01-12 00:01:57 +0100

    forgor about software again

--- src/r_bsp.c
+++ src/r_bsp.c
@@ -907,6 +907,9 @@ static void R_Subsector(size_t num)
 		{
 			for (rover = frontsector->ffloors; rover; rover = rover->next)
 			{
+				if (!(rover->flags & FF_EXISTS) || !(rover->flags & FF_RENDERPLANES))
+					continue;
+
 				sector_t *controlSec = &sectors[rover->secnum];
 
 				if (controlSec->moved != true)
