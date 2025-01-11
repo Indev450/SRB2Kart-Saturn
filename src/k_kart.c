bf0d4077b0a7b64c9c4bd221f5efb4a4db16c850
Alug <alufoliehorst@gmail.com>
2025-01-11 18:10:15 +0100

    hmm try making them have absolute lightlevel

--- src/k_kart.c
+++ src/k_kart.c
@@ -1632,7 +1632,8 @@ void K_KartBouncing(mobj_t *mobj1, mobj_t *mobj2, boolean bounce, boolean solid)
 		fx->eflags |= MFE_VERTICALFLIP;
 	else
 		fx->eflags &= ~MFE_VERTICALFLIP;
-	fx->lightlevel = 255;
+	fx->lightlevel = M_RandomRange(200, 255);
+	fx->frame |= FF_ABSOLUTELIGHTLEVEL;
 	P_SetScale(fx, mobj1->scale);
 
 	// Because this is done during collision now, rmomx and rmomy need to be recalculated
@@ -3277,7 +3278,8 @@ static void K_SpawnDriftSparks(player_t *player)
 				P_SetMobjState(spark, S_DRIFTSPARK_A1);
 		}
 
-		spark->lightlevel = M_RandomRange(20, 255);
+		spark->lightlevel = M_RandomRange(200, 255);
+		spark->frame |= FF_ABSOLUTELIGHTLEVEL;
 
 		K_MatchGenericExtraFlags(spark, player->mo);
 	}
@@ -3611,7 +3613,8 @@ void K_SpawnSparkleTrail(mobj_t *mo)
 
 		sparkle->color = mo->color;
 
-		sparkle->lightlevel = M_RandomRange(20, 255);
+		sparkle->lightlevel = M_RandomRange(200, 255);
+		sparkle->frame |= FF_ABSOLUTELIGHTLEVEL;
 	}
 
 	P_SetMobjState(sparkle, S_KARTINVULN_LARGE1);
@@ -3751,7 +3754,8 @@ void K_DriftDustHandling(mobj_t *spawner)
 			else
 				dust->color = SKINCOLOR_SILVER; // fallback
 
-			dust->lightlevel = M_RandomRange(20, 255);
+			dust->lightlevel = M_RandomRange(200, 255);
+			dust->frame |= FF_ABSOLUTELIGHTLEVEL;
 		}
 		else
 			dust->color = 0; // dont recolour MT_DRIFTDUST
@@ -5289,7 +5293,8 @@ static inline void K_SpawnNormalSpeedLines(player_t *player)
 		}
 	}
 
-	fast->lightlevel = M_RandomRange(20, 255);
+	fast->lightlevel = M_RandomRange(200, 255);
+	fast->frame |= FF_ABSOLUTELIGHTLEVEL;
 }
 
 /**	\brief	Decreases various kart timers and powers per frame. Called in P_PlayerThink in p_user.c
