1237b553f72477efd152dca665f7a9625dac9176
Alug <alufoliehorst@gmail.com>
2025-01-12 00:59:47 +0100

    toggle for camera turnaround on timeover

--- src/p_local.h
+++ src/p_local.h
@@ -148,6 +148,7 @@ extern consvar_t cv_cam_height[MAXSPLITSCREENPLAYERS];
 extern consvar_t cv_cam_speed[MAXSPLITSCREENPLAYERS];
 extern consvar_t cv_cam_rotate[MAXSPLITSCREENPLAYERS];
 extern consvar_t cv_cam_rotspeed[MAXSPLITSCREENPLAYERS];
+extern consvar_t cv_cam_timeover[MAXSPLITSCREENPLAYERS];
 
 extern consvar_t cv_freecam_speed;
 
