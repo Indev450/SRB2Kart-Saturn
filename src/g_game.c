2512a9bc872e8678a4ecb9880bbc08549f55c6e3
Alug <alufoliehorst@gmail.com>
2025-01-13 17:42:22 +0100

    no need to strcmp just check for "savemoddata"
    that bool only ever gets set to true if the savegame has been switched to the modded one and unlike in srb2 this WILL never be reset to false again

--- src/g_game.c
+++ src/g_game.c
@@ -3631,7 +3631,7 @@ void G_LoadGameData(void)
 	}
 
 	// well no clue but dont think it would like reading garbage from vanilla files
-	K_ReadStats(&save, strcmp(gamedatafilename, "modkartdata.dat") != 0);
+	K_ReadStats(&save, !savemoddata);
 
 	modded = READUINT8(save.p);
 
@@ -3739,7 +3739,7 @@ void G_SaveGameData(boolean force)
 	// Version test
 	WRITEUINT32(save.p, 0xFCAFE211);
 
-	K_WriteStats(&save, strcmp(gamedatafilename, "modkartdata.dat") != 0);
+	K_WriteStats(&save, !savemoddata);
 
 	btemp = (UINT8)(savemoddata); // what used to be here was profoundly dunderheaded
 	WRITEUINT8(save.p, btemp);
