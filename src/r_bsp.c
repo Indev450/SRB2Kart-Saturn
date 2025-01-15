559b0bf45a346a0b24f67c03ea32ee3678141c8d
Alug <alufoliehorst@gmail.com>
2025-01-15 12:09:48 +0100

    also skip invisible fofs

--- src/r_bsp.c
+++ src/r_bsp.c
@@ -897,7 +897,7 @@ static void R_Subsector(size_t num)
 		{
 			for (rover = frontsector->ffloors; rover; rover = rover->next)
 			{
-				if (!(rover->flags & FF_EXISTS) || !(rover->flags & FF_RENDERPLANES))
+				if (!(rover->flags & FF_EXISTS) || !(rover->flags & FF_RENDERPLANES) || !(rover->flags & FF_RENDERALL))
 					continue;
 
 				sector_t *controlSec = &sectors[rover->secnum];
