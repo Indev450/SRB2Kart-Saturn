55f2841c335fcbf1da0124d1490e18ad6f35b4cc
Alug <alufoliehorst@gmail.com>
2025-01-16 23:32:59 +0100

    only rumble banan when on ground

--- src/p_tick.c
+++ src/p_tick.c
@@ -440,7 +440,7 @@ static void P_DeviceRumbleTick(void)
 		{
 			low = FRACUNIT / 256;
 		}
-		else if (player->kartstuff[k_bananadrag] > TICRATE)
+		else if (player->kartstuff[k_bananadrag] > TICRATE && P_IsObjectOnGround(player->mo))
 		{
 			if (leveltime & 1) // this is actually funny lel
 				high = FRACUNIT / 64;
