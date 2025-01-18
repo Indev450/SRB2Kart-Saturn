14fff2e3a3680dab9275606d97f259cc00075518
Indev <bdmitry450@gmail.com>
2025-01-19 02:43:18 +0300

    Wipe kart stats on game data reload

--- src/g_game.c
+++ src/g_game.c
@@ -3602,6 +3602,7 @@ void G_LoadGameData(void)
 	// to new gamedata
 	G_ClearRecords(); // main and nights records
 	M_ClearSecrets(); // emblems, unlocks, maps visited, etc
+	K_EraseStats(); // stats
 
 	if (M_CheckParm("-nodata"))
 		return; // Don't load.
