85d68a7b7ba9afe299e8d9e9423454b7d4d4fbc4
Indev <bdmitry450@gmail.com>
2025-01-19 02:28:16 +0300

    Make custom stats fully compatible between different versions, so new fields can be added in future without save corruptions

--- src/k_stats.h
+++ src/k_stats.h
@@ -5,6 +5,8 @@
 #include "d_player.h"
 #include "p_saveg.h"
 
+#define KARTSTATSVERSION 1
+
 typedef struct kartstats_s {
 	// Remember if stats we loaded are vanilla ones or not
 	boolean vanilla;
@@ -12,6 +14,12 @@ typedef struct kartstats_s {
 	// Vanilla
 	tic_t totalplaytime;
 	UINT32 matchesplayed;
+
+	// Custom data, fields go in same order as they are written into game save
+
+	// IMPORTANT: size **must be first**, so K_WriteStats can overwrite it (not that we're gonna change order, but just in case)
+	UINT32 size; // Size required for our stats. If older version reads save from newer one, it will just skip unread fields, avoiding reading corrupted values
+	UINT32 version; // Version, so we know which fields we actually can read
 
 	tic_t raplaytime;
 	tic_t onlineplaytime;
@@ -26,6 +34,8 @@ typedef struct kartstats_s {
 	UINT32 sinks; // Times hitting *others* by kitchen sink
 	UINT32 sinked; // Times *being hit* by kitchen sink
 	UINT32 respawns;
+
+	UINT8 *copy; // Copy of stats block that has been read from file. In case we're older version that reads newer save, we will keep values that we couldn't read
 } kartstats_t;
 
 extern kartstats_t kartstats;
