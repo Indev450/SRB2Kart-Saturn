2036dfd83b0e18bf6f7b3c98b5e277e29348dc0f
Alug <alufoliehorst@gmail.com>
2025-01-22 15:44:25 +0100

    also fix potential use-after-free in HWR_AddPrecipitationSprites
    no clue why ogl didnt do it since its effectivle the same code as software

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4479,7 +4479,7 @@ static void HWR_AddSprites(sector_t *sec)
 static void HWR_AddPrecipitationSprites(void)
 {
 	INT32 xl, xh, yl, yh, bx, by;
-	precipmobj_t *th;
+	precipmobj_t *th, *next;
 
 	fixed_t drawdist;
 
@@ -4511,6 +4511,9 @@ static void HWR_AddPrecipitationSprites(void)
 		{
 			for (th = precipblocklinks[(by * bmapwidth) + bx]; th; th = next)
 			{
+				// Store this beforehand because HWR_ProjectPrecipitationSprite may free th (see P_PrecipThinker)
+				next = th->bnext;
+
 				HWR_ProjectPrecipitationSprite(th);
 			}
 		}
