a3dfa2a930f6af2bbf2cd5feeab9a0c06aa4b3cf
Alug <alufoliehorst@gmail.com>
2025-01-23 20:40:05 +0100

    assign player later

--- src/p_tick.c
+++ src/p_tick.c
@@ -378,8 +378,6 @@ static void P_DeviceRumbleTick(void)
 		UINT16 low = 0;
 		UINT16 high = 0;
 
-		player_t *player = ((i == 0) ? &players[consoleplayer] : &players[displayplayers[i]]);
-
 		if (!cv_usejoystick[i].value)
 		{
 			continue;
@@ -389,6 +387,8 @@ static void P_DeviceRumbleTick(void)
 		{
 			continue;
 		}
+
+		player_t *player = ((i == 0) ? &players[consoleplayer] : &players[displayplayers[i]]);
 
 		if (player->spectator)
 		{
