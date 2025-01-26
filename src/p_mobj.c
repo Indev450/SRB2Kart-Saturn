7016b77ee89f52cc68cd0fd196ed1171ffa13d7a
Alug <alufoliehorst@gmail.com>
2025-01-26 21:17:20 +0100

    also rotate eggboxes in air

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -7815,6 +7815,7 @@ void P_MobjThinker(mobj_t *mobj)
 			break;
 		}
 		case MT_BANANA:
+		case MT_EGGMANITEM:
 			if (cv_sloperoll.value == 2 && cv_bananthrowroll.value && !P_IsObjectOnGround(mobj))
 			{
 				// tilt n tumble
@@ -7826,8 +7827,7 @@ void P_MobjThinker(mobj_t *mobj)
 				else if (cv_bananthrowroll.value == 2)
 					mobj->rollangle -= spin;
 			}
-			/* FALLTHRU */
-		case MT_EGGMANITEM:
+
 			mobj->friction = ORIG_FRICTION/4;
 			if (mobj->momx || mobj->momy)
 				P_SpawnGhostMobj(mobj);
