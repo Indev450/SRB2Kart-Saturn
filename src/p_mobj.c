34603b9128a3857fd13de249eb5acd77d29f03f8
Alug <alufoliehorst@gmail.com>
2025-01-27 23:24:01 +0100

    dont recalc if precip sector didnt move
    forgor i can just check this

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -3634,7 +3634,7 @@ animonly:
 	P_CyclePlayerMobjState(mobj, true);
 }
 
-static void P_CalculatePrecipFloor(precipmobj_t *mobj)
+static void P_CalculatePrecipFloor(precipmobj_t *mobj, boolean spawn)
 {
 	// recalculate floorz each time
 	const sector_t *mobjsecsubsec;
@@ -3642,6 +3642,10 @@ static void P_CalculatePrecipFloor(precipmobj_t *mobj)
 	if (mobj && mobj->subsector && mobj->subsector->sector)
 		mobjsecsubsec = mobj->subsector->sector;
 	else
+		return;
+
+	// no need to recalc anything if not moved
+	if (!spawn && !mobjsecsubsec->moved)
 		return;
 
 	mobj->floorz = P_GetSectorFloorZAt(mobjsecsubsec, mobj->x, mobj->y);
@@ -3735,7 +3739,7 @@ boolean P_PrecipThinker(precipmobj_t *mobj)
 		return true;
 
 	if (renderisnewtic)
-		P_CalculatePrecipFloor(mobj);
+		P_CalculatePrecipFloor(mobj, false);
 
 	// adjust height
 	if ((mobj->z += mobj->momz) <= mobj->floorz)
@@ -10143,7 +10147,7 @@ static precipmobj_t *P_SpawnPrecipMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype
 	mobj->thinker.function.acp1 = (actionf_p1)P_NullPrecipThinker;
 	P_AddPrecipThinker(&mobj->thinker);
 
-	P_CalculatePrecipFloor(mobj);
+	P_CalculatePrecipFloor(mobj, true);
 
 	if (mobj->floorz != starting_floorz)
 		mobj->precipflags |= PCF_FOF;
