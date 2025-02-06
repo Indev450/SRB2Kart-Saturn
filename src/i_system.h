a6a53f6b9c898ba2d68013f803bff6c686e55923
Alug <alufoliehorst@gmail.com>
2025-02-06 02:06:57 +0100

    dont pass bogus values to I_GamepadRumble and I_SetGamepadIndicatorColor
    - fixes rumble and led stuff not working after hotplugging
    - also fixes sdl2-compat breaking
    - i hate karts controller code

--- src/i_system.h
+++ src/i_system.h
@@ -234,8 +234,8 @@ INT32 I_NumJoys(void);
 */
 const char *I_GetJoyName(INT32 joyindex);
 
-void I_GamepadRumble(INT32 device_id, UINT16 low_strength, UINT16 high_strength, UINT32 duration);
-void I_SetGamepadIndicatorColor(INT32 device_id, UINT8 red, UINT8 green, UINT8 blue);
+void I_GamepadRumble(INT32 playernum, UINT16 low_strength, UINT16 high_strength, UINT32 duration);
+void I_SetGamepadIndicatorColor(INT32 playernum, UINT8 red, UINT8 green, UINT8 blue);
 
 #ifndef NOMUMBLE
 #include "p_mobj.h" // mobj_t
