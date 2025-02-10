34c517defc8e11eaf89d6894a403a977159e6014
Alug <alufoliehorst@gmail.com>
2025-02-10 19:05:44 +0100

    dont add buffer delay when local delay is disabled

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -6700,7 +6700,8 @@ static void UpdatePingTable(void)
 
 		simulated_lag = lowest_lag;
 
-		lowest_lag += (tic_t)cv_netticbuffer.value; // account for netticbufffer
+		if (lowest_lag != 0)
+			lowest_lag += (tic_t)cv_netticbuffer.value; // account for netticbufffer
 
 		pingmeasurecount++;
 	}
@@ -6718,7 +6719,8 @@ static void UpdatePingTable(void)
 
 		simulated_lag = lowest_lag;
 
-		lowest_lag += (tic_t)cv_netticbuffer.value; // account for netticbufffer
+		if (lowest_lag != 0)
+			lowest_lag += (tic_t)cv_netticbuffer.value; // account for netticbufffer
 	}
 }
 
