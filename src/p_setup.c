50e9721a0d7e861e63a1585f94e08977ced48507
Alug <alufoliehorst@gmail.com>
2025-02-15 14:27:52 +0100

    also precache animated flats
    i have no clue how tf animated textures work

--- src/p_setup.c
+++ src/p_setup.c
@@ -754,6 +754,7 @@ INT32 P_AddLevelFlat(const char *flatname, levelflat_t *levelflat)
 
 		// store the flat lump number
 		levelflat->lumpnum = R_GetFlatNumForName(flatname);
+		levelflat->baselumpnum = LUMPERROR;
 
 		if (!havesnakerpad && memcmp(levelflat->name, "BOST", 4) == 0)
 		{
@@ -811,6 +812,7 @@ INT32 P_AddLevelFlatRuntime(const char *flatname)
 
 		// store the flat lump number
 		levelflat->lumpnum = R_GetFlatNumForName(flatname);
+		levelflat->baselumpnum = LUMPERROR;
 
 #ifndef ZDEBUG
 		CONS_Debug(DBG_SETUP, "flat #%03d: %s\n", atoi(sizeu1(numlevelflats)), levelflat->name);
