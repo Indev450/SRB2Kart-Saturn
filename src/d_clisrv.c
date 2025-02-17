115eea3828467bae694a13e70a45664f2772700c
Alug <alufoliehorst@gmail.com>
2025-02-17 20:40:22 +0100

    revert all the server ticking stuff

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -6394,20 +6394,18 @@ boolean TryRunTics(tic_t realtics)
 	}
 #endif
 
-	ticking = neededtic > gametic;
-
-	if (ticking)
+	if (neededtic > gametic)
 	{
-		if (realtics)
-			hu_stopped = false;
+		hu_stopped = false;
 	}
 
 	if (player_joining)
 	{
-		if (realtics)
-			hu_stopped = true;
+		hu_stopped = true;
 		return false;
 	}
+
+	ticking = neededtic > gametic;
 
 	if (ticking)
 	{
@@ -6668,14 +6666,6 @@ void NetUpdate(void)
 	if (realtics <= 0) // nothing new to update
 		return;
 
-	if (realtics > 5)
-	{
-		if (server)
-			realtics = 1;
-		else
-			realtics = 5;
-	}
-
 #ifdef DEDICATEDIDLETIME
 	if (server && dedicated && gamestate == GS_LEVEL)
 	{
@@ -6723,6 +6713,14 @@ void NetUpdate(void)
 		}
 	}
 #endif
+
+	if (realtics > 5)
+	{
+		if (server)
+			realtics = 1;
+		else
+			realtics = 5;
+	}
 
 	gametime = nowtime;
 
@@ -6816,8 +6814,6 @@ void NetUpdate(void)
 	}
 	Net_AckTicker();
 	HandleNodeTimeouts();
-
-	nowtime /= NEWTICRATERATIO;
 
 	if (nowtime > resptime)
 	{
