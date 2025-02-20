93b39260085c876a5c731baf1489b20a58dd5dc2
Indev <bdmitry450@gmail.com>
2025-02-20 23:32:05 +0300

    Allow trailing spaces in lj_strscan_scan

--- src/bluajit/lj_strscan.c
+++ src/bluajit/lj_strscan.c
@@ -496,6 +496,9 @@ StrScanFmt lj_strscan_scan(const uint8_t *p, MSize len, TValue *o,
 #endif
 
 #if LJ_INTONLY
+	/* Skip trailing spaces */
+	while (p < pe && *p == ' ') ++p;
+
     if (*p || p < pe)
       return STRSCAN_ERROR;
 #else
