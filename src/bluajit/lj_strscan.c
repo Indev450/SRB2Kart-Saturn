9f76ede41ee2f83426001d759c593c758349d808
Indev <bdmitry450@gmail.com>
2025-03-03 01:03:19 +0300

    Revert "Restore setnumV for fmt == STRSCAN_NUM in lj_strscan_scan"
    This reverts commit 644cd3628658786c9dab84dcbd34ad81b2389e52.

--- src/bluajit/lj_strscan.c
+++ src/bluajit/lj_strscan.c
@@ -575,10 +575,7 @@ StrScanFmt lj_strscan_scan(const uint8_t *p, MSize len, TValue *o,
       fmt = strscan_bin(sp, o, fmt, opt, ex, neg, dig);
     else
 #if LJ_INTONLY
-      if (fmt == STRSCAN_NUM)
-        setnumV(o, (lua_Number)(int32_t)(double)(neg ? ~x+1u : x));
-      else
-        setintV(o, (int32_t)(double)(neg ? ~x+1u : x));
+      setintV(o, (int32_t)(double)(neg ? (~x+1u) : x));
 #else
       fmt = strscan_dec(sp, o, fmt, opt, ex, neg, dig);
 #endif
