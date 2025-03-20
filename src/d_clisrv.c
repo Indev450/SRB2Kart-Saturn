189a28f34953bb4398f4f04d859b8fe189be10aa
Alug <alufoliehorst@gmail.com>
2025-03-20 18:13:53 +0100

    delet player_joining

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -151,7 +151,6 @@ static UINT32 resynch_status[MAXNETNODES]; // 0 bit means synched for that playe
 static UINT8 resynch_sent[MAXNETNODES][MAXPLAYERS]; // what synch packets have we attempted to send to the player
 static UINT8 resynch_inprogress[MAXNETNODES];
 static UINT8 resynch_local_inprogress = false; // WE are desynched and getting packets to fix it.
-static UINT8 player_joining = false;
 UINT8 hu_resynching = 0;
 
 #ifdef SATURNSYNCH
@@ -4877,7 +4876,6 @@ static void HandleConnect(SINT8 node)
 				DEBFILE("send savegame\n");
 			}
 			SV_AddWaitingPlayers();
-			player_joining = true;
 		}
 #else
 #ifndef NONET
@@ -5838,8 +5836,6 @@ static void GetPackets(void)
 {
 	SINT8 node; // The packet sender
 
-	player_joining = false;
-
 	while (HGetPacket())
 	{
 		node = (SINT8)doomcom->remotenode;
@@ -6487,21 +6483,12 @@ boolean TryRunTics(tic_t realtics)
 	}
 #endif
 
-	if (neededtic > gametic)
-	{
-		hu_stopped = false;
-	}
-
-	if (player_joining)
-	{
-		hu_stopped = true;
-		return false;
-	}
-
 	ticking = neededtic > gametic;
 
 	if (ticking)
 	{
+		hu_stopped = false;
+
 		// run the count * tics
 		while (neededtic > gametic)
 		{
