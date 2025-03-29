53aeda3c6b4a7861ffa9af24ce152d2faf36c491
Alug <alufoliehorst@gmail.com>
2025-03-29 18:00:01 +0100

    oop still need to update this

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -6627,7 +6627,7 @@ static void UpdatePingTable(void)
 	if (server)
 	{
 		//if (Playing() && !(gametime % 8)) // Value chosen based on _my vibes man_ << dont do this for v8 atleast, this is placeboeing ppl to hell and back
-		if (netgame && !(gametime % 35))	// update once per second.
+		if (Playing() && !(gametime % 35))	// update once per second.
 			PingUpdate();
 
 		fastest = 0;
