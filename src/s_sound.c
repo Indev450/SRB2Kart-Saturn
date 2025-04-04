721401f85a6b51448ce3a3034bf508aac68b8f01
Alug <alufoliehorst@gmail.com>
2025-04-04 18:13:31 +0200

    delet that egg

--- src/s_sound.c
+++ src/s_sound.c
@@ -509,9 +509,13 @@ void S_StartSoundAtVolume(const void *origin_p, sfxenum_t sfx_id, INT32 volume)
 	if (sfx->skinsound != -1 && origin && origin->skin)
 	{
 		// redirect player sound to the sound in the skin table
-		sfx_id = ((skin_t *)( (origin->localskin) ? origin->localskin : origin->skin ))->soundsid[sfx->skinsound];
+		sfx_id = ((skin_t *)(origin->localskin ? origin->localskin : origin->skin))->soundsid[sfx->skinsound];
 		sfx = &S_sfx[sfx_id];
 	}
+
+	// i seriously get an aneurism by this shitty egg
+	if (strncmp(sfx->name, "DNLT", 4) == 0)
+		return;
 
 	// Initialize sound parameters
 	pitch = NORM_PITCH;
