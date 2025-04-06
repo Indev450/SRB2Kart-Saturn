28703bc03867aa6363748b7f6235ae3b33c0b6eb
Alug <alufoliehorst@gmail.com>
2025-04-06 17:13:04 +0200

    whoopy copy paste

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -5715,9 +5715,6 @@ static void HWR_CheckForHorizonLines(void)
 			if (line->linedef->special != HORIZONSPECIAL)
 				continue;
 
-			if (R_PointOnSegSide(viewx, viewy, line) != 0)
-				continue;
-
 			gl_maphashorizonlines = true;
 			break;
 		}
