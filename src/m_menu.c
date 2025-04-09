92f08de1f6dd418df21104e7157792da59b09f15
Chearii <cherry.avium168@gmail.com>
2025-04-09 16:51:45 -0400

    Dial speedometer

--- src/m_menu.c
+++ src/m_menu.c
@@ -4925,7 +4925,7 @@ void M_Init(void)
 	}
 #endif
 
-	if (!xtra_speedo && !kartz_speedo && !achi_speedo) // why bother?
+	if (!xtra_speedo && !kartz_speedo && !achi_speedo && !dial_speedo) // why bother?
 		OP_SaturnHudMenu[sh_speedometer].status = IT_GRAYEDOUT;
 
 	//if (!xtra_speedo && kartz_speedo)
