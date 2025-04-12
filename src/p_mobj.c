c546b6225ce6051aa0e9b55970cafb564173333c
Alug <alufoliehorst@gmail.com>
2025-04-12 18:10:07 +0200

    move this since it does check for fuse

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -8802,13 +8802,15 @@ static void P_IceBlockFuseThink(mobj_t *mobj)
 
 static boolean P_FuseThink(mobj_t *mobj)
 {
-	if (((mobj->type == MT_RANDOMITEM && mobj->threshold == 69) || mobj->type == MT_EGGMANITEM || mobj->type == MT_FALLINGROCK) && mobj->fuse <= TICRATE)
-		mobj->flags2 ^= MF2_DONTDRAW;
-
 	mobj->fuse--;
 
 	if (mobj->fuse)
+	{
+		if (((mobj->type == MT_RANDOMITEM && mobj->threshold == 69) || mobj->type == MT_EGGMANITEM || mobj->type == MT_FALLINGROCK) && mobj->fuse <= TICRATE)
+			mobj->flags2 ^= MF2_DONTDRAW;
+
 		return true;
+	}
 
 	if (LUA_HookMobj(mobj, MOBJ_HOOK(MobjFuse)) || P_MobjWasRemoved(mobj))
 		;
