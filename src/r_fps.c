7eb311d99c9eb7fa022195a847863e2e016b0ec5
Alug <alufoliehorst@gmail.com>
2025-04-13 09:08:40 +0200

    we dont need all this stuff for precip

--- src/r_fps.c
+++ src/r_fps.c
@@ -364,11 +364,7 @@ void R_InterpolatePrecipMobjState(precipmobj_t *mobj, fixed_t frac, interpmobjst
 		out->z = mobj->z;
 		out->scale = cv_mobjscaleprecip.value ? mapobjectscale : FRACUNIT;
 		//out->subsector = mobj->subsector;
-		out->angle = mobj->angle;
-		out->spritexscale = mobj->spritexscale;
-		out->spriteyscale = mobj->spriteyscale;
-		out->spritexoffset = mobj->spritexoffset;
-		out->spriteyoffset = mobj->spriteyoffset;
+		//out->angle = mobj->angle;
 		return;
 	}
 
@@ -376,13 +372,8 @@ void R_InterpolatePrecipMobjState(precipmobj_t *mobj, fixed_t frac, interpmobjst
 		out->y = R_LerpFixed(mobj->old_y, mobj->y, frac);
 		out->z = R_LerpFixed(mobj->old_z, mobj->z, frac);
 		out->scale = cv_mobjscaleprecip.value ? mapobjectscale : FRACUNIT;
-		out->spritexscale = mobj->spritexscale;
-		out->spriteyscale = mobj->spriteyscale;
-		out->spritexoffset = mobj->spritexoffset;
-		out->spriteyoffset = mobj->spriteyoffset;
 		//out->subsector = R_PointInSubsector(out->x, out->y); // this is unused
-
-		out->angle = R_LerpAngle(mobj->old_angle, mobj->angle, frac);
+		//out->angle = R_LerpAngle(mobj->old_angle, mobj->angle, frac);
 }
 
 static void AddInterpolator(levelinterpolator_t* interpolator)
