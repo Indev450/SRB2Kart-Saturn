429453605578c41bbadd07f22ecd76430b74a54b
Alug <alufoliehorst@gmail.com>
2025-04-14 11:28:23 +0200

    delet erronous null checks
    idk where those even came from lel

--- src/w_wad.c
+++ src/w_wad.c
@@ -1128,9 +1128,6 @@ lumpnum_t W_CheckNumForName(const char *name)
 	INT32 i;
 	lumpnum_t check = INT16_MAX;
 
-	if (name == NULL)
-		return LUMPERROR;
-
 	if (!*name) // some doofus gave us an empty string?
 		return LUMPERROR;
 
@@ -1180,9 +1177,6 @@ lumpnum_t W_CheckNumForLongName(const char *name)
 {
 	INT32 i;
 	lumpnum_t check = INT16_MAX;
-
-	if (name == NULL)
-		return LUMPERROR;
 
 	if (!*name) // some doofus gave us an empty string?
 		return LUMPERROR;
