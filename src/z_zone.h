b0ba261e149a94e33fa3f29586f163d828dc008e
Alug <alufoliehorst@gmail.com>
2025-04-15 14:32:42 +0200

    change those tags
    also add PU_PATCH_DATA

--- src/z_zone.h
+++ src/z_zone.h
@@ -45,11 +45,12 @@ enum
 	PU_SOUND                 = 11, // static while playing
 	PU_MUSIC                 = 12, // static while playing
 
-	PU_PATCH                 = 13, // static entire execution time
-	PU_PATCH_LOWPRIORITY     = 14, // lower priority patch, static until level exited
-	PU_PATCH_ROTATED         = 15, // rotated patch, static until level exited or WAD added
-	PU_SPRITE                = 16, // sprite patch, static until WAD added
-	PU_HUDGFX                = 17, // HUD patch, static until WAD added
+	PU_PATCH                 = 14, // static entire execution time
+	PU_PATCH_LOWPRIORITY     = 15, // lower priority patch, static until level exited
+	PU_PATCH_ROTATED         = 16, // rotated patch, static until level exited or WAD added
+	PU_PATCH_DATA            = 17, // patch data, lifetime depends on the patch that owns it
+	PU_SPRITE                = 18, // sprite patch, static until WAD added
+	PU_HUDGFX                = 19, // HUD patch, static until WAD added
 
 	PU_HWRPATCHINFO          = 21, // Hardware GLPatch_t struct for OpenGL texture cache
 	PU_HWRPATCHCOLMIPMAP     = 22, // Hardware GLMipmap_t struct colormap variation of patch
