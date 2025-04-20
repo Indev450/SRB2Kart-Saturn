3a82442c86ac800056647fb5a2d13e871f4d49a1
Alug <alufoliehorst@gmail.com>
2025-04-20 23:19:15 +0200

    remove lagless camera from the menu
    and change cvar name to reset it for everyone

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -303,7 +303,7 @@ static void UseMouse_OnChange(void)
 consvar_t cv_mouseturn = {"mouseturn", "Off", CV_SAVE, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
 
 // Lagless camera! Yay!
-consvar_t cv_laglesscam = {"laglesscamera", "Off", CV_SAVE, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
+consvar_t cv_laglesscam = {"lagless_camera", "Off", CV_SAVE, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
 
 consvar_t cv_verticallook[MAXSPLITSCREENPLAYERS] = {
 	{"verticallook", "On", CV_SAVE, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL},
