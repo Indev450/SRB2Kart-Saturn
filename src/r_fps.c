63abd00c6a139548c652beb03bae540c372207a1
Alug <alufoliehorst@gmail.com>
2025-05-02 10:22:58 +0200

    that needs to come before old_ stuff gets reset

--- src/r_fps.c
+++ src/r_fps.c
@@ -780,6 +780,11 @@ void R_ResetMobjInterpolationState(mobj_t *mobj)
 	mobj->old_slopepitch2 = mobj->old_slopepitch;
 	mobj->old_sloperoll2 = mobj->old_sloperoll;
 
+	mobj->old_spritexscale2 = mobj->old_spritexscale;
+	mobj->old_spriteyscale2 = mobj->old_spriteyscale;
+	mobj->old_spritexoffset2 = mobj->old_spritexoffset;
+	mobj->old_spriteyoffset2 = mobj->old_spriteyoffset;
+
 	mobj->old_x = mobj->x;
 	mobj->old_y = mobj->y;
 	mobj->old_z = mobj->z;
@@ -799,11 +804,6 @@ void R_ResetMobjInterpolationState(mobj_t *mobj)
 	mobj->old_spriteyscale = mobj->spriteyscale;
 	mobj->old_spritexoffset = mobj->spritexoffset;
 	mobj->old_spriteyoffset = mobj->spriteyoffset;
-
-	mobj->old_spritexscale2 = mobj->old_spritexscale;
-	mobj->old_spriteyscale2 = mobj->old_spriteyscale;
-	mobj->old_spritexoffset2 = mobj->old_spritexoffset;
-	mobj->old_spriteyoffset2 = mobj->old_spriteyoffset;
 
 	if (mobj->player)
 	{
