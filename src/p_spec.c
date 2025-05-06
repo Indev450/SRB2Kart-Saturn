a69e03310e4707305a6cdcdeb75a1855a5fd5d79
Alug <alufoliehorst@gmail.com>
2025-05-06 19:35:10 +0200

    Revert "optimize MT_TUBEWAYPOINT"
    This reverts commit 3d0ac03f06b9027dc9467813e30a08b84c81158b.

--- src/p_spec.c
+++ src/p_spec.c
@@ -3737,7 +3737,9 @@ DoneSection2:
 				INT32 sequence;
 				fixed_t speed;
 				INT32 lineindex;
+				thinker_t *th;
 				mobj_t *waypoint = NULL;
+				mobj_t *mo2;
 				angle_t an;
 
 				if (player->mo->tracer && player->mo->tracer->type == MT_TUBEWAYPOINT)
@@ -3756,7 +3758,27 @@ DoneSection2:
 				speed = abs(lines[lineindex].dx)/8;
 				sequence = abs(lines[lineindex].dy)>>FRACBITS;
 
-				waypoint = P_GetFirstWaypoint(sequence);
+				// scan the thinkers
+				// to find the first waypoint
+				for (th = thinkercap.next; th != &thinkercap; th = th->next)
+				{
+					if (th->function.acp1 != (actionf_p1)P_MobjThinker)
+						continue;
+					if (th->function.acp1 == (actionf_p1)P_RemoveThinkerDelayed)
+						continue;
+
+					mo2 = (mobj_t *)th;
+
+					if (mo2->type != MT_TUBEWAYPOINT)
+						continue;
+					if (mo2->threshold != sequence)
+						continue;
+					if (mo2->health != 0)
+						continue;
+
+					waypoint = mo2;
+					break;
+				}
 
 				if (!waypoint)
 				{
@@ -3796,7 +3818,9 @@ DoneSection2:
 				INT32 sequence;
 				fixed_t speed;
 				INT32 lineindex;
+				thinker_t *th;
 				mobj_t *waypoint = NULL;
+				mobj_t *mo2;
 				angle_t an;
 
 				if (player->mo->tracer && player->mo->tracer->type == MT_TUBEWAYPOINT)
@@ -3815,7 +3839,27 @@ DoneSection2:
 				speed = -(abs(lines[lineindex].dx)/8); // Negative means reverse
 				sequence = abs(lines[lineindex].dy)>>FRACBITS;
 
-				waypoint = P_GetLastWaypoint(sequence);
+				// scan the thinkers
+				// to find the last waypoint
+				for (th = thinkercap.next; th != &thinkercap; th = th->next)
+				{
+					if (th->function.acp1 != (actionf_p1)P_MobjThinker)
+						continue;
+					if (th->function.acp1 == (actionf_p1)P_RemoveThinkerDelayed)
+						continue;
+
+					mo2 = (mobj_t *)th;
+
+					if (mo2->type != MT_TUBEWAYPOINT)
+						continue;
+					if (mo2->threshold != sequence)
+						continue;
+
+					if (!waypoint)
+						waypoint = mo2;
+					else if (mo2->health > waypoint->health)
+						waypoint = mo2;
+				}
 
 				if (!waypoint)
 				{
@@ -3966,12 +4010,15 @@ DoneSection2:
 				INT32 sequence;
 				fixed_t speed;
 				INT32 lineindex;
+				thinker_t *th;
 				mobj_t *waypointmid = NULL;
 				mobj_t *waypointhigh = NULL;
 				mobj_t *waypointlow = NULL;
+				mobj_t *mo2;
 				mobj_t *closest = NULL;
 				line_t junk;
 				vertex_t v1, v2, resulthigh, resultlow;
+				mobj_t *highest = NULL;
 
 				if (player->mo->tracer && player->mo->tracer->type == MT_TUBEWAYPOINT)
 					break;
@@ -4011,16 +4058,104 @@ DoneSection2:
 				// Determine the closest spot on the line between the three waypoints
 				// Put player at that location.
 
-				waypointmid = P_GetClosestWaypoint(sequence, player->mo);
+				// scan the thinkers
+				// to find the first waypoint
+				for (th = thinkercap.next; th != &thinkercap; th = th->next)
+				{
+					if (th->function.acp1 != (actionf_p1)P_MobjThinker)
+						continue;
+					if (th->function.acp1 == (actionf_p1)P_RemoveThinkerDelayed)
+						continue;
 
-				if (!waypointmid)
+					mo2 = (mobj_t *)th;
+
+					if (mo2->type != MT_TUBEWAYPOINT)
+						continue;
+
+					if (mo2->threshold != sequence)
+						continue;
+
+					if (!highest)
+						highest = mo2;
+					else if (mo2->health > highest->health) // Find the highest waypoint # in case we wrap
+						highest = mo2;
+
+					if (closest && P_AproxDistance(P_AproxDistance(player->mo->x-mo2->x, player->mo->y-mo2->y),
+						player->mo->z-mo2->z) > P_AproxDistance(P_AproxDistance(player->mo->x-closest->x,
+						player->mo->y-closest->y), player->mo->z-closest->z))
+						continue;
+
+					// Found a target
+					closest = mo2;
+				}
+
+				waypointmid = closest;
+
+				closest = NULL;
+
+				if (waypointmid == NULL)
 				{
 					CONS_Debug(DBG_GAMELOGIC, "ERROR: WAYPOINT(S) IN SEQUENCE %d NOT FOUND.\n", sequence);
 					break;
 				}
 
-				waypointlow = P_GetPreviousWaypoint(waypointmid, true);
-				waypointhigh = P_GetNextWaypoint(waypointmid, true);
+				// Find waypoint before this one (waypointlow)
+				for (th = thinkercap.next; th != &thinkercap; th = th->next)
+				{
+					if (th->function.acp1 != (actionf_p1)P_MobjThinker)
+						continue;
+					if (th->function.acp1 == (actionf_p1)P_RemoveThinkerDelayed)
+						continue;
+
+					mo2 = (mobj_t *)th;
+
+					if (mo2->type != MT_TUBEWAYPOINT)
+						continue;
+
+					if (mo2->threshold != sequence)
+						continue;
+
+					if (waypointmid->health == 0)
+					{
+						if (mo2->health != highest->health)
+							continue;
+					}
+					else if (mo2->health != waypointmid->health - 1)
+						continue;
+
+					// Found a target
+					waypointlow = mo2;
+					break;
+				}
+
+				// Find waypoint after this one (waypointhigh)
+				for (th = thinkercap.next; th != &thinkercap; th = th->next)
+				{
+					if (th->function.acp1 != (actionf_p1)P_MobjThinker)
+						continue;
+					if (th->function.acp1 == (actionf_p1)P_RemoveThinkerDelayed)
+						continue;
+
+					mo2 = (mobj_t *)th;
+
+					if (mo2->type != MT_TUBEWAYPOINT)
+						continue;
+
+					if (mo2->threshold != sequence)
+						continue;
+
+					if (waypointmid->health == highest->health)
+					{
+						if (mo2->health != 0)
+							continue;
+					}
+					else if (mo2->health != waypointmid->health + 1)
+						continue;
+
+					// Found a target
+					waypointhigh = mo2;
+					break;
+				}
 
 				CONS_Debug(DBG_GAMELOGIC, "WaypointMid: %d; WaypointLow: %d; WaypointHigh: %d\n",
 								waypointmid->health, waypointlow ? waypointlow->health : -1, waypointhigh ? waypointhigh->health : -1);
@@ -4071,7 +4206,6 @@ DoneSection2:
 
 				if (lines[lineindex].flags & ML_EFFECT1) // Don't wrap
 				{
-					mobj_t *highest = P_GetLastWaypoint(sequence);
 					highest->flags |= MF_SLIDEME;
 				}
 
@@ -4083,7 +4217,7 @@ DoneSection2:
 					player->mo->y = resulthigh.y;
 					player->mo->z = resulthigh.z - P_GetPlayerHeight(player);
 				}
-				else if ((lines[lineindex].flags & ML_EFFECT1) && waypointmid->health == numwaypoints[sequence] - 1)
+				else if ((lines[lineindex].flags & ML_EFFECT1) && waypointmid->health == highest->health)
 				{
 					closest = waypointmid;
 					player->mo->x = resultlow.x;
