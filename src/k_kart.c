6653b5ab6c077132e9d1863a069182199f21922f
Alug <alufoliehorst@gmail.com>
2025-05-10 18:56:24 +0200

    Revert "skinlocal deez nuts"
    This reverts commit 12ec3d11a2d9acd62193d5fafa169527fa6e7f01.

--- src/k_kart.c
+++ src/k_kart.c
@@ -10980,6 +10980,7 @@ static void K_drawKartMinimapHead(mobj_t *mo, INT32 x, INT32 y, INT32 flags)
 
 	const skin_t *skin;
 	player_t *player = mo->player;
+	const boolean skinlocal = mo->skinlocal;
 
 	fixed_t amnumxpos, amnumypos;
 	INT32 amxpos, amypos, wntdamxpos, wntdamypos;
@@ -10992,7 +10993,7 @@ static void K_drawKartMinimapHead(mobj_t *mo, INT32 x, INT32 y, INT32 flags)
 #endif
 
 	skin = K_GetMobjSkin(mo);
-	minimaphead = R_GetSkinFaceMini(player);
+	minimaphead = (skinlocal ? localfacemmapprefix : facemmapprefix)[K_GetMobjSkinNum(skin, skinlocal)];
 
 	amnumxpos = (FixedMul(lerp(mo->old_x, mo->x), minimapinfo.zoom) - minimapinfo.offs_x);
 	amnumypos = -(FixedMul(lerp(mo->old_y, mo->y), minimapinfo.zoom) - minimapinfo.offs_y);
@@ -11042,7 +11043,7 @@ static void K_drawKartMinimapHead(mobj_t *mo, INT32 x, INT32 y, INT32 flags)
 		if (mo->colorized)
 			colormap = R_GetTranslationColormap(TC_RAINBOW, mo->color, GTC_CACHE);
 		else
-			colormap = R_GetLocalTranslationColormap(mo->skin, mo->localskin, mo->color, GTC_CACHE, mo->skinlocal);
+			colormap = R_GetLocalTranslationColormap(mo->skin, mo->localskin, mo->color, GTC_CACHE, skinlocal);
 
 		V_DrawFixedPatch(amxpos, amypos, scale, flags, minimaphead, colormap);
 
--- src/p_enemy.c
+++ src/p_enemy.c
@@ -3858,7 +3858,10 @@ void A_SignPlayer(void *thing)
 	if (LUA_CallAction(A_SIGNPLAYER, actor))
 		return;
 
-	if (!actor->target || !actor->target->player)
+	if (!actor->target)
+		return;
+
+	if (!actor->target->player)
 		return;
 
 	// Set the sign to be an appropriate background color for this player's skincolor.
@@ -3873,9 +3876,11 @@ void A_SignPlayer(void *thing)
 
 	// needs - 1 or else it pukes an error out
 	// same thing happens on p_mobj.c
+	if (actor->target->skinlocal)
+		ov->localskin = &localskins[actor->target->player->localskin - 1];
+	else if (actor->target->player->localskin)
+		ov->localskin = &skins[actor->target->player->localskin - 1];
 	ov->skinlocal = actor->target->skinlocal;
-	if (ov->skinlocal || actor->target->player->localskin)
-		ov->localskin = &K_GetSkinArray(ov->skinlocal)[actor->target->player->localskin - 1];
 
 	P_SetMobjState(ov, actor->info->seestate); // S_PLAY_SIGN
 }
