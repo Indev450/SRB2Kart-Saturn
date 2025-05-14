0f0195d35ba1d8f50787dd747eee90553f96c9d0
Alug <alufoliehorst@gmail.com>
2025-05-14 02:01:02 +0200

    Make votescreen backgrounds resolution and dup independant
    also some slight cleanup
    currently this scales vertically to fill out the screen vertically
    not sure if horizontally would be preferred, but thatd most likely cut off alot of parts from some votebgs

--- src/v_video.h
+++ src/v_video.h
@@ -282,6 +282,7 @@ typedef struct player_s player_t;
 void V_DoPostProcessor(INT32 view, INT32 param);
 
 void V_DrawPatchFill(patch_t *pat);
+void V_DrawCenteredScaledFullScreenPatch(patch_t *patch);
 
 void VID_BlitLinearScreen(const UINT8 *srcptr, UINT8 *destptr, INT32 width, INT32 height, size_t srcrowbytes,
 	size_t destrowbytes);
