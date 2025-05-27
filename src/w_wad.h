3c62ff266c41216bb0a4b9e4cb3806cbcf31eba6
Alug <alufoliehorst@gmail.com>
2025-05-27 22:43:53 +0200

    Reapply "Cache start and end locations for WAD folders"
    This reverts commit 30d64ad538fb97657829d6e03b1c9b99e5c15671.

--- src/w_wad.h
+++ src/w_wad.h
@@ -18,6 +18,8 @@
 #include "hardware/hw_data.h"
 #endif
 
+#include "m_aatree.h"
+
 #ifdef __GNUG__
 #pragma interface
 #endif
@@ -138,6 +140,8 @@ typedef struct wadfile_s
 #ifdef ROTSPRITE
 	lumpcache_t *rotcache; // Cache rotsprites for rotating patches.
 #endif
+	aatree_t *startfolders;
+	aatree_t *endfolders;
 	UINT16 numlumps; // this wad's number of resources
 	FILE *handle;
 	UINT32 filesize; // for network
