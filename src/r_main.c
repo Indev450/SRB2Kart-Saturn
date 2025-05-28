72fa7daacd577a88b3678fbde909ef9cfb404bcf
Alug <alufoliehorst@gmail.com>
2025-05-28 16:26:26 +0200

    check for 32bit with __SIZEOF_POINTER__ for precip drawdistance
    thx @chromaticpipe for the tip!

--- src/r_main.c
+++ src/r_main.c
@@ -125,16 +125,17 @@ static CV_PossibleValue_t drawdist_cons_t[] = {
 	{8192, "8192"},	{12288, "12288"}, {20480, "20480"},
 	{24576, "24576"},{0, "Infinite"},	{0, NULL}};
 
-#if defined(__x86_64__) || defined(__amd64__) || defined(__aarch64__) || defined(__arm64__) // only for 64bit (idk how else to proper check lmao)
+#if __SIZEOF_POINTER__ < 8 // only for 64bit
 static CV_PossibleValue_t drawdist_precip_cons_t[] = {
-	{256, "256"}, {512, "512"}, {768, "768"},
+	{256, "256"},   {512, "512"},   {768, "768"},
 	{1024, "1024"}, {1536, "1536"}, {2048, "2048"},
-	{3072, "3072"}, {4096, "4096"}, {0, "None"}, {0, NULL}};
+	{0, "None"},    {0, NULL}};
+
 #else
 static CV_PossibleValue_t drawdist_precip_cons_t[] = {
-	{256, "256"},	{512, "512"},	{768, "768"},
-	{1024, "1024"},	{1536, "1536"},	{2048, "2048"},
-	{0, "None"},	{0, NULL}};
+	{256, "256"},   {512, "512"},   {768, "768"},
+	{1024, "1024"}, {1536, "1536"}, {2048, "2048"},
+	{3072, "3072"}, {4096, "4096"}, {0, "None"}, {0, NULL}};
 #endif
 
 static CV_PossibleValue_t maxinterpdist_cons_t[] = {
