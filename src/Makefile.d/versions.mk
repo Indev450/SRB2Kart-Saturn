8c316827230fa92530c4eac743449f62f6603881
Alug <alufoliehorst@gmail.com>
2025-05-28 15:43:21 +0200

    smol adjustments for main

--- src/Makefile.d/versions.mk
+++ src/Makefile.d/versions.mk
@@ -17,6 +17,10 @@ $(foreach v,$(join $(wordlist 2,$(_n),- $(gcc_versions)),\
 	$(eval $(call _predecessor,$(subst =, ,$(v))))))
 
 # -W -Wno-unused
+
+ifdef GCC150
+ opts+=-std=gnu17
+endif
 WFLAGS:=-Wall -Wno-trigraphs
 ifndef GCC295
 #WFLAGS+=-Wno-packed
