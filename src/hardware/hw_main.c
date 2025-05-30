0e9a825f63d989309ef75a540531259d86ae942e
Alug <alufoliehorst@gmail.com>
2025-05-30 20:52:38 +0200

    some crap from ceepeepeecheck

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4464,7 +4464,10 @@ static void HWR_DrawModels(void)
 			continue;
 		}
 
-		if (spr->mobj && spr->mobj->skin && spr->mobj->sprite == SPR_PLAY)
+		if (!spr->mobj)
+			continue;
+
+		if (spr->mobj->skin && spr->mobj->sprite == SPR_PLAY)
 		{
 			md2_t *md2;
 
--- src/r_data.c
+++ src/r_data.c
@@ -690,12 +690,15 @@ static texpatch_t *R_ParsePatch(boolean actuallyLoadPatch)
 
 	// Patch identifier
 	texturesToken = M_GetToken(NULL);
+
 	if (texturesToken == NULL)
 	{
 		I_Error("Error parsing TEXTURES lump: Unexpected end of file where patch name should be");
 	}
+
 	texturesTokenLength = strlen(texturesToken);
-	if (texturesTokenLength>8)
+
+	if (texturesTokenLength > 8)
 	{
 		I_Error("Error parsing TEXTURES lump: Patch name \"%s\" exceeds 8 characters",texturesToken);
 	}
@@ -713,10 +716,12 @@ static texpatch_t *R_ParsePatch(boolean actuallyLoadPatch)
 	// Comma 1
 	Z_Free(texturesToken);
 	texturesToken = M_GetToken(NULL);
+
 	if (texturesToken == NULL)
 	{
 		I_Error("Error parsing TEXTURES lump: Unexpected end of file where comma after \"%s\"'s patch name should be",patchName);
 	}
+
 	if (strcmp(texturesToken,",")!=0)
 	{
 		I_Error("Error parsing TEXTURES lump: Expected \",\" after %s's patch name, got \"%s\"",patchName,texturesToken);
@@ -725,14 +730,17 @@ static texpatch_t *R_ParsePatch(boolean actuallyLoadPatch)
 	// XPos
 	Z_Free(texturesToken);
 	texturesToken = M_GetToken(NULL);
+
 	if (texturesToken == NULL)
 	{
 		I_Error("Error parsing TEXTURES lump: Unexpected end of file where patch \"%s\"'s x coordinate should be",patchName);
 	}
+
 	endPos = NULL;
 #ifndef AVOID_ERRNO
 	errno = 0;
 #endif
+
 	patchXPos = strtol(texturesToken,&endPos,10);
 	(void)patchXPos; //unused for now
 	if (endPos == texturesToken // Empty string
@@ -748,10 +756,12 @@ static texpatch_t *R_ParsePatch(boolean actuallyLoadPatch)
 	// Comma 2
 	Z_Free(texturesToken);
 	texturesToken = M_GetToken(NULL);
+
 	if (texturesToken == NULL)
 	{
 		I_Error("Error parsing TEXTURES lump: Unexpected end of file where comma after patch \"%s\"'s x coordinate should be",patchName);
 	}
+
 	if (strcmp(texturesToken,",")!=0)
 	{
 		I_Error("Error parsing TEXTURES lump: Expected \",\" after patch \"%s\"'s x coordinate, got \"%s\"",patchName,texturesToken);
@@ -760,6 +770,7 @@ static texpatch_t *R_ParsePatch(boolean actuallyLoadPatch)
 	// YPos
 	Z_Free(texturesToken);
 	texturesToken = M_GetToken(NULL);
+
 	if (texturesToken == NULL)
 	{
 		I_Error("Error parsing TEXTURES lump: Unexpected end of file where patch \"%s\"'s y coordinate should be",patchName);
@@ -1131,9 +1142,9 @@ static void R_InitExtraColormaps(void)
 lumpnum_t R_GetFlatNumForName(const char *name)
 {
 	INT32 i;
-	lumpnum_t lump;
-	lumpnum_t start;
-	lumpnum_t end;
+	lumpnum_t lump = LUMPERROR;
+	lumpnum_t start = LUMPERROR;
+	lumpnum_t end = LUMPERROR;
 
 	// Scan wad files backwards so patched flats take preference.
 	for (i = numwadfiles - 1; i >= 0; i--)
@@ -1608,7 +1619,7 @@ const char *R_ColormapNameForNum(INT32 num)
 	if (num == -1)
 		return "NONE";
 
-	if (num < 0 || num > MAXCOLORMAPS)
+	if (num < 0 || num >= MAXCOLORMAPS)
 		I_Error("R_ColormapNameForNum: num %d is invalid!\n", num);
 
 	if (foundcolormaps[num] == LUMPERROR)
