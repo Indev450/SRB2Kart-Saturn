b29ca13916b4fe9ec5934f58a32925198e8af283
Indev <bdmitry450@gmail.com>
2025-05-31 18:54:07 +0300

    Add musicdefs table into lua

--- src/lua_infolib.c
+++ src/lua_infolib.c
@@ -1103,6 +1103,33 @@ static int lib_sFindMusicCredit(lua_State *L)
 	return 1;
 }
 
+static int lib_getMusicDef(lua_State *L)
+{
+	musicdef_t *def = NULL;
+
+	if (lua_isnumber(L, 2))
+		def = S_GetMusicCredit(lua_tonumber(L, 2));
+	else if (lua_isstring(L, 2))
+		def = S_FindMusicCredit(lua_tostring(L, 2));
+	else
+		return luaL_error(L, "musicdefs index must be number or string (got %s)", luaL_typename(L, 2));
+
+	LUA_PushUserdata(L, def, META_MUSICDEF);
+
+	return 1;
+}
+
+static int lib_setMusicDef(lua_State *L)
+{
+	return luaL_error(L, "musicdefs is read only");
+}
+
+static int lib_musicdefslen(lua_State *L)
+{
+	lua_pushinteger(L, nummusicdefs);
+	return 1;
+}
+
 static int musicdef_get(lua_State *L)
 {
 	musicdef_t *musicdef = *((musicdef_t **)luaL_checkudata(L, 1, META_MUSICDEF));
@@ -1137,6 +1164,16 @@ static int musicdef_get(lua_State *L)
 		return luaL_error(L, LUA_QL("musicdef_t") " has no field named " LUA_QS, lua_tostring(L, 2));
 	}
 	return 0;
+}
+
+static int musicdef_num(lua_State *L)
+{
+	musicdef_t *musicdef = *((musicdef_t **)luaL_checkudata(L, 1, META_MUSICDEF));
+
+	I_Assert(musicdef != NULL);
+
+	lua_pushinteger(L, musicdef->num);
+	return 1;
 }
 
 //////////////////////////////
@@ -1196,6 +1233,9 @@ int LUA_InfoLib(lua_State *L)
 	luaL_newmetatable(L, META_MUSICDEF);
 		lua_pushcfunction(L, musicdef_get);
 		lua_setfield(L, -2, "__index");
+
+		lua_pushcfunction(L, musicdef_num);
+		lua_setfield(L, -2, "__len");
 	lua_pop(L, 1);
 
 	musicdef_fields_ref = Lua_CreateFieldTable(L, musicdef_opt);
@@ -1252,5 +1292,19 @@ int LUA_InfoLib(lua_State *L)
 	lua_pushvalue(L, -1);
 	lua_setglobal(L, "S_sfx");
 	lua_setglobal(L, "sfxinfo");
+
+	lua_newuserdata(L, 0);
+		lua_createtable(L, 0, 2);
+			lua_pushcfunction(L, lib_getMusicDef);
+			lua_setfield(L, -2, "__index");
+
+			lua_pushcfunction(L, lib_setMusicDef);
+			lua_setfield(L, -2, "__newindex");
+
+			lua_pushcfunction(L, lib_musicdefslen);
+			lua_setfield(L, -2, "__len");
+		lua_setmetatable(L, -2);
+	lua_setglobal(L, "musicdefs");
+
 	return 0;
 }
