796204d376fa5baf0e4eb81f745be382ac519a81
Alug <alufoliehorst@gmail.com>
2025-05-31 17:20:26 +0200

    use memcmp during startup map check
    prob not faster just looks a bit better tbh

--- src/d_main.c
+++ src/d_main.c
@@ -1809,7 +1809,7 @@ void D_SRB2Main(void)
 		{
 			name = lumpinfo->name;
 
-			if (name[0] == 'M' && name[1] == 'A' && name[2] == 'P') // Ignore the headers
+			if (memcmp(name, "MAP", 3) == 0) // Ignore the headers
 			{
 				INT16 num;
 				if (name[5] != '\0')
@@ -1839,7 +1839,7 @@ void D_SRB2Main(void)
 		{
 			name = lumpinfo->name;
 
-			if (name[0] == 'M' && name[1] == 'A' && name[2] == 'P') // Ignore the headers
+			if (memcmp(name, "MAP", 3) == 0) // Ignore the headers
 			{
 				INT16 num;
 				if (name[5] != '\0')
