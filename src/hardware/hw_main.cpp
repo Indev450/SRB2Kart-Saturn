9ee5f7958d6f37cbf1e659092828a78da3aeda6a
Alug <alufoliehorst@gmail.com>
2025-07-01 21:11:40 +0200

    tiny optimizations hw_main again

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -771,10 +771,10 @@ static void HWR_RenderPlane(subsector_t *subsector, extrasubsector_t *xsub, bool
 		UINT8 numplanes, j;
 		vertex_t v; // For determining the closest distance from the line to the camera, to split render planes for minimum distortion;
 
-		const float renderdist = 27000.0f; // How far out to properly render the plane
-		const float farrenderdist = 32768.0f; // From here, raise plane to horizon level to fill in the line with some texture distortion
+		static constexpr float renderdist    = 27000.0f; // How far out to properly render the plane
+		static constexpr float farrenderdist = 32768.0f; // From here, raise plane to horizon level to fill in the line with some texture distortion
 
-		seg_t *line = &segs[subsector->firstline];
+		const seg_t *line = &segs[subsector->firstline];
 
 		for (i = 0; i < subsector->numlines; i++, line++)
 		{
@@ -1116,12 +1116,12 @@ static void HWR_SplitWall(sector_t *sector, FOutVector *wallVerts, INT32 texnum,
 		else
 			solid = false;
 
-		height = FixedToFloat(P_GetLightZAt(&list[i], v1x, v1y));
+		height    = FixedToFloat(P_GetLightZAt(&list[i], v1x, v1y));
 		endheight = FixedToFloat(P_GetLightZAt(&list[i], v2x, v2y));
 
 		if (solid)
 		{
-			bheight = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v1x, v1y));
+			bheight    = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v1x, v1y));
 			endbheight = FixedToFloat(P_GetFFloorBottomZAt(list[i].caster, v2x, v2y));
 		}
 
@@ -1129,6 +1129,7 @@ static void HWR_SplitWall(sector_t *sector, FOutVector *wallVerts, INT32 texnum,
 		{
 			if (solid && top > bheight)
 				top = bheight;
+
 			if (solid && endtop > endbheight)
 				endtop = endbheight;
 		}
@@ -1144,15 +1145,12 @@ static void HWR_SplitWall(sector_t *sector, FOutVector *wallVerts, INT32 texnum,
 			endbheight = endrealbot;
 		}
 
-		if (endbheight >= endtop)
-			continue;
-
-		if (bheight >= top)
+		if ((endbheight >= endtop) || (bheight >= top))
 			continue;
 
 		// Found a break
 		// The heights are clamped to ensure the polygon doesn't cross itself.
-		bot = CLAMP(bheight, realbot, top);
+		bot    = CLAMP(bheight, realbot, top);
 		endbot = CLAMP(endbheight, endrealbot, endtop);
 
 		Surf->PolyColor.s.alpha = alpha;
@@ -1181,6 +1179,7 @@ static void HWR_SplitWall(sector_t *sector, FOutVector *wallVerts, INT32 texnum,
 
 	bot = realbot;
 	endbot = endrealbot;
+
 	if ((endtop <= endrealbot) && (top <= realbot))
 		return;
 
@@ -1421,7 +1420,7 @@ void HWR_ProcessSeg(void) // Sort of like GLWall::Process in GZDoom
 
 	// x offset the texture
 	fixed_t texturehpeg = gl_sidedef->textureoffset + gl_curline->offset;
-	float cliplow = (float)texturehpeg;
+	float cliplow  = (float)texturehpeg;
 	float cliphigh = (float)(texturehpeg + (gl_curline->flength*FRACUNIT));
 
 	FUINT lightnum = gl_frontsector->lightlevel;
@@ -2977,7 +2976,7 @@ static void HWR_Subsector(size_t num)
 				if (sub->validcount == validcount)
 					continue;
 
-				sector_t *controlSec = &sectors[rover->secnum];
+				const sector_t *controlSec = &sectors[rover->secnum];
 
 				if (!controlSec->moved)
 					continue;
@@ -3347,7 +3346,7 @@ static gl_vissprite_t *HWR_NewVisSprite(void)
 }
 
 // Finds a floor through which light does not pass.
-/*static fixed_t HWR_OpaqueFloorAtPos(fixed_t x, fixed_t y, fixed_t z, fixed_t height)
+static fixed_t HWR_OpaqueFloorAtPos(fixed_t x, fixed_t y, fixed_t z, fixed_t height)
 {
 	const sector_t *sec = R_PointInSubsector(x, y)->sector;
 	fixed_t floorz = sec->floorheight;
@@ -3548,7 +3547,7 @@ static void HWR_DrawSpriteShadow(gl_vissprite_t *spr, patch_t *gpatch, GLPatch_t
 		sSurf.PolyColor.s.alpha = (UINT8)(sSurf.PolyColor.s.alpha - floorheight/4);
 		HWR_ProcessPolygon(&sSurf, swallVerts, 4, blendmode|PF_Translucent|PF_Modulated, SHADER_NONE, false);
 	}
-}*/
+}
 
 // This is expecting a pointer to an array containing 4 wallVerts for a sprite
 static void HWR_RotateSpritePolyToAim(gl_vissprite_t *spr, FOutVector *wallVerts, const boolean precip, const boolean papersprite)
@@ -3670,16 +3669,16 @@ static void HWR_SplitSprite(gl_vissprite_t *spr, const boolean papersprite)
 	hwrpatch = ((GLPatch_t *)gpatch->hardware);
 
 	// Draw shadow BEFORE sprite
-	/*if (cv_shadow.value // Shadows enabled
+	if (UNLIKELY(cv_shadow.value // Shadows enabled
 		&& (spr->mobj->flags & (MF_SCENERY|MF_SPAWNCEILING|MF_NOGRAVITY)) != (MF_SCENERY|MF_SPAWNCEILING|MF_NOGRAVITY) // Ceiling scenery have no shadow.
 		&& !(spr->mobj->flags2 & MF2_DEBRIS) // Debris have no corona or shadow.
-		&& (spr->mobj->z >= spr->mobj->floorz)) // Without this, your shadow shows on the floor, even after you die and fall through the ground.
+		&& (spr->mobj->z >= spr->mobj->floorz))) // Without this, your shadow shows on the floor, even after you die and fall through the ground.
 	{
 		////////////////////
 		// SHADOW SPRITE! //
 		////////////////////
 		HWR_DrawSpriteShadow(spr, gpatch, hwrpatch);
-	}*/
+	}
 
 	baseWallVerts[0].x = baseWallVerts[3].x = spr->x1;
 	baseWallVerts[2].x = baseWallVerts[1].x = spr->x2;
@@ -3784,7 +3783,7 @@ static void HWR_SplitSprite(gl_vissprite_t *spr, const boolean papersprite)
 
 	for (i = 1; i < sector->numlights; i++)
 	{
-		fixed_t h = P_GetLightZAt(&sector->lightlist[i], spr->mobj->x, spr->mobj->y);
+		const fixed_t h = P_GetLightZAt(&sector->lightlist[i], spr->mobj->x, spr->mobj->y);
 
 		if (!(h <= temp))
 			continue;
@@ -3829,10 +3828,7 @@ static void HWR_SplitSprite(gl_vissprite_t *spr, const boolean papersprite)
 			endbheight = endrealbot;
 		}
 
-		if (endbheight >= endtop)
-			continue;
-
-		if (bheight >= top)
+		if ((endbheight >= endtop) || (bheight >= top))
 			continue;
 
 		// Found a break
@@ -3991,16 +3987,16 @@ static void HWR_DrawSprite(gl_vissprite_t *spr)
 	}
 
 	// Draw shadow BEFORE sprite
-	/*if (cv_shadow.value // Shadows enabled
+	if (UNLIKELY(cv_shadow.value // Shadows enabled
 		&& (spr->mobj->flags & (MF_SCENERY|MF_SPAWNCEILING|MF_NOGRAVITY)) != (MF_SCENERY|MF_SPAWNCEILING|MF_NOGRAVITY) // Ceiling scenery have no shadow.
 		&& !(spr->mobj->flags2 & MF2_DEBRIS) // Debris have no corona or shadow.
-		&& (spr->mobj->z >= spr->mobj->floorz)) // Without this, your shadow shows on the floor, even after you die and fall through the ground.
+		&& (spr->mobj->z >= spr->mobj->floorz))) // Without this, your shadow shows on the floor, even after you die and fall through the ground.
 	{
 		////////////////////
 		// SHADOW SPRITE! //
 		////////////////////
 		HWR_DrawSpriteShadow(spr, gpatch, hwrpatch);
-	}*/
+	}
 
 	// push it toward the camera to mitigate floor-clipping sprites
 	HWR_ApplyDispoffset(spr, wallVerts, papersprite);
@@ -4616,7 +4612,7 @@ static void HWR_AddPrecipitationSprites(void)
 	drawdist = ((fixed_t)(cv_drawdist_precip.value) * (cv_mobjscaleprecip.value ? mapobjectscale : FRACUNIT));
 
 	// No to infinite precipitation draw distance.
-	if (drawdist == 0)
+	if (cv_drawdist_precip.value == 0)
 	{
 		return;
 	}
@@ -5480,10 +5476,11 @@ namespace
 	};
 
 template <RenderViewpointType Type>
-void HWR_RenderViewpoint(gl_portal_t *rootportal, const float fpov, player_t *player, int stencil_level, boolean allow_portals)
+void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_level, boolean allow_portals)
 {
 	gl_portallist_t portallist;
 
+	const float fpov = FixedToFloat(R_GetPlayerFov(player));
 	const boolean skybox = (skyboxmo[0] && cv_skybox.value);
 
 	portallist.base = portallist.cap = NULL;
@@ -5625,8 +5622,8 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, const float fpov, player_t *pl
 };
 
 extern "C" {
-	void HWR_RenderPortalViewpoint(gl_portal_t *rootportal, const float fpov, player_t *player, int stencil_level, boolean allow_portals) {
-		HWR_RenderViewpoint<RenderViewpointType::kPortal>(rootportal, fpov, player, stencil_level, allow_portals);
+	void HWR_RenderPortalViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_level, boolean allow_portals) {
+		HWR_RenderViewpoint<RenderViewpointType::kPortal>(rootportal, player, stencil_level, allow_portals);
 	}
 }
 
@@ -5635,8 +5632,6 @@ extern "C" {
 // ==========================================================================
 static void HWR_RenderFrame(player_t *player, boolean skybox)
 {
-	const float fpov = FixedToFloat(R_GetPlayerFov(player));
-
 	// set window position
 	gl_viewwindowx = gl_baseviewwindowx;
 	gl_viewwindowy = gl_baseviewwindowy;
@@ -5676,9 +5671,9 @@ static void HWR_RenderFrame(player_t *player, boolean skybox)
 
 	portalclipline = NULL;
 	if (UNLIKELY(HWR_UsePortals()))
-		HWR_RenderViewpoint<RenderViewpointType::kPortal>(NULL, fpov, player, 0, !skybox);
+		HWR_RenderViewpoint<RenderViewpointType::kPortal>(NULL, player, 0, !skybox);
 	else
-		HWR_RenderViewpoint<RenderViewpointType::kNormal>(NULL, fpov, player, 0, !skybox);
+		HWR_RenderViewpoint<RenderViewpointType::kNormal>(NULL, player, 0, !skybox);
 
 	// Unset transform and shader
 	GL_SetTransform(NULL);
--- src/hardware/hw_main.h
+++ src/hardware/hw_main.h
@@ -114,8 +114,8 @@ void HWR_SetViewSize(void);
 void HWR_AddCommands(void);
 
 void HWR_RenderPlayerView(void);
-void HWR_RenderViewpoint(gl_portal_t *rootportal, const float fpov, player_t *player, int stencil_level, boolean allow_portals);
-void HWR_RenderPortalViewpoint(gl_portal_t *rootportal, const float fpov, player_t *player, int stencil_level, boolean allow_portals);
+void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_level, boolean allow_portals);
+void HWR_RenderPortalViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_level, boolean allow_portals);
 
 void HWR_ClearSkyDome(void);
 void HWR_BuildSkyDome(void);
