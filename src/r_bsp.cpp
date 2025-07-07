0b9b9c22d01853245080b506dcfbc041242a7060
GenericHeroGuy <genericheroguy@noreply.codeberg.org>
2025-07-07 23:19:30 +0200

    Fix fake floors in Donut Plains disappearing when chasecam is off
    Also remove the useless R_PointInSubsector calls

--- src/r_bsp.cpp
+++ src/r_bsp.cpp
@@ -198,10 +198,10 @@ sector_t *R_FakeFlat(sector_t *sec, sector_t *tempsec, INT32 *floorlightlevel,
 		boolean underwater;
 		UINT8 i = R_GetViewNumber();
 
-		if (viewplayer == &players[displayplayers[i]] && camera[i].chase)
-			heightsec = R_PointInSubsector(camera[i].x, camera[i].y)->sector->heightsec;
-		else if (i > splitscreen && viewmobj)
-			heightsec = R_PointInSubsector(pviewmobj->x, pviewmobj->y)->sector->heightsec;
+		if (viewplayer == &players[displayplayers[i]] && camera[R_GetViewNumber()].chase)
+			heightsec = camera->subsector->sector->heightsec;
+		else if (viewmobj)
+			heightsec = pviewmobj->subsector->sector->heightsec;
 		else
 			return sec;
 
