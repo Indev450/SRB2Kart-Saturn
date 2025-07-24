b2d1771e795d78696aede0b68106e2a797176875
Alug <alufoliehorst@gmail.com>
2025-07-24 14:50:12 +0200

    dont free our poly_subsectors twice
    also use calloc for em

--- src/hardware/hw_glob.h
+++ src/hardware/hw_glob.h
@@ -55,9 +55,8 @@ void HWR_ObjectLightLevelPost(gl_vissprite_t *spr, const sector_t *sector, INT32
 extern poly_subsector_t *poly_subsectors;
 extern size_t num_poly_subsector;
 
-void HWR_InitPolyPool(void);
-void HWR_FreePolyPool(void);
-void HWR_FreeExtraSubsectors(void);
+void HWR_Init_PolyPool(void);
+void HWR_Free_PolyPool(void);
 
 // --------
 // hw_cache.c
