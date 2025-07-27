c5116dea1acd12dbcb9c8538dc19c9119f264dba
Gustaf Alhäll <gustaf@hanicef.me>
2025-07-27 20:06:19 +0200

    Add support for OpenBSD

--- src/sdl/i_system.cpp
+++ src/sdl/i_system.cpp
@@ -1398,10 +1398,14 @@ void I_Sleep(UINT32 ms)
 
 void I_SleepDuration(precise_t duration)
 {
-#if defined(__linux__) || defined(__FreeBSD__) || defined(__HAIKU__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__HAIKU__) || defined(__OpenBSD__)
 	UINT64 precision = I_GetPrecisePrecision();
 	precise_t dest = I_GetPreciseTime() + duration;
+#ifdef __OpenBSD__
+	precise_t slack = (precision / 50); // 20 ms slack
+#else
 	precise_t slack = (precision / 5000); // 0.2 ms slack
+#endif
 	if (duration > slack)
 	{
 		duration -= slack;
@@ -1414,7 +1418,11 @@ void I_SleepDuration(precise_t duration)
 #endif
 		};
 		int status;
+#ifdef __OpenBSD__
+		do status = nanosleep(&ts, &ts);
+#else
 		do status = clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, &ts);
+#endif
 		while (status == EINTR);
 	}
 
