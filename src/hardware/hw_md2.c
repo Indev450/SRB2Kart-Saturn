4a9f6b6a222d238f33eb0d89f387be5d030fdda3
Alug <alufoliehorst@gmail.com>
2025-07-29 00:24:13 +0200

    we dont need to clamp sprite lightlevels everywhere in ogl
    theyre an int32 due to the directional light system stuff and get clamped in HWR_Lighting as a result

--- src/hardware/hw_md2.c
+++ src/hardware/hw_md2.c
@@ -1150,7 +1150,7 @@ void HWR_DrawMD2(gl_vissprite_t *spr)
 			light = R_GetPlaneLight(sector, spr->mobj->z + spr->mobj->height, false); // Always use the light at the top instead of whatever I was doing before
 
 			if (!(spr->mobj->frame & FF_FULLBRIGHT))
-				lightlevel = *sector->lightlist[light].lightlevel > 255 ? 255 : *sector->lightlist[light].lightlevel;
+				lightlevel = *sector->lightlist[light].lightlevel;
 
 			if (sector->lightlist[light].extra_colormap)
 				colormap = sector->lightlist[light].extra_colormap;
@@ -1158,7 +1158,7 @@ void HWR_DrawMD2(gl_vissprite_t *spr)
 		else
 		{
 			if (!(spr->mobj->frame & FF_FULLBRIGHT))
-				lightlevel = sector->lightlevel > 255 ? 255 : sector->lightlevel;
+				lightlevel = sector->lightlevel;
 
 			if (sector->extra_colormap)
 				colormap = sector->extra_colormap;
