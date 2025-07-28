ff10b77414cf1e657120173ae81ac01981dd2865
chromaticpipe <chromaticpipe@gmail.com>
2025-07-28 14:00:26 -0500

    Fix OpenGL flash palettes ignoring saturation

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -6063,6 +6063,8 @@ static void HWR_DoPostProcessor(player_t *player)
 
 		Surf.PolyColor.s.alpha = 0xc0; // match software mode
 
+		V_CubeApply(&Surf.PolyColor.s.red, &Surf.PolyColor.s.green, &Surf.PolyColor.s.blue);
+
 		GL_DrawPolygon(&Surf, v, 4, PF_Modulated|PF_Translucent|PF_NoTexture|PF_NoDepthTest);
 	}
 
