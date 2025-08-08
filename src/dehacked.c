378eb28ad18da381fa96dd0d8f4f040cce28afda
Indev <bdmitry450@gmail.com>
2025-08-08 17:53:46 +0300

    Fix potential buffer overflow in DEH_LoadDehackedFile

--- src/dehacked.c
+++ src/dehacked.c
@@ -2862,7 +2862,7 @@ static void DEH_LoadDehackedFile(MYFILE *f, UINT16 wad)
 	dbg_line = -1; // start at -1 so the first line is 0.
 	while (!myfeof(f))
 	{
-		char origpos[128];
+		char origpos[256];
 		INT32 size = 0;
 		char *traverse;
 
@@ -2876,6 +2876,12 @@ static void DEH_LoadDehackedFile(MYFILE *f, UINT16 wad)
 		{
 			traverse++;
 			size++;
+		}
+
+		if (size > (int)sizeof(origpos))
+		{
+			deh_warning("Line is too long");
+			size = sizeof(origpos)-1;
 		}
 
 		strncpy(origpos, s, size);
