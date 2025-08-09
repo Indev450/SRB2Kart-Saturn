bbb4749815eaa0cf642d75ac66161c0fadf5b1ae
Alug <alufoliehorst@gmail.com>
2025-08-09 17:10:41 +0200

    speedup addon search
    by adding a blacklist of folders
    logs, luafiles, replay etc shouldnt ever contain addons so those can be skipped entirely
    this massively speeds up searching for addons on windows
    also will make findfile try to search in the designated addon folders first so it doesent look in the home or kart folders before it
    this is probably not the best way to do things
    but idk what im even doing, im not a coder just some chao who likes to sleep all day

--- src/sdl/i_system.cpp
+++ src/sdl/i_system.cpp
@@ -2458,7 +2458,7 @@ static const char *searchWad(const char *searchDir)
 	filestatus_t fstemp;
 
 	strcpy(tempsw, WADKEYWORD1);
-	fstemp = filesearch(tempsw,searchDir,NULL,true,20);
+	fstemp = filesearch(tempsw, searchDir, NULL, true, 20, true);
 	if (fstemp == FS_FOUND)
 	{
 		pathonly(tempsw);
