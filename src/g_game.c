e7c40dd91405ee8a3647351e4e0eb9851e5f9386
Alug <alufoliehorst@gmail.com>
2025-08-15 09:59:37 +0200

    fix -Wunused-value warnings
    we dont do anything with this, so just skip both

--- src/g_game.c
+++ src/g_game.c
@@ -1244,7 +1244,7 @@ static void G_DoLoadLevel(boolean resetplayer)
 	}
 
 	// Setup the level.
-	if (!P_SetupLevel(false,false))
+	if (!P_SetupLevel(false, false))
 	{
 		// fail so reset game stuff
 		Command_ExitGame_f();
