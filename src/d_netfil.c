f7120ec7a5a59f6d2693d9a5149acb10d87d0085
toaster <rollerorbital@gmail.com>
2025-08-25 21:00:44 +0100

    findfile aggressive optimisation: If srb2home and srb2path are the current working directory, don't search in them
    srb2path will report "." while this optimisation is in play, but this can be disabled if necessary by modifying the small #if (1) in IdentifyVersion

--- src/d_netfil.c
+++ src/d_netfil.c
@@ -1208,23 +1208,29 @@ filestatus_t findfile(char *filename, const UINT8 *wantedmd5sum, boolean complet
 		// if not found at all, just move on without doing anything
 	}
 
-	// next, check SRB2's "home" directory
-	homecheck = filesearch(filename, srb2home, wantedmd5sum, completepath, 10);
+	// next, check SRB2's "home" directory (if non-'.')
+	if (strcmp(srb2home, "."))
+	{
+		homecheck = filesearch(filename, srb2home, wantedmd5sum, completepath, 10);
 
-	if (homecheck == FS_FOUND) // we found the file, so return that we have :)
-		return FS_FOUND;
-	else if (homecheck == FS_MD5SUMBAD) // file has a bad md5; move on and look for a file with the right md5
-		badmd5 = true;
-	// if not found at all, just move on without doing anything
+		if (homecheck == FS_FOUND) // we found the file, so return that we have :)
+			return FS_FOUND;
+		else if (homecheck == FS_MD5SUMBAD) // file has a bad md5; move on and look for a file with the right md5
+			badmd5 = true;
+		// if not found at all, just move on without doing anything
+	}
 
-	// next, check SRB2's "path" directory
-	homecheck = filesearch(filename, srb2path, wantedmd5sum, completepath, 10);
+	// next, check SRB2's "path" directory (also if non-'.')
+	if (strcmp(srb2path, "."))
+	{
+		homecheck = filesearch(filename, srb2path, wantedmd5sum, completepath, 10);
 
-	if (homecheck == FS_FOUND) // we found the file, so return that we have :)
-		return FS_FOUND;
-	else if (homecheck == FS_MD5SUMBAD) // file has a bad md5; move on and look for a file with the right md5
-		badmd5 = true;
-	// if not found at all, just move on without doing anything
+		if (homecheck == FS_FOUND) // we found the file, so return that we have :)
+			return FS_FOUND;
+		else if (homecheck == FS_MD5SUMBAD) // file has a bad md5; move on and look for a file with the right md5
+			badmd5 = true;
+		// if not found at all, just move on without doing anything
+	}
 
 	// finally check "." directory
 	homecheck = filesearch(filename, ".", wantedmd5sum, completepath, 10);
