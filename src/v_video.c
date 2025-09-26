097d118095d0bd4ffc061de1fd055d387529ed98
GenericHeroGuy <genericheroguy@noreply.codeberg.org>
2025-09-26 23:59:53 +0200

    Fix VID_BlitLinearScreen's SSE version ignoring width (closes #128)

--- src/v_video.c
+++ src/v_video.c
@@ -615,7 +615,7 @@ static void CV_constextsize_OnChange(void)
 void VID_BlitLinearScreen(const UINT8 *srcptr, UINT8 *destptr, INT32 width, INT32 height, size_t srcrowbytes,
 	size_t destrowbytes)
 {
-	if (srcrowbytes == destrowbytes)
+	if (srcrowbytes == destrowbytes && srcrowbytes == width)
 	{
 		size_t i = srcrowbytes * height;
 #if defined(__SSE__)
