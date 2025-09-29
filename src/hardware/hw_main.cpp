a5218669569568be10c05d4762f3430bd0871d81
Alug <alufoliehorst@gmail.com>
2025-09-29 13:43:19 +0200

    rename this

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -3003,7 +3003,7 @@ static void HWR_Subsector(size_t num)
 
 				const sector_t *controlSec = &sectors[rover->secnum];
 
-				if (!controlSec->moved)
+				if (controlSec->moved != true)
 					continue;
 
 				anyMoved = true;
