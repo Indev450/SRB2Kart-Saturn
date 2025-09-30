5be5ae3f5f26cb3552be2230a95d6e1b06e8cdf1
Alug <alufoliehorst@gmail.com>
2025-09-30 19:35:01 +0200

    delet CLIENT_LOADINGSCREEN

--- src/d_netfil.h
+++ src/d_netfil.h
@@ -59,19 +59,15 @@ extern char downloaddir[512];
 
 typedef struct
 {
-#ifdef CLIENT_LOADINGSCREEN
 	INT32 current;
 	INT32 completednum;
 	UINT32 completedsize;
 	INT32 totalnum;
 	UINT32 totalsize;
-#endif
-
 #ifdef HAVE_CURL
 	INT32 remaining;
 	boolean http_failed;
 	boolean http_running;
-
 	char http_source[MAX_MIRROR_LENGTH+1];
 #endif
 } file_download_t;
@@ -79,7 +75,6 @@ typedef struct
 extern file_download_t filedownload;
 
 #ifdef HAVE_CURL
-
 typedef struct HTTP_login HTTP_login;
 
 extern struct HTTP_login
