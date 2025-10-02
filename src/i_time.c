ab79a02462704bd7c34ed6b8f9c4080f875546f3
Alug <alufoliehorst@gmail.com>
2025-10-02 16:20:15 +0200

    reuse ticratescaled from I_UpdateTime
    fixes major performance regression + prob shouldnt take this into account this later eitherway

--- src/i_time.c
+++ src/i_time.c
@@ -29,16 +29,14 @@ consvar_t cv_timescale = {"timescale", "1.0", CV_NETVAR|CV_CHEAT|CV_FLOAT, times
 static precise_t enterprecise, oldenterprecise;
 static fixed_t entertic, oldentertics;
 static double tictimer;
+static double ticratescaled;
 
 // experiment to prevent timing issues
 // this returns the global time state accounted with how much time has passed
 // since it was last updated
 static void I_GetTimeAndFrac(tic_t *outtics, fixed_t *outfrac)
 {
-	double ticratescaled;
 	double elapsedseconds;
-
-	ticratescaled = (double)TICRATE * FixedToDouble(cv_timescale.value);
 
 	elapsedseconds = (double)(I_GetPreciseTime() - oldenterprecise) / I_GetPrecisePrecision();
 
@@ -81,11 +79,11 @@ void I_InitializeTime(void)
 	entertic = 0;
 	oldentertics = 0;
 	tictimer = 0.0;
+	ticratescaled = 1.0;
 }
 
 void I_UpdateTime(fixed_t timescale)
 {
-	double ticratescaled;
 	double elapsedseconds;
 	tic_t realtics;
 
