90c3d02dbffeac3156b608ca302317b0e46fbc75
Alug <alufoliehorst@gmail.com>
2025-10-08 20:46:27 +0200

    ensure columnbuffer gets flushed during portal traversal
    in worst case this does nothing btw

--- src/r_main.cpp
+++ src/r_main.cpp
@@ -1490,6 +1490,7 @@ void R_RenderPlayerView(player_t *player)
 			R_RenderViewpoint(&masks[nummasks - 1], true);
 
 			R_ClipSprites(ds_p - (masks[nummasks - 1].drawsegs[1] - masks[nummasks - 1].drawsegs[0]), portal);
+			R_ResetColumnBuffer();
 
 			Portal_Remove(portal);
 		}
