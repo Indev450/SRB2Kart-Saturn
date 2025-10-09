e7eddb8aae2454490aaedd5fee400be40fdaa0ec
Alug <alufoliehorst@gmail.com>
2025-10-09 18:16:57 +0200

    fix buffer overflows in dedi build too

--- src/dedicated/i_system.c
+++ src/dedicated/i_system.c
@@ -1474,7 +1474,7 @@ void I_Error(const char *error, ...)
 		if (errorcount > 20)
 		{
 			va_start(argptr, error);
-			vsprintf(buffer, error, argptr);
+			vsnprintf(buffer, 8192, error, argptr);
 			va_end(argptr);
 
 			I_OutputMsg("SRB2Kart %s Recursive Error", buffer);
@@ -1488,7 +1488,7 @@ void I_Error(const char *error, ...)
 
 	// Display error message in the console before we start shutting it down
 	va_start(argptr, error);
-	vsprintf(buffer, error, argptr);
+	vsnprintf(buffer, 8192, error, argptr);
 	va_end(argptr);
 	I_OutputMsg("\nI_Error(): %s\n", buffer);
 
