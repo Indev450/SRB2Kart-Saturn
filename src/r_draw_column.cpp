5765f3df5a635bef4738a2ee94506eef17161877
Alug <alufoliehorst@gmail.com>
2025-10-12 22:30:16 +0200

    this does not need marked as restrict

--- src/r_draw_column.cpp
+++ src/r_draw_column.cpp
@@ -233,7 +233,7 @@ static void R_DrawColumnTemplate(drawcolumndata_t *dc)
 		else
 			dest = R_GetBufferOpaque(dc);
 
-		restrict INT32 stride = 8; //SoM: Oh, Oh it's MAGIC! You know...
+		INT32 stride = 8; //SoM: Oh, Oh it's MAGIC! You know...
 
 		if constexpr (Type & DrawColumnType::DC_DIRECT)
 			stride = vid.width;
