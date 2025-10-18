39269648a92c8654f5c574075b067614d7d5cddf
Alug <alufoliehorst@gmail.com>
2025-10-18 16:52:31 +0200

    use npow2 screen textures if GL_ARB_texture_non_power_of_two is supported
    apparently some gl drivers may use a slower path for streamed textures that only get partly updated, this tricks them into thinking this did a "full update" and resulting in using a faster renderpath

--- src/hardware/r_opengl/r_opengl.h
+++ src/hardware/r_opengl/r_opengl.h
@@ -125,6 +125,7 @@ extern GLint			screen_width;
 extern GLint			screen_height;
 extern GLbyte			screen_depth;
 extern GLint			maximumAnisotropy;
+extern boolean			supportNPO2tex;
 extern boolean 			supportMipMap;
 
 /**	\brief OpenGL flags for video driver
