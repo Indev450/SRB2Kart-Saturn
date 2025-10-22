a294f0aa95f4bcdc06c222b1eed22cc5f8ab4703
Alug <alufoliehorst@gmail.com>
2025-10-22 10:38:22 +0200

    remove erronous ";"

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -4476,7 +4476,7 @@ static void Command_Addfile(void)
 	if (IsPlayerAdmin(consoleplayer) && (!server)) // Request to add file
 		SendNetXCmd(XD_REQADDFILE, buf, buf_p - buf);
 	else
-		SendNetXCmd(XD_ADDFILE, buf, buf_p - buf);;
+		SendNetXCmd(XD_ADDFILE, buf, buf_p - buf);
 }
 
 /** Adds something at runtime.
