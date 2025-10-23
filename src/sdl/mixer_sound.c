2f865f97c76688518ea958a08c75e54daca01c00
Alug <alufoliehorst@gmail.com>
2025-10-23 10:55:46 +0200

    clean up controller dpad input handling
    just treat them as "normal" buttons instead of manually polling for them
    alot cleaner and gets rid of yet another polling loop
    also clean up some other stuff
    needs to be tested with multiple controllers in splitscreen but cba to do that atm

--- src/sdl/mixer_sound.c
+++ src/sdl/mixer_sound.c
@@ -73,7 +73,7 @@ write netcode into the sound code, OKAY?
 
 
 //static UINT16 BUFFERSIZE = 2048;
-static UINT16 SAMPLERATE = 44100;
+static const UINT16 SAMPLERATE = 44100;
 
 #ifdef HAVE_OPENMPT
 #include "libopenmpt/libopenmpt.h"
