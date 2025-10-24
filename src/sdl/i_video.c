863d9efbab67e5a138549e3a824525888b8cd5b7
Alug <alufoliehorst@gmail.com>
2025-10-24 18:28:57 +0200

    remove those memsets
    not needed anymore now

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -656,8 +656,6 @@ static void Impl_HandleMouseButtonEvent(SDL_MouseButtonEvent evt, Uint32 type)
 	/// \todo inputEvent.button.which
 	if (USE_MOUSEINPUT)
 	{
-		SDL_memset(&event, 0, sizeof(event_t));
-
 		switch (type)
 		{
 			case SDL_MOUSEBUTTONUP:
@@ -702,8 +700,6 @@ static void Impl_HandleMouseWheelEvent(SDL_MouseWheelEvent evt)
 
 	if (USE_MOUSEINPUT)
 	{
-		SDL_memset(&event, 0, sizeof(event_t));
-
 		if (evt.y > 0)
 		{
 			event.data1 = KEY_MOUSEWHEELUP;
