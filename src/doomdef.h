88363cf45d819047f93f9656909620c781e4a206
Alug <alufoliehorst@gmail.com>
2025-10-27 22:58:42 +0100

    add NOHOLEPUNCH flag to disable holepunch entirely

--- src/doomdef.h
+++ src/doomdef.h
@@ -651,7 +651,9 @@ extern const char *compdate, *comptime, *comprevision, *compbranch;
 
 #if defined (HAVE_CURL)
 #define MASTERSERVER
+#ifndef NOHOLEPUNCH
 #define HOLEPUNCH
+#endif
 #else
 #undef UPDATE_ALERT
 #endif
