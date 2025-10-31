f3a883f00bb2ff32e7c08ab308ed943a0db9fa55
Alug <alufoliehorst@gmail.com>
2025-10-31 20:25:43 +0100

    md5: raise block buffer size to 64k
    did some benchmarking and this was more or less the best size in terms of performance, gaining around 20-25 mb/s on my system

--- src/md5.c
+++ src/md5.c
@@ -345,7 +345,7 @@ static void *md5_finish_ctx(struct md5_ctx *ctx, void *resbuf)
 int md5_stream(FILE *stream, void *resblock)
 {
 	/* Important: BLOCKSIZE must be a multiple of 64.  */
-#define BLOCKSIZE 4096
+#define BLOCKSIZE (64 << 10)
 	struct md5_ctx ctx;
 	char buffer[BLOCKSIZE + 72];
 	size_t sum = 0;
