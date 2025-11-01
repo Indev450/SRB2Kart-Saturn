456a380e3a975311ecfbc898635a17232e12e8f3
Alug <alufoliehorst@gmail.com>
2025-11-01 15:39:21 +0100

    HWR_PrecacheLevel: disable sprite precaching
    in some cases this may lead to crashes, its mostly useless in gl eitherway due to how colourmapped sprites work

--- src/hardware/hw_cache.c
+++ src/hardware/hw_cache.c
@@ -745,7 +745,7 @@ static void HWR_PrecacheLevelTextures(void)
 	free(texturepresent);
 }
 
-static void HWR_PrecacheLevelSprites(void)
+/*static void HWR_PrecacheLevelSprites(void)
 {
 	patch_t *spritepatch;
 	char *spritepresent;
@@ -808,7 +808,7 @@ static void HWR_PrecacheLevelSprites(void)
 		}
 	}
 	free(spritepresent);
-}
+}*/
 
 void HWR_PrecacheLevel(void)
 {
@@ -822,7 +822,7 @@ void HWR_PrecacheLevel(void)
 	HWR_PrecacheLevelTextures();
 
 	// Precache sprites.
-	HWR_PrecacheLevelSprites();
+	//HWR_PrecacheLevelSprites();
 }
 
 void HWR_LoadMapTextures(size_t pnumtextures)
