b676e1d420d65696eb48822f9dca20f7295b3921
Alug <alufoliehorst@gmail.com>
2025-11-01 15:33:13 +0100

    Mipmaps: fix translucent textures turning black
    i.e. since we allow texture filtering on translucent textures now, the min filter may specifcy one of the mipmap filters, this means we NEED to call GL_GENERATE_MIPMAP and set the LODs for them, otherwise no texture will be generated
    alternatively we could do a bunch of special cases for the mipmap filters, but this is simpler

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -1246,8 +1246,6 @@ static inline boolean HWR_BlendMidtextureSurface(FSurfaceInfo *pSurf)
 			case 256:
 				if (gl_linedef->blendmode)
 					blendmode = HWR_SurfaceBlend(gl_linedef->blendmode, R_GetLinedefTransTable(gl_linedef->alpha), pSurf);
-				else
-					blendmode = PF_Translucent;
 				break;
 			default:
 				if (gl_linedef->blendmode)
@@ -1259,8 +1257,6 @@ static inline boolean HWR_BlendMidtextureSurface(FSurfaceInfo *pSurf)
 				}
 				else if (gl_linedef->alpha >= 0 && gl_linedef->alpha < FRACUNIT)
 					blendmode = HWR_TranstableToAlpha(R_GetLinedefTransTable(gl_linedef->alpha), pSurf);
-				else
-					blendmode = PF_Masked;
 				break;
 		}
 	}
