feff5a7c17487681b0a1680ebdb4d105303e70b3
Alug <alufoliehorst@gmail.com>
2025-11-01 16:32:04 +0100

    make a toggle for using angleturn in replays
    while this is more accurate to how the player experienced stuff in the actual game, this also can be somewhat jittery and stuttery depending on the net conditions the replay was recorded in, so make this toggable if someone wants it

--- src/p_user.c
+++ src/p_user.c
@@ -3824,7 +3824,7 @@ boolean P_MoveChaseCamera(player_t *player, camera_t *thiscam, boolean resetcall
 		// Hack-adjacent.
 		// Sometimes stale ticcmds send a weird angle at the start of the race.
 		// P_UpdatePlayerAngle knows to ignore cmd angle when you literally can't turn, so we do the same here.
-		if (leveltime > starttime)
+		if (cv_demoangturn.value && leveltime > starttime)
 			focusangle = player->cmd.angleturn << 16;
 		else
 			focusangle = mo->angle; // Just use something known sane.
