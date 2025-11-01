fc6ebca065e9ed7a164be2f82883b289b1a05793
Alug <alufoliehorst@gmail.com>
2025-11-01 19:36:28 +0100

    postautoloaded can be static
    not used outside of d_main

--- src/d_main.h
+++ src/d_main.h
@@ -61,7 +61,6 @@ extern boolean minilighticon;     // mkwii-style minimap headlight
 //
 
 // autoload stuff
-extern boolean postautoloaded;
 void D_AddPostloadFiles(void);
 
 // the infinite loop of D_SRB2Loop() called from win_main for windows version
