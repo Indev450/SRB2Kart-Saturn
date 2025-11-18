2ee8dc09d19c673858216c9f1e650f305b28b84b
Alug <alufoliehorst@gmail.com>
2025-11-18 21:52:42 +0100

    fix opengl portals (and its contents) being culled in some cases they shouldnt be
    we do culling per our viewmatrix since a while, which wasnt properly set up for portals beforehand and used whatever was last which was then incorrectly applied to portal culling :chaobaba:

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -5435,10 +5435,14 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_
 			currentportallist = &portallist;
 			HWR_SetPortalState(GLPORTAL_SEARCH);
 
-			HWR_ClearClipper();
+			HWR_SetTransform(fpov);
 
+			HWR_ClearSprites();
+			HWR_ClearClipper();
 			if (rootportal)
+			{
 				HWR_PortalClipping(rootportal);
+			}
 
 			validcount++;
 
@@ -5456,13 +5460,13 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_
 		}
 	}
 
-	// draw normal things in current frame in current incremented stencil buffer area
-	HWR_SetStencilState(HWR_STENCIL_NORMAL, stencil_level);
-
 	HWR_SetTransform(fpov);
 
 	HWR_ClearSprites();
 	HWR_ClearClipper();
+
+	// draw normal things in current frame in current incremented stencil buffer area
+	HWR_SetStencilState(HWR_STENCIL_NORMAL, stencil_level);
 
 	if constexpr (Type == RenderViewpointType::kPortal)
 	{
