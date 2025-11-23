20a08df1e1a481dc7fd1f0380ef541104c76fa47
Alug <alufoliehorst@gmail.com>
2025-11-24 00:30:07 +0100

    K_PlayerEffectsShouldBlend: also apply when player has invulnerability

--- src/k_kart.c
+++ src/k_kart.c
@@ -5373,7 +5373,8 @@ boolean K_PlayerEffectsShouldBlend(player_t *player)
 	if (!cv_playerblendeffects.value || !player->mo)
 		return false;
 
-	if (player->kartstuff[k_sneakertimer])
+	if (player->kartstuff[k_sneakertimer] ||
+		player->kartstuff[k_invincibilitytimer])
 		return true;
 
 	// this is how the percentage speedometer calcs, i suck at maths so this was the easiest thing to do lmao
