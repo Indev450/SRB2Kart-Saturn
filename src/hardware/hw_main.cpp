1294f4b7c605ee57052ea6cf67665581bca582c2
Alug <alufoliehorst@gmail.com>
2025-11-23 23:07:51 +0100

    HACK: absuse batching system for sprites and drawnodes
    use batching system without sorting for order sensitive things like sprites and transparent stuff
    even despite not being "optimally" sorted, drawing them in one go, instead of directly still gives a nice speed increase in certain cases (on my ryzen 5600 i get around 120fps+ on teiai towers starting area!)

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -4396,6 +4396,9 @@ static void HWR_RenderDrawNodes(void)
 	// Okay! Let's draw it all! Woo!
 	GL_SetTransform(&atransform);
 
+	if (LIKELY(cv_glbatching.value))
+		HWR_StartBatching();
+
 	for (i = 0; i < numdrawnodes; i++)
 	{
 		gl_drawnode_t *drawnode = &drawnodes[sortindex[i]];
@@ -4446,6 +4449,9 @@ static void HWR_RenderDrawNodes(void)
 				break;
 		}
 	}
+
+	if (LIKELY(cv_glbatching.value))
+		HWR_RenderBatches(false);
 
 	PS_STOP_TIMING(ps_hw_nodedrawtime);
 
@@ -5411,6 +5417,7 @@ void HWR_SetTransform(float fpov)
 void HWR_ClearClipper(void)
 {
 	const angle_t a1 = gld_FrustumAngle(gl_aimingangle);
+
 	gld_clipper_Clear();
 	gld_clipper_SafeAddClipRange(viewangle + a1, viewangle - a1);
 #ifdef HAVE_SPHEREFRUSTRUM
@@ -5493,7 +5500,7 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_
 	{
 		if (allow_portals && rootportal)
 		{
-			HWR_PortalFrame(rootportal);// for portalclipsector, it could have gone null from search
+			HWR_PortalFrame(rootportal); // for portalclipsector, it could have gone null from search
 			HWR_PortalClipping(rootportal);
 		}
 	}
@@ -5543,7 +5550,7 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_
 	PS_STOP_TIMING(ps_bsptime);
 
 	if (LIKELY(cv_glbatching.value))
-		HWR_RenderBatches();
+		HWR_RenderBatches(true);
 
 	if constexpr (Type == RenderViewpointType::kPortal)
 	{
@@ -5567,10 +5574,14 @@ void HWR_RenderViewpoint(gl_portal_t *rootportal, player_t *player, int stencil_
 	HWR_SortVisSprites();
 	PS_STOP_TIMING(ps_hw_spritesorttime);
 	PS_START_TIMING(ps_hw_spritedrawtime);
+	if (LIKELY(cv_glbatching.value))
+		HWR_StartBatching();
 	if (UNLIKELY(cv_glmdls.value))
 		HWR_DrawSprites<DrawSpritesType::kModels>();
 	else
 		HWR_DrawSprites<DrawSpritesType::kSprites>();
+	if (LIKELY(cv_glbatching.value))
+		HWR_RenderBatches(false);
 	PS_STOP_TIMING(ps_hw_spritedrawtime);
 
 	ps_numdrawnodes.value.i    = 0;
