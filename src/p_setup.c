028a288aff80070e26002e2f6ea8606d4ccdf067
Alug <alufoliehorst@gmail.com>
2025-11-24 01:02:27 +0100

    remove erronous P_RunDynamicSlopes call in P_PreTicker
    we dont run preticker when we netjoin so.....

--- src/p_setup.c
+++ src/p_setup.c
@@ -2747,7 +2747,7 @@ boolean P_SetupLevel(boolean fromnetsave, boolean reloadinggamestate)
 		M_ResetDemoList();
 	}
 
-	midgamejoin = fromnetsave; // makes dynslopes run in P_Ticker/P_PreTicker to avoid synch issues and other stuff
+	midgamejoin = fromnetsave; // makes dynslopes run in P_Ticker to avoid synch issues and other stuff
 
 	levelloading = true;
 
