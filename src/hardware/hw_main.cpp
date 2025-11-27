1e394b05300ede6655643174884c4b0ba78d84d9
Alug <alufoliehorst@gmail.com>
2025-11-27 16:20:03 +0100

    pass frontsector ceiling z to R_Prep3DFloors
    in both cases "R_Subsector" already got the frontsectors ceiling z, so no need to get it again within R_Prep3DFloors
    should save some P_GetSectorCeilingZAt per frame

--- src/hardware/hw_main.cpp
+++ src/hardware/hw_main.cpp
@@ -2964,7 +2964,7 @@ static void HWR_Subsector(size_t num)
 		if (anyMoved == true)
 		{
 			gl_frontsector->numlights = sub->sector->numlights = 0;
-			R_Prep3DFloors(gl_frontsector);
+			R_Prep3DFloors(gl_frontsector, locCeilingHeight);
 			sub->sector->lightlist = gl_frontsector->lightlist;
 			sub->sector->numlights = gl_frontsector->numlights;
 			sub->sector->moved = gl_frontsector->moved = false;
