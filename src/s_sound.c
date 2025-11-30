2398c6f5f3500cac042e764345fe82f5be64c6e3
Alug <alufoliehorst@gmail.com>
2025-11-30 21:55:30 +0100

    fix startup playing a super loud bit of music

--- src/s_sound.c
+++ src/s_sound.c
@@ -2281,15 +2281,16 @@ static void GameSounds_OnChange(void)
 	if (M_CheckParm("-nosound") || M_CheckParm("-noaudio"))
 		return;
 
-	sound_disabled = !cv_gamesounds.value;
-	if (sound_disabled)
+	if (sound_disabled && cv_gamesounds.value)
 	{
+		sound_disabled = false;
 		I_StartupSound(); // will return early if initialised
 		S_InitSfxChannels(cv_soundvolume.value);
 		S_StartSound(NULL, sfx_strpst);
 	}
-	else
+	else if (!sound_disabled && !cv_gamesounds.value)
 	{
+		sound_disabled = true;
 		S_StopSounds();
 	}
 }
@@ -2314,9 +2315,9 @@ void GameMusic_OnChange(void)
 	if (M_CheckParm("-nomusic") || M_CheckParm("-noaudio"))
 		return;
 
-	music_disabled = !cv_gamedigimusic.value;
-	if (music_disabled)
+	if (music_disabled && cv_gamedigimusic.value)
 	{
+		music_disabled = false;
 		I_StartupSound(); // will return early if initialised
 		I_InitMusic();
 
@@ -2327,8 +2328,9 @@ void GameMusic_OnChange(void)
 		else
 			S_ChangeMusicInternal("titles", looptitle);
 	}
-	else
+	else if (!music_disabled && !cv_gamedigimusic.value)
 	{
+		music_disabled = true;
 		S_StopMusic();
 	}
 }
