a80b311aba99324bc6863f4ca2e61d75116cee42
Alug <alufoliehorst@gmail.com>
2025-11-30 20:28:35 +0100

    remove hwr_shutdown call
    honestly this didnt help with the issue i had, game should already release all the memory on quit anyways.
    this just makes it take longer to quit

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -2187,16 +2187,11 @@ void I_ShutdownGraphics(void)
 
 	graphics_started = false;
 	I_OutputMsg("shut down\n");
-
 #ifdef HWRENDER
-	if (vid.glstate == VID_GL_LIBRARY_LOADED)
-		HWR_Shutdown();
-
 	if (sdlglcontext)
 		SDL_GL_DestroyContext(sdlglcontext);
 	sdlglcontext = NULL;
 #endif
-
 	if (texture)
 		SDL_DestroyTexture(texture);
 	texture = NULL;
