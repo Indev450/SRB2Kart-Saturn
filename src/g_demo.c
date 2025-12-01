3ee8ffe857977b97ca663e76500b62d2144b15a1
Alug <alufoliehorst@gmail.com>
2025-12-01 21:03:18 +0100

    replays: always check if map is loaded not just in old 1.0 replays
    cause like, otherwise it will just I_Error which is worse lol
    like this we just get thrown back to titlescreen

--- src/g_demo.c
+++ src/g_demo.c
@@ -2785,6 +2785,16 @@ void G_DoPlayDemo(char *defdemoname)
 #endif
 	demobuf.p += 4; // Extrainfo location
 
+	// ...*map* not loaded?
+	if (!gamemap || (gamemap > NUMMAPS) || !mapheaderinfo[gamemap-1] || !(mapheaderinfo[gamemap-1]->menuflags & LF2_EXISTSHACK))
+	{
+		snprintf(msg, 1024, M_GetText("%s features a course that is not currently loaded.\n"), pdemoname);
+		CONS_Alert(CONS_ERROR, "%s", msg);
+		M_StartMessage(msg, M_ReturnToTitleFromError, MM_EVENTHANDLER);
+		G_ResetDemoPlayback();
+		return;
+	}
+
 #ifdef DEMO_COMPAT_100
 	if (demo.version == 0x0001)
 	{
@@ -2811,16 +2821,6 @@ void G_DoPlayDemo(char *defdemoname)
 		if (!SetPlayerSkin(0, skin))
 		{
 			snprintf(msg, 1024, M_GetText("%s features a character that is not currently loaded.\n"), pdemoname);
-			CONS_Alert(CONS_ERROR, "%s", msg);
-			M_StartMessage(msg, M_ReturnToTitleFromError, MM_EVENTHANDLER);
-			G_ResetDemoPlayback();
-			return;
-		}
-
-		// ...*map* not loaded?
-		if (!gamemap || (gamemap > NUMMAPS) || !mapheaderinfo[gamemap-1] || !(mapheaderinfo[gamemap-1]->menuflags & LF2_EXISTSHACK))
-		{
-			snprintf(msg, 1024, M_GetText("%s features a course that is not currently loaded.\n"), pdemoname);
 			CONS_Alert(CONS_ERROR, "%s", msg);
 			M_StartMessage(msg, M_ReturnToTitleFromError, MM_EVENTHANDLER);
 			G_ResetDemoPlayback();
