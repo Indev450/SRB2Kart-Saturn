f5c2af2bc7159e4a469784889e9fdc9644413b0f
Alug <alufoliehorst@gmail.com>
2025-12-03 16:39:56 +0100

    Blua: small optimization (reorder of BinOpr enum to unify some cases in switches)
    https://github.com/lua/lua/commit/d7872dcf91eb2075312a03973a6cdb489610c796

--- src/blua/lcode.h
+++ src/blua/lcode.h
@@ -21,13 +21,13 @@
 
 
 /*
-** grep "ORDER OPR" if you change these enums
+** grep "ORDER OPR" if you change these enums  (ORDER OP)
 */
 typedef enum BinOpr {
   OPR_ADD, OPR_SUB, OPR_MUL, OPR_DIV, OPR_MOD, OPR_POW,
   OPR_CONCAT,
-  OPR_NE, OPR_EQ,
-  OPR_LT, OPR_LE, OPR_GT, OPR_GE,
+  OPR_EQ, OPR_LT, OPR_LE,
+  OPR_NE, OPR_GT, OPR_GE,
   OPR_AND, OPR_OR,
   OPR_BAND, OPR_BOR, OPR_BXOR,
   OPR_BSHL, OPR_BSHR,
