a2feaa7fde4c2d3138cf21c40c2f8f671fac78ad
Alug <alufoliehorst@gmail.com>
2025-12-07 20:22:25 +0100

    dont call I_Quit from within "quit_handler", let it signal main loop to quit
    fixes an issue for me which led to having the game threads getting stuck when sending an interrupt signal to the game due to some thread issues with the sdl subsystems
    
    shoutout to dsda doom for this

--- src/d_main.c
+++ src/d_main.c
@@ -679,6 +679,9 @@ void D_SRB2Loop(void)
 
 	for (;;)
 	{
+		if (I_Interrupted())
+			I_Quit();
+
 		// capbudget is the minimum precise_t duration of a single loop iteration
 		precise_t capbudget;
 		precise_t elapsed;
--- src/sdl/i_system.cpp
+++ src/sdl/i_system.cpp
@@ -1670,21 +1670,19 @@ FUNCNORETURN static ATTRNORETURN void signal_handler(INT32 num)
 }
 #endif
 
-FUNCNORETURN static ATTRNORETURN void quit_handler(int num)
-{
-#ifdef HAVE_THREADS
-	if (g_main_thread_id != std::this_thread::get_id())
-	{
-		// Do not attempt any sort of recovery if this signal triggers off the main thread
-		signal(num, SIG_DFL);
-		raise(num);
-		exit(-2);
-	}
-#endif
+static volatile sig_atomic_t interrupted = 0;
 
+boolean I_Interrupted(void)
+{
+	return interrupted;
+}
+
+static void quit_handler(int num)
+{
 	signal(num, SIG_DFL); //default signal action
 	raise(num);
-	I_Quit();
+	//I_Quit();
+	interrupted = true;
 }
 
 static void I_RegisterSignals(void)
