e6cbd31e72b059dd79e4eae8420d0994fdaf17d3
Alug <alufoliehorst@gmail.com>
2025-12-07 17:09:48 +0100

    Revert "Fix Haiku build"
    This reverts commit e716de35cc75e25983e10bdd3d768f659fdb2971.

--- src/sdl/sdl_sound.c
+++ src/sdl/sdl_sound.c
@@ -1197,11 +1197,7 @@ boolean I_SetSongSpeed(float speed)
 	if (synth_player)
 	{
 		SDL_LockAudioStream(audio_stream);
-#if FLUIDSYNTH_VERSION_MINOR < 2
-		fluid_player_set_bpm(synth_player, speed);
-#else
 		fluid_player_set_tempo(synth_player, FLUID_PLAYER_TEMPO_INTERNAL, speed);
-#endif
 		SDL_UnlockAudioStream(audio_stream);
 		return true;
 	}
