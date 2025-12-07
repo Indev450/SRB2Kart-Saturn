b2ae5679ca576d346b07c14b9329fd09c9755b4a
Alug <alufoliehorst@gmail.com>
2025-12-07 17:11:30 +0100

    Revert "a"
    This reverts commit 7bec44fc3351a8fdc994a2d1e30640d777228011.

--- cmake/Modules/FindSDL2.cmake
+++ cmake/Modules/FindSDL2.cmake
@@ -1,43 +1,43 @@
-# Find SDL3
+# Find SDL2
 # Once done, this will define
 #
-#  SDL3_FOUND - system has SDL3
-#  SDL3_INCLUDE_DIRS - SDL3 include directories
-#  SDL3_LIBRARIES - link libraries
+#  SDL2_FOUND - system has SDL2
+#  SDL2_INCLUDE_DIRS - SDL2 include directories
+#  SDL2_LIBRARIES - link libraries
 
 include(LibFindMacros)
 
-libfind_pkg_check_modules(SDL3_PKGCONF SDL3)
+libfind_pkg_check_modules(SDL2_PKGCONF SDL2)
 
 # includes
-find_path(SDL3_INCLUDE_DIR
+find_path(SDL2_INCLUDE_DIR
 	NAMES SDL.h
 	PATHS
-		${SDL3_PKGCONF_INCLUDE_DIRS}
-		"/usr/include/SDL3"
-		"/usr/local/include/SDL3"
+		${SDL2_PKGCONF_INCLUDE_DIRS}
+		"/usr/include/SDL2"
+		"/usr/local/include/SDL2"
 )
 
 # library
-find_library(SDL3_LIBRARY
-	NAMES SDL3
+find_library(SDL2_LIBRARY
+	NAMES SDL2
 	PATHS
-		${SDL3_PKGCONF_LIBRARY_DIRS}
+		${SDL2_PKGCONF_LIBRARY_DIRS}
 		"/usr/lib"
 		"/usr/local/lib"
 )
 
 # set include dir variables
-set(SDL3_PROCESS_INCLUDES SDL3_INCLUDE_DIR)
-set(SDL3_PROCESS_LIBS SDL3_LIBRARY)
-libfind_process(SDL3)
+set(SDL2_PROCESS_INCLUDES SDL2_INCLUDE_DIR)
+set(SDL2_PROCESS_LIBS SDL2_LIBRARY)
+libfind_process(SDL2)
 
-if(SDL3_FOUND AND NOT TARGET SDL3::SDL3)
-	add_library(SDL3::SDL3 UNKNOWN IMPORTED)
+if(SDL2_FOUND AND NOT TARGET SDL2::SDL2)
+	add_library(SDL2::SDL2 UNKNOWN IMPORTED)
 	set_target_properties(
-		SDL3::SDL3
+		SDL2::SDL2
 		PROPERTIES
-		IMPORTED_LOCATION "${SDL3_LIBRARY}"
-		INTERFACE_INCLUDE_DIRECTORIES "${SDL3_INCLUDE_DIR}"
+		IMPORTED_LOCATION "${SDL2_LIBRARY}"
+		INTERFACE_INCLUDE_DIRECTORIES "${SDL2_INCLUDE_DIR}"
 	)
 endif()
