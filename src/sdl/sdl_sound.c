e8e20bbfad45cd469e8a3e094a7eef7aba9ca515
Alug <alufoliehorst@gmail.com>
2025-12-07 17:09:11 +0100

    Revert "Limit audio channels to 2"
    This reverts commit a553bd1a9a8c2694d004181c1db07e9bce7b3cb4.

--- src/sdl/sdl_sound.c
+++ src/sdl/sdl_sound.c
@@ -528,8 +528,6 @@ void I_StartupSound(void)
 
 	virtual_spec.format = SDL_AUDIO_F32LE;
 	virtual_spec.channels = actual_spec.channels;
-	if (virtual_spec.channels > 2)
-		virtual_spec.channels = 2; // TODO: add support for surround
 	virtual_spec.freq = actual_spec.freq;
 
 	music_volume = sfx_volume = 0;
