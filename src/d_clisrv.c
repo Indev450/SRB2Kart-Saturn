ea0f5d11d37b91e90b6fe3bdf8119218a04d5022
Alug <alufoliehorst@gmail.com>
2025-12-09 18:06:48 +0100

    netcode: replace some stuff with memsets + add a little bounds safety in some places

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -1945,8 +1945,11 @@ static boolean SV_ResendingSavegameToAnyone(void)
 	INT32 i;
 
 	for (i = 0; i < MAXNETNODES; i++)
+	{
 		if (nodeingame[i] && resendingsavegame[i])
 			return true;
+	}
+
 	return false;
 }
 #endif
@@ -2951,8 +2954,10 @@ static void CL_ConnectToServer(void)
 		{
 			pnumnodes = 0;
 			for (i = 0; i < MAXNETNODES; i++)
+			{
 				if (nodeingame[i])
 					pnumnodes++;
+			}
 		}
 	}
 	while (!(cl_mode == CL_CONNECTED && (client || (server && nodewaited <= pnumnodes))));
@@ -4428,8 +4433,10 @@ void D_QuitNetGame(void)
 
 		netbuffer->packettype = PT_SERVERSHUTDOWN;
 		for (i = 0; i < MAXNETNODES; i++)
+		{
 			if (nodeingame[i])
 				HSendPacket(i, true, 0, 0);
+		}
 #ifdef MASTERSERVER
 		if (serverrunning && netgame && cv_advertise.value) // see mserv.c Online()
 			UnregisterServer();
@@ -6879,8 +6886,10 @@ static inline void PingUpdate(void)
 
 	//send out our ping packets
 	for (i = 0; i < MAXNETNODES; i++)
+	{
 		if (nodeingame[i])
 			HSendPacket(i, true, 0, sizeof(INT32) * (MAXPLAYERS+1));
+	}
 
 	pingmeasurecount = 0; //Reset count
 }
@@ -6983,10 +6992,15 @@ static void RenewHolePunch(void)
 static void HandleNodeTimeouts(void)
 {
 	INT32 i;
+
 	if (server)
+	{
 		for (i = 1; i < MAXNETNODES; i++)
+		{
 			if (nodeingame[i] && freezetimeout[i] < I_GetTime())
 				Net_ConnectionTimeout(i);
+		}
+	}
 }
 
 // Keep the network alive while not advancing tics!
@@ -7196,11 +7210,13 @@ void NetUpdate(void)
 			}
 
 			for (i = 0; i < MAXNETNODES; ++i)
+			{
 				if (resynch_inprogress[i])
 				{
 					SV_SendResynch(i);
 					counts = -666;
 				}
+			}
 
 			// Do not make tics while resynching
 			if (counts != -666)
