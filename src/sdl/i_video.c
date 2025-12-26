dfb4ef2e1659863e430bf31d140c9bf6b9e2795e
Alug <alufoliehorst@gmail.com>
2025-12-26 23:18:19 +0100

    simplify opengl vsync set

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -1422,7 +1422,7 @@ void I_FinishUpdate(void)
 #ifdef HWRENDER
 	if (rendermode == render_opengl)
 	{
-		OglSdlFinishUpdate(cv_vidwait.value);
+		OglSdlFinishUpdate();
 		return;
 	}
 #endif
@@ -2209,16 +2209,16 @@ UINT32 I_GetRefreshRate(void)
 
 static void Impl_SetVsync(void)
 {
-#if SDL_VERSION_ATLEAST(2,0,18)
-	if (renderer)
-		SDL_RenderSetVSync(renderer, cv_vidwait.value);
-#endif
 #ifdef HWRENDER
-	if (!renderer && rendermode == render_opengl &&
-		 sdlglcontext != NULL && SDL_GL_GetCurrentContext() == sdlglcontext)
+	if (rendermode == render_opengl &&
+		sdlglcontext != NULL && SDL_GL_GetCurrentContext() == sdlglcontext)
 	{
 		SDL_GL_SetSwapInterval(cv_vidwait.value ? 1 : 0);
 	}
+#endif
+#if SDL_VERSION_ATLEAST(2,0,18)
+	if (renderer)
+		SDL_RenderSetVSync(renderer, cv_vidwait.value);
 #endif
 }
 
