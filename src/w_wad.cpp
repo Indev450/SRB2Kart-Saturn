380a2f7d4a701563cc8b499ffdbdf0bf7545a245
Alug <alufoliehorst@gmail.com>
2025-12-27 15:13:13 +0100

    fix w_wad oopsie

--- src/w_wad.cpp
+++ src/w_wad.cpp
@@ -2012,7 +2012,7 @@ void *W_CachePatchNameRotated(const char *name, INT32 rotationangle, INT32 tag)
 	rspr = static_cast<rotsprite_t *>(W_GetCachedRotPatchPwad(WADFILENUM(num), LUMPNUM(num)));
 
 	if (rspr == NULL)
-		return W_CachePatchNum(num, tag);
+		return W_CachePatchNum(W_GetNumForName("MISSING"), tag);
 
 	if (rspr->patches[idx] == NULL)
 	{
@@ -2020,7 +2020,7 @@ void *W_CachePatchNameRotated(const char *name, INT32 rotationangle, INT32 tag)
 
 		ptr = static_cast<patch_t *>(W_CachePatchNum(num, PU_PATCH));
 		if (ptr == NULL)
-			return W_CachePatchNum(num, tag);
+			return W_CachePatchNum(W_GetNumForName("MISSING"), tag);
 
 		// >y pivot centered
 		// >x pivot not centered
