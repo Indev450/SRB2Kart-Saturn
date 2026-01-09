8fe3d13935370048eaf2b3c20bf750a876625848
Alug <alufoliehorst@gmail.com>
2026-01-09 11:08:10 +0100

    fix InitColorLUT

--- src/v_video.c
+++ src/v_video.c
@@ -3821,7 +3821,7 @@ void InitColorLUT(colorlookup_t *lut, RGBA_t *palette, boolean makecolors)
 		lut->init = true;
 		memcpy(lut->palette, palette, palsize);
 
-		for (i = 0; i < 0xFFFF; i++)
+		for (i = 0; i < 0x10000; i++)
 			lut->table[i] = 0xFFFF;
 
 		if (makecolors)
@@ -3834,7 +3834,9 @@ void InitColorLUT(colorlookup_t *lut, RGBA_t *palette, boolean makecolors)
 				{
 					for (b = 0; b < 0xFF; b++)
 					{
-						lut->table[i] = GetColorLUT(lut, r, g, b);
+						i = CLUTINDEX(r, g, b);
+						if (lut->table[i] == 0xFFFF)
+							lut->table[i] = NearestPaletteColor(r, g, b, palette);
 					}
 				}
 			}
