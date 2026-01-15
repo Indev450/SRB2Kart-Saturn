3ec9ddfb87c91b87aa66aa3b8be1194e29b85555
Alug <alufoliehorst@gmail.com>
2026-01-15 21:20:17 +0100

    Revert "get rid of calling "NetUpdate" in the renderer"
    This reverts commit 5ee3b41969e3ea72580e38a15d53a4a7eceac225.

--- src/d_main.c
+++ src/d_main.c
@@ -593,6 +593,8 @@ static boolean D_Display(void)
 	//
 	if (wipe)
 	{
+		// note: moved up here because NetUpdate does input changes
+		// and input during wipe tends to mess things up
 		wipedefindex += WIPEFINALSHIFT;
 
 		if (rendermode != render_none)
@@ -602,6 +604,8 @@ static boolean D_Display(void)
 			ranwipe = true;
 		}
 	}
+
+	NetUpdate(); // send out any new accumulation
 
 	// It's safe to end the game now.
 	if (G_GetExitGameFlag())
