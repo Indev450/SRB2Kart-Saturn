34f004c172c9336ea8b44e8705480ee50830dc98
Alug <alufoliehorst@gmail.com>
2026-02-02 16:10:15 +0100

    hw_clip: replace "RoundToInt" with lrint
    prefer to use standard lib functions if possible
    lrint does the same thing (well ofc as long as no one touches the rounding direction löl) and is even quite a lot faster (though this is negligible in this case)

--- src/hardware/hw_clip.c
+++ src/hardware/hw_clip.c
@@ -335,7 +335,7 @@ angle_t gld_FrustumAngle(angle_t tiltangle)
 	floatangle = 2.0 + (45.0 + ((double)tilt / 1.9)) * clipfov / 90.0;
 	if (floatangle >= 180.0)
 		return 0xffffffff;
-	a1 = (angle_t)xs_CRoundToInt(ANG1 * floatangle);
+	a1 = (angle_t)lrint(ANG1 * floatangle);
 	return a1;
 }
 
