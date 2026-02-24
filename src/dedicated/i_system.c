014a22e9336e4b5d34bdda2afbd61ca12de07c7a
Alug <alugfolie@proton.me>
2026-02-24 16:06:27 +0100

    w_wad: replace unordered_map with verstable hashmap for lumpnumcache + convert back to C from C++
    been rethinking this and been doing some digging online
    the unordered_map was just a quick and simple fix for performance issues in blan, but came with caveats:
    - unordered_map is simple to use but not particulary great in terms of performance during lookups especially
    - we needed to convert w_wad to c++ just for the lumpnumcache
    - due to needing to preserve the key since they gotta outlive the scope of the function that inserts things into the map, we effectively did a string copy on each map insert and lookup
    after some research came across verstable, which is basically almost the exact same as unordered map but in C while being extremely fast (even more with its builtin string hash and comparisor)
    only caveat with verstable is we need to rely on a new lib, though its just a single header file, and we gotta manage ensuring the input keys outlive functionscope ourselves, which we do with strdup in this case
    good thing, verstable allows to call a "destructor" specified by us whenever it removes or replaces keys in the map
    this also means we only need to copy strings on insert and not on lookup anymore
    all in all this is much faster while giving us the simple usage of c++ unordered_map while staying in C
    
    due to the strdup quirk i had to slightly modify verstable to allow to take const as inputs for the getter functions even if the keys themselves are not const marked with "KEY_GET_CONST"
    
    Verstable can be found here: https://github.com/JacksonAllan/Verstable
    and is licensed under MIT which is perfect!

--- src/dedicated/i_system.c
+++ src/dedicated/i_system.c
@@ -1384,6 +1384,7 @@ static void I_Fork(void)
 
 INT32 I_StartupSystem(void)
 {
+	W_Startup();
 	I_StartupConsole();
 #ifdef NEWSIGNALHANDLER
 	// This is useful when debugging. It lets GDB attach to
