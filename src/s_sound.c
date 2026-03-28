6760546bcd5facc000327e8be0c56d21022193d7
Indev <bdmitry450@gmail.com>
2026-03-29 00:34:11 +0300

    I thought i reverted debug print aaaaaaaa

--- src/s_sound.c
+++ src/s_sound.c
@@ -1346,7 +1346,6 @@ static boolean ReadMusicDefFields(UINT16 wadnum, int line, char *stoken, musicde
 
 // turn _ into spaces.
 #define ADDDEF(field)\
-	CONS_Printf("%s = %s\n",#field, textline); \
 	STRBUFCPY(def->field, textline);\
 	for (textline = def->field; *textline; textline++)\
 		if (*textline == '_') *textline = ' ';
