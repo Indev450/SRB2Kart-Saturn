eea6bb5c1af18db7805a5cd086a587387d66b7a6
Alug <alugfolie@proton.me>
2026-05-10 20:14:28 +0200

    remove outdated comment about precipmobj

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -5815,7 +5815,6 @@ static void BaseNumLaps_OnChange(void)
 	}
 }
 
-
 static void KartFrantic_OnChange(void)
 {
 	if ((boolean)cv_kartfrantic.value != franticitems && gamestate == GS_LEVEL && leveltime > starttime)
