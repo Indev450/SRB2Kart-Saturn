83e305a8c1fe3f51e64f51461d0117401a9461ba
Alug <alugfolie@proton.me>
2026-06-04 09:24:11 +0200

    call PingUpdate more often
    not for 9.3 but a later version
    this is so mindelay is actually useful at all, since it relies on this, and updating this once per second is just not fast enough to make this really viable online to stabilize unstable ping

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -6989,8 +6989,8 @@ static void UpdatePingTable(void)
 
 	if (server)
 	{
-		//if (playing && !(gametime % 8)) // Value chosen based on _my vibes man_ << dont do this for v8 atleast, this is placeboeing ppl to hell and back
-		if (playing && !(gametime % 35))	// update once per second.
+		if (playing && !(gametime % 8)) // Value chosen based on _my vibes man_
+		//if (playing && !(gametime % 35))  // update once per second.
 			PingUpdate();
 
 		fastest = 0;
