4073c2b6997864231ea8584184635e599273fa16
Alug <alugfolie@proton.me>
2026-06-18 17:31:06 +0200

    :chaosleep:

--- src/hu_stuff.c
+++ src/hu_stuff.c
@@ -1282,14 +1282,14 @@ boolean HU_Responder(event_t *ev)
 			I_SetTextInput(false);
 		}
 		else if ((c == KEY_UPARROW || c == KEY_PGUP || c == KEY_MOUSEWHEELUP) &&
-			     (chat_scroll > 0 && !OLDCHAT)) // CHAT SCROLLING YAYS!
+			     ((chat_scroll > 0) && !OLDCHAT)) // CHAT SCROLLING YAYS!
 		{
 			chat_scroll--;
 			justscrolledup = true;
 			chat_scrolltime = 4;
 		}
 		else if ((c == KEY_DOWNARROW || c == KEY_PGDN || c == KEY_MOUSEWHEELDOWN) &&
-			    ((chat_scroll < chat_maxscroll) && (chat_maxscroll) > 0 && !OLDCHAT))
+			    ((chat_scroll < chat_maxscroll) && (chat_maxscroll > 0) && !OLDCHAT))
 		{
 			chat_scroll++;
 			justscrolleddown = true;
