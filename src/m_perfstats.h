KartKrewDev/RingRacers

Add basic profiling for Lua hooks (except HUD hooks)
- Add lua_profile cheat: calculate average time spent in
  hooks over a period
- Show hook times in a sorted table, along with lump name,
  script line number and hook type
- Show cumulative time spent in game logic hooks and
  percentage of overhead to game logic

src/m_perfstats.h
@@ -30,6 +30,7 @@ typedef enum
 } ps_types_t;
 
 extern precise_t ps_tictime;
+extern precise_t ps_prevtictime;
 
 extern precise_t ps_playerthink_time;
 extern precise_t ps_botticcmd_time;

Indev450/SRB2Kart-Saturn

src/m_perfstats.h
@@ -36,6 +36,7 @@ typedef struct
 #define PS_STOP_TIMING(metric) metric.value.p = I_GetPreciseTime() - metric.value.p
 
 extern ps_metric_t ps_tictime;
+extern ps_metric_t ps_prevtictime;
 
 extern ps_metric_t ps_playerthink_time;
 extern ps_metric_t ps_thinkertime;