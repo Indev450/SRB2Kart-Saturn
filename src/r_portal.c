045e46134c0006a2f4b93b34c50748606299491b
Alug <alugfolie@proton.me>
2026-07-13 07:41:01 +0200

    this can be static
    its unused anyways
    also adds plane bounds check which was missing

--- src/r_portal.c
+++ src/r_portal.c
@@ -272,7 +272,7 @@ static boolean TrimVisplaneBounds(const visplane_t* plane, INT16* start, INT16* 
  * Applies the necessary offsets and rotation to give
  * a depth illusion to the skybox.
  */
-void Portal_AddSkybox(const visplane_t* plane)
+static void Portal_AddSkybox(const visplane_t* plane)
 {
 	INT16 start, end;
 	mapheader_t *mh;
@@ -337,6 +337,9 @@ void Portal_AddSkyboxPortals(void)
 	{
 		for (pl = visplanes[i]; pl; pl = pl->next)
 		{
+			if (pl->minx >= pl->maxx)
+				continue;
+
 			if (pl->picnum == skyflatnum)
 			{
 				Portal_AddSkybox(pl);
