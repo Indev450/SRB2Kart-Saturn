KartKrewDev/RingRacers

Support >4GB sys mem in memfree for linux, win32, macOS sys mem counts
Also includes memory counts for the extended allocation functionality

src/android/i_system.c
@@ -34,7 +34,7 @@ static INT64 start_time; // as microseconds since the epoch
 
 // I should probably return how much memory is remaining
 // for this process, considering Android's process memory limit.
-UINT32 I_GetFreeMem(UINT32 *total)
+UINT64 I_GetFreeMem(UINT64 *total)
 {
   // what the heck?  sysinfo() is partially missing in bionic?
   /* struct sysinfo si; */
@@ -44,8 +44,8 @@ UINT32 I_GetFreeMem(UINT32 *total)
   /* return si.freeram; */
   char buf[1024];
   char *memTag;
-  UINT32 freeKBytes;
-  UINT32 totalKBytes;
+  UINT64 freeKBytes;
+  UINT64 totalKBytes;
   INT32 n;
   INT32 meminfo_fd = -1;
 
@@ -56,7 +56,7 @@ UINT32 I_GetFreeMem(UINT32 *total)
   if (n < 0)
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 
@@ -64,7 +64,7 @@ UINT32 I_GetFreeMem(UINT32 *total)
   if (NULL == (memTag = strstr(buf, MEMTOTAL)))
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 
@@ -74,7 +74,7 @@ UINT32 I_GetFreeMem(UINT32 *total)
   if (NULL == (memTag = strstr(buf, MEMFREE)))
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 

src/sdl/i_system.cpp
@@ -81,6 +81,12 @@ typedef LPVOID (WINAPI *p_MapViewOfFile) (HANDLE, DWORD, DWORD, DWORD, SIZE_T);
 #if defined (__unix__) || defined(__APPLE__) || (defined (UNIXCOMMON) && !defined (__HAIKU__))
 #if defined (__linux__)
 #include <sys/vfs.h>
+#elif defined(__APPLE__)
+#include <sys/param.h>
+#include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <mach/mach.h>
 #else
 #include <sys/param.h>
 #include <sys/mount.h>
@@ -2503,8 +2509,7 @@ static long get_entry(const char* name, const char* buf)
 }
 #endif
 
-// quick fix for compil
-UINT32 I_GetFreeMem(UINT32 *total)
+UINT64 I_GetFreeMem(UINT64 *total)
 {
 #ifdef FREEBSD
 	struct vmmeter sum;
@@ -2518,14 +2523,14 @@ UINT32 I_GetFreeMem(UINT32 *total)
 	if ((kd = kvm_open(NULL, NULL, NULL, O_RDONLY, "kvm_open")) == NULL)
 	{
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 	if (kvm_nlist(kd, namelist) != 0)
 	{
 		kvm_close (kd);
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 	if (kvm_read(kd, namelist[X_SUM].n_value, &sum,
@@ -2533,33 +2538,33 @@ UINT32 I_GetFreeMem(UINT32 *total)
 	{
 		kvm_close(kd);
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 	kvm_close(kd);
 
 	if (total)
-		*total = sum.v_page_count * sum.v_page_size;
-	return sum.v_free_count * sum.v_page_size;
+		*total = (UINT64)sum.v_page_count * sum.v_page_size;
+	return (UINT64)sum.v_free_count * sum.v_page_size;
 #elif defined (SOLARIS)
 	/* Just guess */
 	if (total)
 		*total = 32 << 20;
 	return 32 << 20;
 #elif defined (_WIN32)
-	MEMORYSTATUS info;
+	MEMORYSTATUSEX info;
 
-	info.dwLength = sizeof (MEMORYSTATUS);
-	GlobalMemoryStatus( &info );
+	info.dwLength = sizeof (MEMORYSTATUSEX);
+	GlobalMemoryStatusEx( &info );
 	if (total)
-		*total = (UINT32)info.dwTotalPhys;
-	return (UINT32)info.dwAvailPhys;
+		*total = (UINT64)info.ullTotalPhys;
+	return (UINT64)info.ullAvailPhys;
 #elif defined (__linux__)
 	/* Linux */
 	char buf[1024];
 	char *memTag;
-	UINT32 freeKBytes;
-	UINT32 totalKBytes;
+	UINT64 freeKBytes;
+	UINT64 totalKBytes;
 	INT32 n;
 	INT32 meminfo_fd = -1;
 	long Cached;
@@ -2576,7 +2581,7 @@ UINT32 I_GetFreeMem(UINT32 *total)
 	{
 		// Error
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 
@@ -2585,12 +2590,12 @@ UINT32 I_GetFreeMem(UINT32 *total)
 	{
 		// Error
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 
 	memTag += sizeof (MEMTOTAL);
-	totalKBytes = atoi(memTag);
+	totalKBytes = strtoul(memTag, NULL, 10);
 
 	if ((memTag = strstr(buf, MEMAVAILABLE)) == NULL)
 	{
@@ -2604,7 +2609,7 @@ UINT32 I_GetFreeMem(UINT32 *total)
 		{
 			// Error
 			if (total)
-				*total = 0L;
+				*total = 0;
 			return 0;
 		}
 		freeKBytes = MemAvailable;
@@ -2612,12 +2617,40 @@ UINT32 I_GetFreeMem(UINT32 *total)
 	else
 	{
 		memTag += sizeof (MEMAVAILABLE);
-		freeKBytes = atoi(memTag);
+		freeKBytes = strtoul(memTag, NULL, 10);
 	}
 
 	if (total)
 		*total = totalKBytes << 10;
 	return freeKBytes << 10;
+#elif defined(__APPLE__)
+	/* macOS */
+	mach_port_t host = mach_host_self();
+	kern_return_t kr;
+	mach_msg_type_number_t count;
+	vm_size_t v_page_size;
+	struct vm_statistics64 vm_stats;
+	uint64_t total_mem, free_mem;
+	size_t size;
+
+	size = sizeof(total_mem);
+	if (sysctlbyname("hw.memsize", &total_mem, &size, NULL, 0) < 0)
+		total_mem = 0;
+
+	kr = host_page_size(host, &v_page_size);
+	if (kr != KERN_SUCCESS)
+		v_page_size = 4096;
+
+	count = HOST_VM_INFO64_COUNT;
+	kr = host_statistics64(host, HOST_VM_INFO64, (host_info64_t)&vm_stats, &count);
+	if (kr == KERN_SUCCESS)
+		free_mem = (uint64_t)(vm_stats.free_count + vm_stats.inactive_count) * v_page_size;
+	else
+		free_mem = 0;
+
+	if (total)
+		*total = (UINT64)total_mem;
+	return (UINT64)free_mem;
 #else
 	// Guess 48 MB.
 	if (total)

Indev450/SRB2Kart-Saturn

src/android/i_system.c
@@ -34,8 +34,8 @@ size_t I_GetFreeMem(size_t *total)
   /* return si.freeram; */
   char buf[1024];
   char *memTag;
-  UINT32 freeKBytes;
-  UINT32 totalKBytes;
+  size_t freeKBytes;
+  size_t totalKBytes;
   INT32 n;
   INT32 meminfo_fd = -1;
 
@@ -46,7 +46,7 @@ size_t I_GetFreeMem(size_t *total)
   if (n < 0)
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 
@@ -54,7 +54,7 @@ size_t I_GetFreeMem(size_t *total)
   if (NULL == (memTag = strstr(buf, MEMTOTAL)))
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 
@@ -64,7 +64,7 @@ size_t I_GetFreeMem(size_t *total)
   if (NULL == (memTag = strstr(buf, MEMFREE)))
     {
       // Error
-      *total = 0L;
+      *total = 0;
       return 0;
     }
 

src/sdl/i_system.cpp
@@ -83,6 +83,12 @@ typedef LPVOID (WINAPI *p_MapViewOfFile) (HANDLE, DWORD, DWORD, DWORD, SIZE_T);
 #if defined (__unix__) || defined(__APPLE__) || (defined (UNIXCOMMON) && !defined (__HAIKU__))
 #if defined (__linux__)
 #include <sys/vfs.h>
+#elif defined(__APPLE__)
+#include <sys/param.h>
+#include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <mach/mach.h>
 #else
 #include <sys/param.h>
 #include <sys/mount.h>
@@ -2635,13 +2641,13 @@ size_t I_GetFreeMem(size_t *total)
 		*total = 32 << 20;
 	return 32 << 20;
 #elif defined (_WIN32)
-	MEMORYSTATUS info;
+	MEMORYSTATUSEX info;
 
-	info.dwLength = sizeof (MEMORYSTATUS);
-	GlobalMemoryStatus( &info );
+	info.dwLength = sizeof (MEMORYSTATUSEX);
+	GlobalMemoryStatusEx( &info );
 	if (total)
-		*total = (size_t)info.dwTotalPhys;
-	return (size_t)info.dwAvailPhys;
+		*total = (size_t)info.ullTotalPhys;
+	return (size_t)info.ullAvailPhys;
 #elif defined (__OS2__)
 	UINT32 pr_arena;
 
@@ -2674,7 +2680,7 @@ size_t I_GetFreeMem(size_t *total)
 	{
 		// Error
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 
@@ -2683,12 +2689,12 @@ size_t I_GetFreeMem(size_t *total)
 	{
 		// Error
 		if (total)
-			*total = 0L;
+			*total = 0;
 		return 0;
 	}
 
 	memTag += sizeof (MEMTOTAL);
-	totalKBytes = (size_t)atoi(memTag);
+	totalKBytes = strtoul(memTag, NULL, 10);
 
 	if ((memTag = strstr(buf, MEMAVAILABLE)) == NULL)
 	{
@@ -2702,7 +2708,7 @@ size_t I_GetFreeMem(size_t *total)
 		{
 			// Error
 			if (total)
-				*total = 0L;
+				*total = 0;
 			return 0;
 		}
 		freeKBytes = MemAvailable;
@@ -2710,12 +2716,40 @@ size_t I_GetFreeMem(size_t *total)
 	else
 	{
 		memTag += sizeof (MEMAVAILABLE);
-		freeKBytes = atoi(memTag);
+		freeKBytes = strtoul(memTag, NULL, 10);
 	}
 
 	if (total)
 		*total = totalKBytes << 10;
 	return freeKBytes << 10;
+#elif defined(__APPLE__)
+	/* macOS */
+	mach_port_t host = mach_host_self();
+	kern_return_t kr;
+	mach_msg_type_number_t count;
+	vm_size_t v_page_size;
+	struct vm_statistics64 vm_stats;
+	uint64_t total_mem, free_mem;
+	size_t size;
+
+	size = sizeof(total_mem);
+	if (sysctlbyname("hw.memsize", &total_mem, &size, NULL, 0) < 0)
+		total_mem = 0;
+
+	kr = host_page_size(host, &v_page_size);
+	if (kr != KERN_SUCCESS)
+		v_page_size = 4096;
+
+	count = HOST_VM_INFO64_COUNT;
+	kr = host_statistics64(host, HOST_VM_INFO64, (host_info64_t)&vm_stats, &count);
+	if (kr == KERN_SUCCESS)
+		free_mem = (uint64_t)(vm_stats.free_count + vm_stats.inactive_count) * v_page_size;
+	else
+		free_mem = 0;
+
+	if (total)
+		*total = (size_t)total_mem;
+	return (size_t)free_mem;
 #else
 	// Guess 48 MB.
 	if (total)