4631844c9ff13b8389a6b121b9c72fe4bc6d88b7
Alug <alugfolie@proton.me>
2026-07-24 16:07:14 +0200

    revert removing the slopelanding dupe
    dont think this would cause any issues at all, but i dont feel like maybe causing some dumbass edge cases anywhere really
    can test this further after v10 release since its already taking ages

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -2573,7 +2573,7 @@ static void P_PlayerZMovement(mobj_t *mo)
 			P_SetPlayerMobjState(mo, S_KART_STND1);
 		}
 
-#if 0 // praying that removing this dupe wont cause any issues...
+#if 1 // praying that removing this dupe wont cause any issues...
 		if (!mo->standingslope && (mo->eflags & MFE_VERTICALFLIP ? tmceilingslope : tmfloorslope))
 		{
 			// Handle landing on slope during Z movement
