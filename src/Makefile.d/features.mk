4d8e33cebf7c8e7c2a385516d8d6e6bb7cae5ff9
Alug <alugfolie@proton.me>
2026-08-06 22:15:29 +0200

    fix compiler complains when compiling with valgrind support

--- src/Makefile.d/features.mk
+++ src/Makefile.d/features.mk
@@ -91,7 +91,7 @@ ifdef VALGRIND
 VALGRIND_PKGCONFIG?=valgrind
 VALGRIND_LDFLAGS=
 $(eval $(call Use_pkg_config,VALGRIND))
-ZDEBUG=1
+#ZDEBUG=1 we enable this by default
 opts+=-DHAVE_VALGRIND
 endif
 
