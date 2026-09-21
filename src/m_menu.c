e0b9d131caee65bb1e4378c94a94dca663c19642
Indev <bdmitry450@gmail.com>
2026-09-21 22:35:21 +0300

    20

--- src/m_menu.c
+++ src/m_menu.c
@@ -9399,7 +9399,7 @@ static void M_SetupControlsMenu(UINT8 pnum)
 	{
 		// Hide P1-only controls
 		OP_AllControlsMenu[19].status = IT_GRAYEDOUT2; // Chat
-		OP_AllControlsMenu[10].status = IT_GRAYEDOUT2; // Rankings
+		OP_AllControlsMenu[20].status = IT_GRAYEDOUT2; // Rankings
 		OP_AllControlsMenu[21].status = IT_GRAYEDOUT2; // Pause
 		OP_AllControlsMenu[22].status = IT_GRAYEDOUT2; // Screenshot
 		OP_AllControlsMenu[23].status = IT_GRAYEDOUT2; // GIF
@@ -9411,7 +9411,7 @@ static void M_SetupControlsMenu(UINT8 pnum)
 	{
 		// Unhide P1-only controls
 		OP_AllControlsMenu[19].status = IT_CONTROL; // Chat
-		OP_AllControlsMenu[10].status = IT_CONTROL; // Rankings
+		OP_AllControlsMenu[20].status = IT_CONTROL; // Rankings
 		OP_AllControlsMenu[21].status = IT_CONTROL; // Pause
 		OP_AllControlsMenu[22].status = IT_CONTROL; // Screenshot
 		OP_AllControlsMenu[23].status = IT_CONTROL; // GIF
