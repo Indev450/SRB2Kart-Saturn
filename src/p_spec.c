9521183eb3c465c3546ecb835e5237316cfa7469
Alug <alugfolie@proton.me>
2026-09-23 19:16:56 +0200

    cast this to boolean
    no effective change just wanna be more explicit there

--- src/p_spec.c
+++ src/p_spec.c
@@ -5474,7 +5474,7 @@ void P_SpawnSpecials(INT32 fromnetsave, boolean reloadinggamestate)
 					else
 						sectors[s].flags &= ~SF_GRAVITYFLIP;
 
-					CheckForReverseGravity = (sectors[s].flags & SF_GRAVITYFLIP);
+					CheckForReverseGravity = (boolean)(sectors[s].flags & SF_GRAVITYFLIP);
 				}
 				break;
 
